347183814e05182a23426b66ef5b6546fe103115
Bertrand Songis <bsongis@gmail.com>
2019-12-09 16:16:56 +0100

    Cosmetics

--- radio/src/thirdparty/libopenui/src/static.cpp
+++ radio/src/thirdparty/libopenui/src/static.cpp
@@ -23,9 +23,9 @@
 void StaticText::paint(BitmapBuffer * dc)
 {
   if (windowFlags & BUTTON_BACKGROUND)
-    dc->drawSolidFilledRect(0, 0, rect.w, rect.h, DISABLE_COLOR);
+    dc->drawSolidFilledRect(0, 0 + FIELD_PADDING_TOP, rect.w, rect.h, DISABLE_COLOR);
   if (textFlags & CENTERED)
     dc->drawText(rect.w / 2, (rect.h - getFontHeight(textFlags)) / 2, text.c_str(), textFlags);
   else
-    dc->drawText(0, 0, text.c_str(), textFlags);
+    dc->drawText(0, 0 + FIELD_PADDING_TOP, text.c_str(), textFlags);
 }
