2a0b3b30b63860e3c062397cf5a91e05f5e9b376
Bertrand Songis <bsongis@gmail.com>
2020-04-06 09:16:40 +0200

    Generic carousel added

---
+++ radio/src/thirdparty/libopenui/src/carousel.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) OpenTX
+ *
+ * Source:
+ *  https://github.com/opentx/libopenui
+ *
+ * This file is a part of libopenui library.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+#include "carousel.h"
+
+void CarouselWindow::update()
+{
+  auto first = min<int>(selection - 2, items.size() - 5);
+  first = max(0, first);
+  coord_t lastPosition = 0;
+
+  for (auto & item: items) {
+    item.front->detach();
+    item.back->detach();
+  }
+
+  for (uint8_t index = first; lastPosition < width() && index < items.size(); index++) {
+    auto & item = items[index];
+    auto window = (index == selection ? item.front : item.back);
+    window->attach(this);
+    window->setLeft(lastPosition);
+    window->setTop((height() - window->height()) / 2);
+    lastPosition += window->width() + CAROUSEL_SPACING;
+  }
+}
+
+void Carousel::onEvent(event_t event)
+{
+  TRACE_WINDOWS("%s received event 0x%X", getWindowDebugString().c_str(), event);
+
+  if (event == EVT_ROTARY_RIGHT) {
+    if (body->selection < body->items.size() - 1)
+      select(body->selection + 1);
+  }
+  else if (event == EVT_ROTARY_LEFT) {
+    if (body->selection > 0)
+      select(body->selection - 1);
+  }
+  else {
+    Window::onEvent(event);
+  }
+}
