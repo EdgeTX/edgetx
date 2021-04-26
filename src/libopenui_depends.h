d07dc66c7e5158c7966e603e46695b6f8e82cfea
Raphael Coeffic <raphael.coeffic@frafos.com>
2021-04-26 17:04:56 +0200

    Cleanup

--- src/libopenui_depends.h
+++ src/libopenui_depends.h
@@ -26,6 +26,7 @@ void lcdNextLayer();
 uint16_t* lcdGetScratchBuffer();
 void DMACopyBitmap(uint16_t * dest, uint16_t destw, uint16_t desth, uint16_t x, uint16_t y, const uint16_t * src, uint16_t srcw, uint16_t srch, uint16_t srcx, uint16_t srcy, uint16_t w, uint16_t h);
 void DMACopyAlphaBitmap(uint16_t * dest, uint16_t destw, uint16_t desth, uint16_t x, uint16_t y, const uint16_t * src, uint16_t srcw, uint16_t srch, uint16_t srcx, uint16_t srcy, uint16_t w, uint16_t h);
+void DMACopyAlphaMask(uint16_t * dest, uint16_t destw, uint16_t desth, uint16_t x, uint16_t y, const uint8_t * src, uint16_t srcw, uint16_t srch, uint16_t srcx, uint16_t srcy, uint16_t w, uint16_t h, uint16_t bg_color);
 void onKeyPress();
 void onKeyError();
 void killEvents(event_t event);
