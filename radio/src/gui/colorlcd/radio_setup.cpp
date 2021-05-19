ad3a6bb1d6dee88e7c7ebf664f36fb27e25ea062
olliw42 <waldmanns@gmx.de>
2021-05-19 19:11:26 +0200

    combo box for pwr off delay

--- radio/src/gui/colorlcd/radio_setup.cpp
+++ radio/src/gui/colorlcd/radio_setup.cpp
@@ -18,6 +18,13 @@
  * GNU General Public License for more details.
  */
 
+#if defined(PWR_BUTTON_PRESS)
+// should eventually go into translations
+#define LEN_PWR_OFF_DELAYS    "\002"
+#define TR_PWR_OFF_DELAYS     "0s""1s""2s""3s"
+const char STR_PWR_OFF_DELAYS[] = LEN_PWR_OFF_DELAYS TR_PWR_OFF_DELAYS;
+#endif
+
 #define LANGUAGE_PACKS_DEFINITION
 
 #include "radio_setup.h"
@@ -363,17 +370,19 @@ void RadioSetupPage::build(FormWindow * window)
   }
 
 #if defined(PWR_BUTTON_PRESS)
+  // Pwr Off Delay
+  {
     new StaticText(window, grid.getLabelSlot(), STR_PWR_OFF_DELAY);
-    edit = new NumberEdit(window, grid.getFieldSlot(), 0, 3,
-                               [=]() -> int32_t {
-                                 return 2 - g_eeGeneral.pwrOffSpeed;
-                               },
-                               [=](int32_t newValue) {
-                                 g_eeGeneral.pwrOffSpeed = 2 - newValue;
-                                 SET_DIRTY();
-                               });
-    edit->setSuffix("s");
+    new Choice(window, grid.getFieldSlot(), STR_PWR_OFF_DELAYS, 0, 3,
+               [=]() -> int32_t {
+               return 2 - g_eeGeneral.pwrOffSpeed;
+               },
+               [=](int32_t newValue) {
+                   g_eeGeneral.pwrOffSpeed = 2 - newValue;
+                   SET_DIRTY();
+               });
     grid.nextLine();
+  }
 #endif
   
 #if defined(INTERNAL_GPS)
