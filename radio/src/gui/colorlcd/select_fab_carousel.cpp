efdf76a9f5ee9acbb090a7b0fe35be5b6b84626c
Raphael Coeffic <raphael.coeffic@frafos.com>
2021-05-23 09:04:19 +0200

    Updated libopenui
    And fix the view main menu issue with this PR.

--- radio/src/gui/colorlcd/select_fab_carousel.cpp
+++ radio/src/gui/colorlcd/select_fab_carousel.cpp
@@ -36,14 +36,10 @@ void SelectFabCarousel::setMaxButtons(uint8_t max)
 void SelectFabCarousel::addButton(uint8_t icon, const char* title,
                                   std::function<uint8_t(void)> pressHandler)
 {
-  coord_t y_pos = 0;//SELECT_BUTTON_BORDER;
-  coord_t x_pos = pageWidth * buttons;// + SELECT_BUTTON_BORDER / 2;
+  coord_t y_pos = 0;
+  coord_t x_pos = pageWidth * buttons;
   buttons++;
 
-  /*auto button =*/ new SelectFabButton(this, x_pos, y_pos, icon, title, pressHandler);
+  new SelectFabButton(this, x_pos, y_pos, icon, title, pressHandler);
   setInnerWidth(pageWidth * buttons);
-
-  // y_pos += button->width() + SELECT_BUTTON_BORDER;
-  // setHeight(y_pos);
-  // setInnerHeight(y_pos);
 }
