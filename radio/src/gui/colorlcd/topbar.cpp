5f23d35e2c2ff3a7f12423eb90c9d98d0dd69e3e
Raphael Coeffic <raphael.coeffic@frafos.com>
2021-05-24 19:41:55 +0200

    SetupTopBarWidgetsPage: forward touch-slide to children windows
    Resolves #73

--- radio/src/gui/colorlcd/topbar.cpp
+++ radio/src/gui/colorlcd/topbar.cpp
@@ -88,6 +88,7 @@ bool SetupTopBarWidgetsPage::onTouchSlide(coord_t x, coord_t y,
                                           coord_t startX, coord_t startY,
                                           coord_t slideX, coord_t slideY)
 {
+  Window::onTouchSlide(x, y, startX, startY, slideX, slideY);
   return true;
 }
 
