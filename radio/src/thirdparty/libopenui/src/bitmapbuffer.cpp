20f026eef56c4211811573df5eeb8c8ffbf235fc
Raphael Coeffic <raphael.coeffic@frafos.com>
2021-06-07 08:58:49 +0200

    Added BSD license for the implementation of drawFilledTriangle()

--- radio/src/thirdparty/libopenui/src/bitmapbuffer.cpp
+++ radio/src/thirdparty/libopenui/src/bitmapbuffer.cpp
@@ -551,7 +551,39 @@ void BitmapBuffer::invertRect(coord_t x, coord_t y, coord_t w, coord_t h, LcdFla
   }
 }
 
-void BitmapBuffer::drawFilledTriangle(coord_t x0, coord_t y0, coord_t x1, coord_t y1, coord_t x2, coord_t y2, LcdFlags flags, uint8_t opacity)
+/*
+  Notice: BitmapBuffer::drawFilledTriangle use the implementation
+          made for the Adafruit GFX library, which is licensed under BSD
+          license.
+
+  Software License Agreement (BSD License)
+
+  Copyright (c) 2012 Adafruit Industries.  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  - Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+  - Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
+*/
+void BitmapBuffer::drawFilledTriangle(coord_t x0, coord_t y0, coord_t x1,
+                                      coord_t y1, coord_t x2, coord_t y2,
+                                      LcdFlags flags, uint8_t opacity)
 {
   coord_t a, b;
 
