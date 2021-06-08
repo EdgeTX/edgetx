b1dbedd79e5b247b6ae01b6570133bde3d9101fd
Raphael Coeffic <raphael.coeffic@frafos.com>
2021-06-08 07:57:31 +0200

    Removed unrelated statistics methods

--- radio/src/lua/api_mavlink.cpp
+++ radio/src/lua/api_mavlink.cpp
@@ -48,57 +48,6 @@ static int luaMavlinkGetChannelStatus(lua_State * L)
   return 1;
 }
 
-//-- some statistics --
-
-extern int _end;
-extern int _heap_end;
-extern unsigned char *heap;
-
-static int luaMavlinkGetMemUsed(lua_State * L)
-{
-  uint32_t s = luaGetMemUsed(lsScripts);
-#if defined(COLORLCD)
-  uint32_t w = luaGetMemUsed(lsWidgets);
-  uint32_t e = luaExtraMemoryUsage;
-#else
-  uint32_t w = 0;
-  uint32_t e = 0;
-#endif
-  lua_createtable(L, 0, 6);
-  lua_pushtableinteger(L, "scripts", s);
-  lua_pushtableinteger(L, "widgets", w);
-  lua_pushtableinteger(L, "extra", e);
-  lua_pushtableinteger(L, "total", s+w+e);
-  lua_pushtableinteger(L, "heap_used", (int)(heap - (unsigned char *)&_end));
-  lua_pushtableinteger(L, "heap_free", (int)((unsigned char *)&_heap_end - heap));
-  return 1;
-}
-
-static int luaMavlinkGetStackUsed(lua_State * L)
-{
-  lua_createtable(L, 0, 10);
-  lua_pushtableinteger(L, "main_available", stackAvailable()*4);
-  lua_pushtableinteger(L, "main_size", stackSize()*4);
-  lua_pushtableinteger(L, "menus_available", menusStack.available()*4);
-  lua_pushtableinteger(L, "menus_size", menusStack.size());
-  lua_pushtableinteger(L, "mixer_available", mixerStack.available()*4);
-  lua_pushtableinteger(L, "mixer_size", mixerStack.size());
-  lua_pushtableinteger(L, "audio_available", audioStack.available()*4);
-  lua_pushtableinteger(L, "audio_size", audioStack.size());
-  lua_pushtableinteger(L, "mavlink_available", mavlinkStack.available()*4);
-  lua_pushtableinteger(L, "mavlink_size", mavlinkStack.size());
-  return 1;
-}
-
-static int luaMavlinkGetTaskStats(lua_State *L)
-{
-  lua_newtable(L);
-  lua_pushtableinteger(L, "time", mavlinkTaskRunTime());
-  lua_pushtableinteger(L, "max", mavlinkTaskRunTimeMax());
-  lua_pushtableinteger(L, "loop", mavlinkTaskLoop());
-  return 1;
-}
-
 //-- mavlink api --
 
 static int luaMavlinkGetSystemId(lua_State *L)
@@ -215,9 +164,6 @@ static int luaMavlinkSendMessage(lua_State *L)
 const luaL_Reg mavlinkLib[] = {
   { "getVersion", luaMavlinkGetVersion },
   { "getChannelStatus", luaMavlinkGetChannelStatus },
-  { "getMemUsed", luaMavlinkGetMemUsed },
-  { "getStackUsed", luaMavlinkGetStackUsed },
-  { "getTaskStats", luaMavlinkGetTaskStats },
 
   { "getSystemId", luaMavlinkGetSystemId },
   { "getAutopilotIds", luaMavlinkGetAutopilotIds },
