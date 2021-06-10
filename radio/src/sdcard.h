65fca96b690f1fc1fb30020891eaeebc6a545c49
Raphael Coeffic <raphael.coeffic@frafos.com>
2021-06-10 15:17:16 +0200

    Move back to RADIO and MODELS
    instead of RADIO24 and MODELS24

--- radio/src/sdcard.h
+++ radio/src/sdcard.h
@@ -29,8 +29,8 @@
 
 #define PATH_SEPARATOR      "/"
 #define ROOT_PATH           PATH_SEPARATOR
-#define MODELS_PATH         ROOT_PATH "MODELS24"      // no trailing slash = important //TODO Temporary while working on both 2.3 and 2.4
-#define RADIO_PATH          ROOT_PATH "RADIO24"       // no trailing slash = important
+#define MODELS_PATH         ROOT_PATH "MODELS"      // no trailing slash = important
+#define RADIO_PATH          ROOT_PATH "RADIO"       // no trailing slash = important
 #define LOGS_PATH           ROOT_PATH "LOGS"
 #define SCREENSHOTS_PATH    ROOT_PATH "SCREENSHOTS"
 #define SOUNDS_PATH         ROOT_PATH "SOUNDS/en"
