b951dbf9de53a65c4fe17cc31ceb7c22481289ee
eshifri <eshifri@hotmail.com>
2021-07-19 15:17:31 -0700

    fix for #444: Accidentally altering a checkbox.

--- radio/src/thirdparty/libopenui/src/checkbox.cpp
+++ radio/src/thirdparty/libopenui/src/checkbox.cpp
@@ -44,11 +44,15 @@ void CheckBox::onEvent(event_t event)
 #if defined(HARDWARE_TOUCH)
 bool CheckBox::onTouchEnd(coord_t x, coord_t y)
 {
-  if (enabled) {
-    onKeyPress();
-    _setValue(!getValue());
-    setFocus(SET_FOCUS_DEFAULT);
-    invalidate();
+  if (enabled)
+  {
+    if (x < (rect.w >> 2))
+    {
+      onKeyPress();
+      _setValue(!getValue());
+      setFocus(SET_FOCUS_DEFAULT);
+      invalidate();
+    }
   }
   return true;
 }
