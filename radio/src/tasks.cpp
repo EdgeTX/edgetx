6a202015e047c9cfd7aceaa282313c26fb8071a8
Raphael Coeffic <raphael.coeffic@frafos.com>
2021-08-02 17:23:48 +0200

    Fix flashing firmware over S.Port (modules / receiver).
    Resolves #541

--- radio/src/tasks.cpp
+++ radio/src/tasks.cpp
@@ -128,11 +128,12 @@ void execMixerFrequentActions()
   bluetooth.wakeup();
 #endif
 
-  DEBUG_TIMER_START(debugTimerTelemetryWakeup);
-  telemetryWakeup();
-  DEBUG_TIMER_STOP(debugTimerTelemetryWakeup);
+  if (!s_pulses_paused) {
+    DEBUG_TIMER_START(debugTimerTelemetryWakeup);
+    telemetryWakeup();
+    DEBUG_TIMER_STOP(debugTimerTelemetryWakeup);
+  }
 }
-
 
 TASK_FUNCTION(mixerTask)
 {
