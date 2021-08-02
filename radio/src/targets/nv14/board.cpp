d30d53479f9c66cc931e5c289cd38263b7ee0759
eshifri <eshifri@hotmail.com>
2021-08-02 15:14:55 -0700

    cleanup

--- radio/src/targets/nv14/board.cpp
+++ radio/src/targets/nv14/board.cpp
@@ -177,11 +177,13 @@ void boardInit()
   usbInit();
   hapticInit();
 
- #if defined(RTCLOCK) && !defined(COPROCESSOR)
+  boardState = BOARD_STARTED;
+
+ #if defined(RTCLOCK)
   rtcInit(); // RTC must be initialized before rambackupRestore() is called
 #endif
  
-  boardState = BOARD_STARTED;
+  
 #if defined(DEBUG)
   DBGMCU_APB1PeriphConfig(DBGMCU_IWDG_STOP|DBGMCU_TIM1_STOP|DBGMCU_TIM2_STOP|DBGMCU_TIM3_STOP|DBGMCU_TIM4_STOP|DBGMCU_TIM5_STOP|DBGMCU_TIM6_STOP|DBGMCU_TIM7_STOP|DBGMCU_TIM8_STOP|DBGMCU_TIM9_STOP|DBGMCU_TIM10_STOP|DBGMCU_TIM11_STOP|DBGMCU_TIM12_STOP|DBGMCU_TIM13_STOP|DBGMCU_TIM14_STOP, ENABLE);
 #endif
