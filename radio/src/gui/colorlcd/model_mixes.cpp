aa28f1be44a986d9320710978e373a9b393bb308
eshifri <eshifri@hotmail.com>
2021-08-21 15:38:55 -0700

    Shift flight modes string display for vertical screen

--- radio/src/gui/colorlcd/model_mixes.cpp
+++ radio/src/gui/colorlcd/model_mixes.cpp
@@ -287,12 +287,13 @@ class MixLineButton : public CommonInputOrMixButton
       drawFlightModes(dc, line.flightModes, textColor);
     }
 
+    // Put this icon on the first line, since we jave more space there
     uint8_t delayslow = 0;
     if (line.speedDown || line.speedUp) delayslow = 1;
     if (line.delayUp || line.delayDown) delayslow += 2;
     if (delayslow) {
       BitmapBuffer *delayslowbmp[] = {mixerSetupSlowIcon, mixerSetupDelayIcon, mixerSetupDelaySlowIcon};
-      dc->drawMask(width() - 16, PAGE_LINE_HEIGHT + FIELD_PADDING_TOP, delayslowbmp[delayslow - 1], textColor);
+      dc->drawMask(width() - 16, FIELD_PADDING_TOP, delayslowbmp[delayslow - 1], textColor);
     }
   }
 };
