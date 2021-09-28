a0c09e4c0667fe981c981cc0023634810c11ef2e
kkoenig1@optonline.com <kkoenig1@optonline.com>
2021-09-28 14:16:39 -0400

    more ui tweaks.  WidgetSettings top margin and color, much better flightmode edit screen. fix labels in model_setup

--- radio/src/gui/colorlcd/model_flightmodes.cpp
+++ radio/src/gui/colorlcd/model_flightmodes.cpp
@@ -24,6 +24,8 @@
 
 #define SET_DIRTY()     storageDirty(EE_MODEL)
 
+#define LABEL_RIGHT_MARGIN 10
+
 ModelFlightModesPage::ModelFlightModesPage():
   PageTab(STR_MENUFLIGHTMODES, ICON_MODEL_FLIGHT_MODES)
 {
@@ -42,16 +44,7 @@ class FlightModeGroup: public FormGroup
       index(index)
     {
       setFocusHandler([=] (bool focus) {
-
-        int32_t color = focus ? COLOR_THEME_PRIMARY2 : COLOR_THEME_PRIMARY1;
-        if (index == getFlightMode()) 
-          color = COLOR_THEME_PRIMARY1;
-
-        for (auto window: getChildren()) {
-          if (dynamic_cast<StaticText *>(window) != nullptr) {
-            window->setTextFlags(color);
-          }
-        }
+        setSubWindowColors(focus);
       });
     }
 
@@ -67,23 +60,43 @@ class FlightModeGroup: public FormGroup
 
     void paint(BitmapBuffer * dc) override
     {
-      uint32_t fillColor = index == getFlightMode() ? COLOR_THEME_ACTIVE : COLOR_THEME_SECONDARY2;
-      if (getFocus() == this) 
-        fillColor = COLOR_THEME_FOCUS;
-        
-      if (index == getFlightMode()) {
-        dc->drawSolidFilledRect(0, 0, width(), height(), COLOR_THEME_ACTIVE);
-      }
-      else {
-        dc->drawSolidFilledRect(0, 0, width(), height(), fillColor);
-      }
+      uint32_t fillColor = getFocus() != this ? COLOR_THEME_SECONDARY2 : COLOR_THEME_FOCUS;
+      dc->drawSolidFilledRect(0, 0, width(), height(), fillColor);
+
       FormGroup::paint(dc);
+    }
+
+    void setSubWindowColors(bool focus)
+    {
+
+      int32_t color = focus ? COLOR_THEME_PRIMARY2 : COLOR_THEME_PRIMARY1;
+      for (auto window: getChildren()) {
+        if (dynamic_cast<StaticText *>(window) != nullptr) {
+          LcdFlags flags = dynamic_cast<Subtitle *>(window) == nullptr ? RIGHT : 0;
+          window->setTextFlags(flags | color);
+        }
+      }
     }
 
   protected:
     uint8_t index;
     bool active = false;
 };
+
+rect_t getRightAlignedLabelSlot(FormGridLayout &grid)
+{
+  auto rect = grid.getLabelSlot();
+  rect.w -= LABEL_RIGHT_MARGIN;
+  return rect;
+}
+
+rect_t getRightAlignedFieldSlot(FormGridLayout &grid, uint8_t count, uint8_t index)
+{
+  auto rect = grid.getFieldSlot(count, index);
+  rect.w -= LABEL_RIGHT_MARGIN;
+  return rect;
+}
+
 
 void ModelFlightModesPage::build(FormWindow * window)
 {
@@ -102,24 +115,52 @@ void ModelFlightModesPage::build(FormWindow * window)
     grid.spacer();
 
     char label[16];
-    getFlightModeString(label, i+1);
+    getFlightModeString(label, i + 1);
     new Subtitle(group, grid.getLabelSlot(), label, 0, COLOR_THEME_PRIMARY1);
-    grid.nextLine();
+
+    FormField *a = nullptr;
+    std::function<uint32_t (FormField *)> backgroundHandler = [=](FormField *field) {
+      if (! field->isEditMode()  && !field->hasFocus())
+        return i == getFlightMode() ? COLOR_THEME_ACTIVE : COLOR_THEME_PRIMARY2;
+      else if (field->hasFocus() && ! field->isEditMode())
+        return COLOR_THEME_FOCUS;
+      else
+        return COLOR_THEME_EDIT;
+    };
+
+    bool bNarrowScreen = LCD_W < LCD_H;
 
     // Flight mode name
-    new StaticText(group, grid.getLabelSlot(true), STR_NAME, 0, COLOR_THEME_PRIMARY1);
-    new ModelTextEdit(group, grid.getFieldSlot(), g_model.flightModeData[i].name, LEN_FLIGHT_MODE_NAME);
-    grid.nextLine();
+    new StaticText(group, getRightAlignedLabelSlot(grid), STR_NAME, 0, RIGHT | COLOR_THEME_PRIMARY1);
+
+    rect_t r = grid.getFieldSlot();;
+    if (! bNarrowScreen) {
+      r = grid.getFieldSlot(3,0);
+      r.w += 53;
+    }
+    a = new ModelTextEdit(group, r, g_model.flightModeData[i].name, LEN_FLIGHT_MODE_NAME);
+    a->setBackgroundHandler(backgroundHandler);
 
     // Flight mode switch
     if (i > 0) {
-      new StaticText(group, grid.getLabelSlot(true), STR_SWITCH, 0, COLOR_THEME_PRIMARY1);
-      new SwitchChoice(group, grid.getFieldSlot(), SWSRC_FIRST_IN_MIXES, SWSRC_LAST_IN_MIXES, GET_SET_DEFAULT(g_model.flightModeData[i].swtch));
+      if (! bNarrowScreen) {
+        new StaticText(group, getRightAlignedFieldSlot(grid, 3, 0), STR_SWITCH, 0, RIGHT | COLOR_THEME_PRIMARY1);
+        a = new SwitchChoice(group, grid.getFieldSlot(3, 2), SWSRC_FIRST_IN_MIXES, SWSRC_LAST_IN_MIXES, GET_SET_DEFAULT(g_model.flightModeData[i].swtch));
+      } 
+      else {
+        grid.nextLine();
+        new StaticText(group, getRightAlignedLabelSlot(grid), STR_SWITCH, 0, RIGHT | COLOR_THEME_PRIMARY1);
+        a = new SwitchChoice(group, grid.getFieldSlot(), SWSRC_FIRST_IN_MIXES, SWSRC_LAST_IN_MIXES, GET_SET_DEFAULT(g_model.flightModeData[i].swtch));
+      }
+      a->setBackgroundHandler(backgroundHandler);
+      grid.nextLine();
+    }
+    else {
       grid.nextLine();
     }
 
     // Flight mode trims
-    new StaticText(group, grid.getLabelSlot(true), STR_TRIMS, 0, COLOR_THEME_PRIMARY1);
+    new StaticText(group, getRightAlignedLabelSlot(grid), STR_TRIMS, 0, RIGHT | COLOR_THEME_PRIMARY1);
 
     const char * STR_VTRIMS_MODES = "\002"
                                     "--"
@@ -143,28 +184,35 @@ void ModelFlightModesPage::build(FormWindow * window)
                                     "+8";
     for (int t = 0; t < NUM_TRIMS; t++) {
       // TODO isTrimModeAvailable to avoid +{{CURRENT}}
-      new Choice(group, grid.getFieldSlot(NUM_TRIMS, t), STR_VTRIMS_MODES, -1, 2*MAX_FLIGHT_MODES-1,
+      a = new Choice(group, grid.getFieldSlot(NUM_TRIMS, t), STR_VTRIMS_MODES, -1, 2*MAX_FLIGHT_MODES-1,
                  GET_DEFAULT(g_model.flightModeData[i].trim[t].mode==TRIM_MODE_NONE ? -1 : g_model.flightModeData[i].trim[t].mode),
                  SET_DEFAULT(g_model.flightModeData[i].trim[t].mode));
+      a->setBackgroundHandler(backgroundHandler);
     }
     grid.nextLine();
 
     // Flight mode fade in / out
-    new StaticText(group, grid.getLabelSlot(true), "Fade in/out", 0, COLOR_THEME_PRIMARY1);
-    new NumberEdit(group, grid.getFieldSlot(2, 0), 0, DELAY_MAX,
+    new StaticText(group, getRightAlignedLabelSlot(grid), "Fade in/out", 0, RIGHT | COLOR_THEME_PRIMARY1);
+    a = new NumberEdit(group, grid.getFieldSlot(2, 0), 0, DELAY_MAX,
                           GET_DEFAULT(g_model.flightModeData[i].fadeIn),
                           SET_VALUE(g_model.flightModeData[i].fadeIn, newValue),
                           0, PREC1);
-    new NumberEdit(group, grid.getFieldSlot(2, 1), 0, DELAY_MAX,
+    a->setBackgroundHandler(backgroundHandler);
+    a = new NumberEdit(group, grid.getFieldSlot(2, 1), 0, DELAY_MAX,
                           GET_DEFAULT(g_model.flightModeData[i].fadeOut),
                           SET_VALUE(g_model.flightModeData[i].fadeOut, newValue),
                           0, PREC1);
+    a->setBackgroundHandler(backgroundHandler);
+
     grid.nextLine();
 
     grid.spacer();
     coord_t height = grid.getWindowHeight();
     group->setHeight(height);
     y += height + 2;
+
+    if (i == 0)
+      group->setSubWindowColors(true);
   }
 
   char label[32];
