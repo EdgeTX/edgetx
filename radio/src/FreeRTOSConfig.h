d77ff2f4e918c71920e93b75bceca67028db2c42
Raphael Coeffic <raphael.coeffic@frafos.com>
2021-10-14 20:08:28 +0200

    Do not use FreeRTOS functions as long as the scheduler is not started
    Otherwise interrupts with a logical priority lower than configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY are masked after the first call to xTaskResumeAll() (after the first critical section is entered, and never released).

--- radio/src/FreeRTOSConfig.h
+++ radio/src/FreeRTOSConfig.h
@@ -74,6 +74,10 @@ to exclude the API function. */
 #define INCLUDE_vTaskDelayUntil         1
 #define INCLUDE_vTaskDelay              1
 
+#if defined(THREADSAFE_MALLOC)
+#define INCLUDE_xTaskGetSchedulerState  1
+#endif
+
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
 	/* __BVIC_PRIO_BITS will be specified when CMSIS is being used. */
