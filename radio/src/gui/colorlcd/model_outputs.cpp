3d4d1f714d55e001c871962faca1e53518c0764a
kkoenig1@optonline.com <kkoenig1@optonline.com>
2021-11-20 06:21:38 -0500

    change move to add

--- radio/src/gui/colorlcd/model_outputs.cpp
+++ radio/src/gui/colorlcd/model_outputs.cpp
@@ -216,7 +216,7 @@ void ModelOutputsPage::build(FormWindow * window, int8_t focusChannel)
   grid.spacer(PAGE_PADDING);
   grid.setLabelWidth(66);
 
-  new TextButton(window, grid.getLineSlot(), STR_MOVE_ALL_TRIMS_TO_SUBTRIMS,
+  new TextButton(window, grid.getLineSlot(), STR_ADD_ALL_TRIMS_TO_SUBTRIMS,
     [=] () {
       moveTrimsToOffsets(); // if highlighted and menu pressed - move trims to offsets
       rebuild(window, 0);
