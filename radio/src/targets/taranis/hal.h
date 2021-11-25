059232b3bb03da04bfd7a5246016b8d004303385
Raphael Coeffic <raphael.coeffic@frafos.com>
2021-11-25 10:20:37 +0100

    Aux serial to HAL LL part 1 (tbc)

--- radio/src/targets/taranis/hal.h
+++ radio/src/targets/taranis/hal.h
@@ -1688,10 +1688,13 @@
   #define AUX_SERIAL_GPIO_PinSource_TX      GPIO_PinSource10
   #define AUX_SERIAL_GPIO_PinSource_RX      GPIO_PinSource11
   #define AUX_SERIAL_GPIO_AF                GPIO_AF_USART3
+  #define AUX_SERIAL_GPIO_AF_LL             LL_GPIO_AF_7
   #define AUX_SERIAL_USART                  USART3
   #define AUX_SERIAL_USART_IRQHandler       USART3_IRQHandler
   #define AUX_SERIAL_USART_IRQn             USART3_IRQn
+  #define AUX_SERIAL_DMA_RX                 DMA1
   #define AUX_SERIAL_DMA_Stream_RX          DMA1_Stream1
+  #define AUX_SERIAL_DMA_Stream_RX_LL       LL_DMA_STREAM_1
   #define AUX_SERIAL_DMA_Channel_RX         DMA_Channel_4
 #endif
 
