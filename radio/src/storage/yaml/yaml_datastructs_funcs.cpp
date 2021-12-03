eace8e5a792525232495ab3724bb52f9cb5ac6a8
eshifri <eshifri@hotmail.com>
2021-12-02 21:09:04 -0800

    Remove extra "lua" from yaml mixes

--- radio/src/storage/yaml/yaml_datastructs_funcs.cpp
+++ radio/src/storage/yaml/yaml_datastructs_funcs.cpp
@@ -281,8 +281,6 @@ static bool w_mixSrcRaw(const YamlNode* node, uint32_t val, yaml_writer_func wf,
     else if (val >= MIXSRC_FIRST_LUA
              && val <= MIXSRC_LAST_LUA) {
       
-        if (!wf(opaque, "lua(", 4)) return false;
-
         val -= MIXSRC_FIRST_LUA;
         uint32_t script = val / MAX_SCRIPT_OUTPUTS;
 
