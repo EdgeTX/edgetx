8228dd6b034e27af926137fa3e5b98d679d0f9ea
elecpower <neilh713@tpg.com.au>
2021-12-14 10:30:07 +1100

    Add range checks to switches

--- companion/src/constants.h
+++ companion/src/constants.h
@@ -46,6 +46,7 @@
 #define CPN_MAX_MOUSE_ANALOGS          2
 #define CPN_MAX_GYRO_ANALOGS           2
 #define CPN_MAX_ANALOGS                (CPN_MAX_STICKS + CPN_MAX_POTS + CPN_MAX_SLIDERS + CPN_MAX_MOUSE_ANALOGS + CPN_MAX_GYRO_ANALOGS)
+#define CPN_MAX_SENSORS                60
 
 #define CPN_STR_APP_NAME               QCoreApplication::translate("Companion", "EdgeTX Companion")
 #define CPN_STR_TTL_INFO               QCoreApplication::translate("Companion", "Information")        // shared Title Case words, eg. for a window title or section heading
