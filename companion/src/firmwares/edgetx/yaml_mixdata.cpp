7ef56c5d60fd872df29b0c2b27a1cc3b3744c2a1
Raphael Coeffic <raphael.coeffic@frafos.com>
2022-01-01 11:57:56 +0100

    Throw proper YAML exception instead of std::invalid_argument

--- companion/src/firmwares/edgetx/yaml_mixdata.cpp
+++ companion/src/firmwares/edgetx/yaml_mixdata.cpp
@@ -23,27 +23,6 @@
 #include "yaml_rawsource.h"
 #include "curvereference.h"
 
-// static const YamlLookupTable timerModeLut = {
-//     {TimerData::TIMERMODE_OFF, "OFF"},
-//     {TimerData::TIMERMODE_ON, "ON"},
-//     {TimerData::TIMERMODE_START, "START"},
-//     {TimerData::TIMERMODE_THR, "THR"},
-//     {TimerData::TIMERMODE_THR_REL, "THR_REL"},
-//     {TimerData::TIMERMODE_THR_START, "THR_START"},
-// };
-
-// static const YamlLookupTable trainerModeLut = {
-//   {  TRAINER_MODE_OFF, "OFF"  },
-//   {  TRAINER_MODE_MASTER_JACK, "MASTER_TRAINER_JACK"  },
-//   {  TRAINER_MODE_SLAVE_JACK, "SLAVE"  },
-//   {  TRAINER_MODE_MASTER_SBUS_EXTERNAL_MODULE, "MASTER_SBUS_EXT"  },
-//   {  TRAINER_MODE_MASTER_CPPM_EXTERNAL_MODULE, "MASTER_CPPM_EXT"  },
-//   {  TRAINER_MODE_MASTER_BATTERY_COMPARTMENT, "MASTER_BATT_COMP"  },
-//   {  TRAINER_MODE_MASTER_BLUETOOTH, "MASTER_BT"  },
-//   {  TRAINER_MODE_SLAVE_BLUETOOTH, "SLAVE_BT"  },
-//   {  TRAINER_MODE_MULTI, "MASTER_MULTI"  },
-// };
-
 static const YamlLookupTable mixMultiplexLut = {
   {  MLTPX_ADD, "ADD"  },
   {  MLTPX_MUL, "MUL"  },
@@ -75,7 +54,11 @@ int32_t YamlReadMixWeight(const YAML::Node& node)
     return 10000 + std::stoi(val.substr(2));
   }
 
-  return std::stoi(val);
+  try {
+    return std::stoi(val);
+  } catch(...) {
+    throw YAML::TypedBadConversion<int>(node.Mark());
+  }
 }
 
 std::string YamlWriteMixWeight(int32_t sval)
