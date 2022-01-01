59be4cd5fc828bf20605286bcb8a3f24d153375c
gagarinlg <malte@langermann.net>
2022-01-01 15:46:11 +0100

    fix image display problems in TX16S simulator

--- radio/src/targets/simu/simulcd.cpp
+++ radio/src/targets/simu/simulcd.cpp
@@ -134,7 +134,7 @@ void lcdInit()
 void DMAFillRect(uint16_t *dest, uint16_t destw, uint16_t desth, uint16_t x,
                  uint16_t y, uint16_t w, uint16_t h, uint16_t color)
 {
-#if defined(LCD_VERTICAL_INVERT)
+#if defined(LCD_VERTICAL_INVERT) && 0
   x = destw - (x + w);
   y = desth - (y + h);
 #endif
@@ -151,7 +151,7 @@ void DMACopyBitmap(uint16_t *dest, uint16_t destw, uint16_t desth, uint16_t x,
                    uint16_t srch, uint16_t srcx, uint16_t srcy, uint16_t w,
                    uint16_t h)
 {
-#if defined(LCD_VERTICAL_INVERT)
+#if defined(LCD_VERTICAL_INVERT) && 0
   x = destw - (x + w);
   y = desth - (y + h);
   srcx = srcw - (srcx + w);
@@ -170,7 +170,7 @@ void DMACopyAlphaBitmap(uint16_t *dest, uint16_t destw, uint16_t desth,
                         uint16_t srcw, uint16_t srch, uint16_t srcx,
                         uint16_t srcy, uint16_t w, uint16_t h)
 {
-#if defined(LCD_VERTICAL_INVERT)
+#if defined(LCD_VERTICAL_INVERT) && 0
   x = destw - (x + w);
   y = desth - (y + h);
   srcx = srcw - (srcx + w);
@@ -205,7 +205,7 @@ void DMACopyAlphaMask(uint16_t *dest, uint16_t destw, uint16_t desth,
                       uint16_t srch, uint16_t srcx, uint16_t srcy, uint16_t w,
                       uint16_t h, uint16_t fg_color)
 {
-#if defined(LCD_VERTICAL_INVERT)
+#if defined(LCD_VERTICAL_INVERT) && 0
   x = destw - (x + w);
   y = desth - (y + h);
   srcx = srcw - (srcx + w);
