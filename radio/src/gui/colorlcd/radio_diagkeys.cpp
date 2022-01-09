c26f1ca2c7d06c0bcedc664f4a7f532c3abad61f
rotorman <risto.koiva@web.de>
2022-01-09 20:26:56 +0100

    Switches A to H work now.

--- radio/src/gui/colorlcd/radio_diagkeys.cpp
+++ radio/src/gui/colorlcd/radio_diagkeys.cpp
@@ -84,7 +84,7 @@ class RadioKeyDiagsWindow : public Window
       dc->drawNumber(70, y, rotencValue, COLOR_THEME_PRIMARY1);
 #endif
 #else // defined(PCBNV14) || defined(PCBPL18)
-      // KEYS
+      // KEYS (both radios do NOT have physical keys, only remapped trim keys)
       {
         coord_t y = 1;
         dc->drawTextAtIndex(KEY_COLUMN, y, STR_VKEYS, KEY_ENTER, COLOR_THEME_PRIMARY1);
