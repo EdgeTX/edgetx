43ed059be3b0ad36c5125f8ba47fc219f6114283
Raphael Coeffic <raphael.coeffic@frafos.com>
2022-01-16 14:38:18 +0100

    Add ZORRO & TPRO definitions

--- radio/src/targets/taranis/hal.h
+++ radio/src/targets/taranis/hal.h
@@ -1427,9 +1427,12 @@
   #define INTMODULE_GPIO_PinSource_RX   GPIO_PinSource7
   #define INTMODULE_USART               USART1
   #define INTMODULE_GPIO_AF             GPIO_AF_USART1
+  #define INTMODULE_GPIO_AF_LL          LL_GPIO_AF_7
   #define INTMODULE_USART_IRQHandler    USART1_IRQHandler
   #define INTMODULE_USART_IRQn          USART1_IRQn
+  #define INTMODULE_DMA                 DMA2
   #define INTMODULE_DMA_STREAM          DMA2_Stream7
+  #define INTMODULE_DMA_STREAM_LL       LL_DMA_STREAM_7
   #define INTMODULE_DMA_STREAM_IRQ         DMA2_Stream7_IRQn
   #define INTMODULE_DMA_STREAM_IRQHandler  DMA2_Stream7_IRQHandler
   #define INTMODULE_DMA_FLAG_TC         DMA_IT_TCIF7
