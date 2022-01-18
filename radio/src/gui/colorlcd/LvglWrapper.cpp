b45ad2e38c904813c35a829e342e471ebb2656af
kkoenig1@optonline.com <kkoenig1@optonline.com>
2022-01-18 08:41:09 -0500

    additional changges for keyboard processing

--- radio/src/gui/colorlcd/LvglWrapper.cpp
+++ radio/src/gui/colorlcd/LvglWrapper.cpp
@@ -32,18 +32,11 @@ static lv_disp_draw_buf_t disp_buf;
 static lv_indev_drv_t touchDriver;
 static lv_indev_drv_t keyboard_drv;
 
+static TouchState lastState;
 
 #if defined(HARDWARE_TOUCH)
-static bool touchOccured = false;
-bool getTouchOccured()
-{
-  return touchOccured;
-}
-
-static TouchState lastState;
 TouchState getLastTochState()
 {
-  touchOccured = false;
   return lastState;
 }
 #endif
@@ -55,6 +48,7 @@ static void flushLcd(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_
   lv_disp_flush_ready(disp_drv);
 }
 
+
 extern "C" void keyboardDriverRead(lv_indev_drv_t *drv, lv_indev_data_t *data)
 {
   // if there is a keyboard event then call checkevents
@@ -65,10 +59,6 @@ extern "C" void keyboardDriverRead(lv_indev_drv_t *drv, lv_indev_data_t *data)
 extern "C" void touchDriverRead(lv_indev_drv_t *drv, lv_indev_data_t *data)
 {
 #if defined(HARDWARE_TOUCH)
-  if(!touchPanelEventOccured())
-    return;
-
-  touchOccured = true;
   TouchState st = touchPanelRead();
   lastState = st; // hack for now
 
@@ -85,13 +75,6 @@ extern "C" void touchDriverRead(lv_indev_drv_t *drv, lv_indev_data_t *data)
 #endif
     data->state = LV_INDEV_STATE_PRESSED;
   } else {
-#if defined (LCD_VERTICAL_INVERT)
-    data->point.x = LCD_W - st.x;
-    data->point.y = LCD_H - st.y;
-#else
-    data->point.x = st.x;
-    data->point.y = st.y;
-#endif
     data->state = LV_INDEV_STATE_RELEASED;
   }
 #endif
@@ -110,7 +93,6 @@ lv_color_t makeLvColor(uint32_t colorFlags)
 
 lv_obj_t * canvas=nullptr;
 static lv_color_t cbuf[LV_CANVAS_BUF_SIZE_TRUE_COLOR(LCD_W, LCD_H)] __SDRAM;
-BitmapBuffer canBuf(BMP_RGB565, LCD_W, LCD_H, (uint16_t *)cbuf);
 
 extern BitmapBuffer * lcdFront;
 extern BitmapBuffer * lcd;
