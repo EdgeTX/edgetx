be99b1a363569aeb199821199b4e2f618b91eb21
gagarinlg <malte@langermann.net>
2022-02-13 13:34:22 +0100

    preparation for PL18, differentiate between physical and UI screen size adapt to partial draw calls from LVGL

--- radio/src/targets/horus/board.h
+++ radio/src/targets/horus/board.h
@@ -564,6 +564,8 @@ void ledBlue();
 // LCD driver
 #define LCD_W                          480
 #define LCD_H                          272
+#define LCD_PHYS_H                     LCD_H
+#define LCD_PHYS_W                     LCD_W
 #define LCD_DEPTH                      16
 void lcdInit();
 void lcdRefresh();
