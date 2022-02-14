c62208261e909038db5bee189983b15ef285cb16
Raphael Coeffic <raphael.coeffic@frafos.com>
2022-02-14 14:07:14 +0100

    Allows for multi-byte pictograms in MenuToolbarButton

--- radio/src/thirdparty/libopenui/src/menutoolbar.cpp
+++ radio/src/thirdparty/libopenui/src/menutoolbar.cpp
@@ -28,10 +28,10 @@ void MenuToolbarButton::paint(BitmapBuffer * dc)
         MENUS_TOOLBAR_BUTTON_WIDTH - 2 * MENUS_TOOLBAR_BUTTON_PADDING,
         MENUS_TOOLBAR_BUTTON_WIDTH - 2 * MENUS_TOOLBAR_BUTTON_PADDING,
         COLOR_THEME_FOCUS);
-    dc->drawSizedText(rect.w / 2, (rect.h - getFontHeight(FONT(STD))) / 2 + 1,
-                      &picto, 1, CENTERED | COLOR_THEME_PRIMARY2);
+    dc->drawText(rect.w / 2, (rect.h - getFontHeight(FONT(STD))) / 2 + 1,
+                 picto, CENTERED | COLOR_THEME_PRIMARY2);
   } else {
-    dc->drawSizedText(rect.w / 2, (rect.h - getFontHeight(FONT(STD))) / 2 + 1,
-                      &picto, 1, CENTERED | COLOR_THEME_PRIMARY1);
+    dc->drawText(rect.w / 2, (rect.h - getFontHeight(FONT(STD))) / 2 + 1,
+                 picto, CENTERED | COLOR_THEME_PRIMARY1);
   }
 }
