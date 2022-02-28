EdgeTX/edgetx

commit 0391174e066a8b28cb87c656bec787a4a0fb648c

fix: Lua get/setGlobalVariable not honoring flight mode setting (#1602)
When GV was set to e.g. `FM0` , `setGlobalVariable` would change it to own value. Now it works as it should

radio/src/lua/api_model.cpp
@@ -25,6 +25,7 @@
 #include "lua_api.h"
 #include "../timers.h"
 #include "model_init.h"
+#include "gvars.h"
 
 #if defined(SDCARD_YAML)
 #include <storage/sdcard_yaml.h>
@@ -1513,7 +1514,7 @@ static int luaModelGetGlobalVariable(lua_State *L)
   unsigned int idx = luaL_checkunsigned(L, 1);
   unsigned int phase = luaL_checkunsigned(L, 2);
   if (phase < MAX_FLIGHT_MODES && idx < MAX_GVARS)
-    lua_pushinteger(L, g_model.flightModeData[phase].gvars[idx]);
+    lua_pushinteger(L, getGVarValue(idx, phase));
   else
     lua_pushnil(L);
   return 1;
@@ -1541,7 +1542,7 @@ static int luaModelSetGlobalVariable(lua_State *L)
   unsigned int phase = luaL_checkunsigned(L, 2);
   int value = luaL_checkinteger(L, 3);
   if (phase < MAX_FLIGHT_MODES && idx < MAX_GVARS && value >= -GVAR_MAX && value <= GVAR_MAX) {
-    g_model.flightModeData[phase].gvars[idx] = value;
+    SET_GVAR(idx, value, phase);
     storageDirty(EE_MODEL);
   }
   return 0;

commit 34212be0ef84675f58882080a6fdbd54203d651e

fix: Disable LUA GV funcs if `GVARS=NO` build Compile option breakage from #1602

radio/src/lua/api_model.cpp
@@ -1487,6 +1487,7 @@ static int luaModelSetOutput(lua_State *L)
   return 0;
 }
 
+#if defined(GVARS)
 /*luadoc
 @function model.getGlobalVariable(index, flight_mode)
 
@@ -1547,6 +1548,7 @@ static int luaModelSetGlobalVariable(lua_State *L)
   }
   return 0;
 }
+#endif
 
 /*luadoc
 @function model.getSensor(sensor)
@@ -1644,8 +1646,10 @@ const luaL_Reg modelLib[] = {
   { "setCurve", luaModelSetCurve },
   { "getOutput", luaModelGetOutput },
   { "setOutput", luaModelSetOutput },
+#if defined (GVARS)
   { "getGlobalVariable", luaModelGetGlobalVariable },
   { "setGlobalVariable", luaModelSetGlobalVariable },
+#endif
   { "getSensor", luaModelGetSensor },
   { "resetSensor", luaModelResetSensor },
   { NULL, NULL }  /* sentinel */