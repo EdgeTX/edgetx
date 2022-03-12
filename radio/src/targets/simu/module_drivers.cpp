119d0246fdbcf0126bf3fd94ec3929ff4af7f7cb
Raphael Coeffic <raphael.coeffic@frafos.com>
2022-03-12 08:59:43 +0100

    Use normal soft-serial function for AFHDS3

--- radio/src/targets/simu/module_drivers.cpp
+++ radio/src/targets/simu/module_drivers.cpp
@@ -44,8 +44,7 @@ void extmodulePxx1SerialStart() {}
 void extmodulePxx1PulsesStart() {}
 void extmoduleSendInvertedByte(uint8_t) {}
 void extmoduleSendNextFramePxx1(void const*, unsigned short) {}
-void extmoduleSendNextFrameAFHDS3(void const*, unsigned short) {}
-void extmoduleSendNextFrameSoftSerial100kbit(void const*, unsigned short, bool) {}
+void extmoduleSendNextFrameSoftSerial(void const*, unsigned short, bool) {}
 void extmoduleSendNextFramePpm(void*, unsigned short, unsigned short, bool) {}
 
 void init_trainer_ppm() {}
