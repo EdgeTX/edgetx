ab491239d5e5bac7cec94e702e72eaa6e7751cae
wimalopaan <wilhelm.wm.meier@googlemail.com>
2022-03-22 19:26:04 +0100

    * removed IBUS trainer via ext module bay * let only one of the SBUS/IBUS chooseable (only one can fill the trainer inputs)

--- radio/src/sbus.cpp
+++ radio/src/sbus.cpp
@@ -65,66 +65,6 @@ void sbusSetGetByte(int (*fct)(uint8_t*))
   sbusGetByte = fct;
 }
 
-// Range for pulses (ppm input) is [-512:+512]
-//void processSbusFrame(uint8_t * sbus, int16_t * pulses, uint32_t size)
-//{
-//  if (size != SBUS_FRAME_SIZE || sbus[0] != SBUS_START_BYTE ||
-//      sbus[SBUS_FRAME_SIZE - 1] != SBUS_END_BYTE) {
-//    return;  // not a valid SBUS frame
-//  }
-//  if ((sbus[SBUS_FLAGS_IDX] & (1 << SBUS_FAILSAFE_BIT)) ||
-//      (sbus[SBUS_FLAGS_IDX] & (1 << SBUS_FRAMELOST_BIT))) {
-//    return;  // SBUS invalid frame or failsafe mode
-//  }
-
-//  sbus++; // skip start byte
-
-//  uint32_t inputbitsavailable = 0;
-//  uint32_t inputbits = 0;
-//  for (uint32_t i=0; i<MAX_TRAINER_CHANNELS; i++) {
-//    while (inputbitsavailable < SBUS_CH_BITS) {
-//      inputbits |= *sbus++ << inputbitsavailable;
-//      inputbitsavailable += 8;
-//    }
-//    *pulses++ = ((int32_t) (inputbits & SBUS_CH_MASK) - SBUS_CH_CENTER) * 5 / 8;
-//    inputbitsavailable -= SBUS_CH_BITS;
-//    inputbits >>= SBUS_CH_BITS;
-//  }
-
-//  ppmInputValidityTimer = PPM_IN_VALID_TIMEOUT;
-//}
-
-//void processSbusInput()
-//{
-//#if !defined(SIMU)
-//  uint8_t rxchar;
-//  uint32_t active = 0;
-//  static uint8_t SbusIndex = 0;
-//  static uint16_t SbusTimer;
-//  static uint8_t SbusFrame[SBUS_FRAME_SIZE];
-
-//  while (sbusGetByte(&rxchar)) {
-//    active = 1;
-//    if (SbusIndex > SBUS_FRAME_SIZE-1) {
-//      SbusIndex = SBUS_FRAME_SIZE-1;
-//    }
-//    SbusFrame[SbusIndex++] = rxchar;
-//  }
-//  if (active) {
-//    SbusTimer = getTmr2MHz();
-//    return;
-//  }
-//  else {
-//    if (SbusIndex) {
-//      if ((uint16_t) (getTmr2MHz() - SbusTimer) > SBUS_FRAME_GAP_DELAY) {
-//        processSbusFrame(SbusFrame, ppmInput, SbusIndex);
-//        SbusIndex = 0;
-//      }
-//    }
-//  }
-//#endif
-//}
-
 namespace  SBus {
     struct Servo {
         using SBus = Trainer::Protocol::SBus;
