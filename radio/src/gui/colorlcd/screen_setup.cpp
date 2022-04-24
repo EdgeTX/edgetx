4671ae99cb427aaebd66af5d00c64a60852a8ad0
Raphael Coeffic <raphael.coeffic@frafos.com>
2022-04-24 14:03:39 +0200

    split SetupWidgetsPage

--- radio/src/gui/colorlcd/screen_setup.cpp
+++ radio/src/gui/colorlcd/screen_setup.cpp
@@ -26,11 +26,11 @@
 #include "topbar.h"
 #include "libopenui.h"
 #include "layouts/layout_factory_impl.h"
+#include "widgets_setup.h"
 
 #define SET_DIRTY()   storageDirty(EE_MODEL)
 #define BUTTON_HEIGHT 30
 #define BUTTON_WIDTH  75
-
 
 class LayoutChoice: public FormField
 {
@@ -107,149 +107,6 @@ class LayoutChoice: public FormField
   protected:
     std::function<const LayoutFactory *()> getValue;
     std::function<void(const LayoutFactory *)> setValue;
-};
-
-SetupWidgetsPageSlot::SetupWidgetsPageSlot(FormGroup * parent, const rect_t & rect, WidgetsContainer* container, uint8_t slotIndex):
-  Button(parent, rect)
-{
-  setPressHandler([parent, container, slotIndex]() -> uint8_t {
-
-      Menu * menu = new Menu(parent);
-      menu->addLine(TR_SELECT_WIDGET, [=]() {
-          Menu * menu = new Menu(parent);
-          for (auto factory: getRegisteredWidgets()) {
-            menu->addLine(factory->getDisplayName(), [=]() {
-                container->createWidget(slotIndex, factory);
-                auto widget = container->getWidget(slotIndex);
-                if(widget->getOptions() && widget->getOptions()->name)
-                  new WidgetSettings(parent, widget);
-            });
-          }
-      });
-
-      if (container->getWidget(slotIndex)) {
-        auto widget = container->getWidget(slotIndex);
-        if(widget->getOptions() && widget->getOptions()->name)
-          menu->addLine(TR_WIDGET_SETTINGS, [=]() {
-            new WidgetSettings(parent, widget);
-        });
-        menu->addLine(STR_REMOVE_WIDGET, [=]() {
-            container->removeWidget(slotIndex);
-        });
-      }
-
-      return 0;
-    });
-}
-
-void SetupWidgetsPageSlot::paint(BitmapBuffer * dc)
-{
-  if (hasFocus()) {
-    dc->drawRect(0, 0, width()-1, height()-1, 2, STASHED, COLOR_THEME_FOCUS);
-  }
-  else {
-    dc->drawSolidRect(0, 0, width()-1, height()-1, 2, COLOR_THEME_PRIMARY3);
-  }
-}
-
-class SetupWidgetsPage: public FormWindow
-{
-  public:
-    SetupWidgetsPage(ScreenMenu* menu, uint8_t customScreenIdx):
-      FormWindow(ViewMain::instance(), {0, 0, 0, 0}, FORM_FORWARD_FOCUS),
-      menu(menu),
-      customScreenIdx(customScreenIdx)
-    {
-      Layer::push(this);
-
-      // attach this custom screen here so we can display it
-      auto screen = customScreens[customScreenIdx];
-      if (screen) {
-        setRect(screen->getRect());
-        auto viewMain = ViewMain::instance();
-        savedView = viewMain->getCurrentMainView();
-        viewMain->setCurrentMainView(customScreenIdx);
-        viewMain->bringToTop();
-      }
-
-      for (unsigned i = 0; i < screen->getZonesCount(); i++) {
-        auto rect = screen->getZone(i);
-        auto widget = new SetupWidgetsPageSlot(this, rect, customScreens[customScreenIdx], i);
-        if (i == 0) widget->setFocus();
-      }
-
-#if defined(HARDWARE_TOUCH)
-      new Button(
-          this, {0, 0, MENU_HEADER_BACK_BUTTON_WIDTH, MENU_HEADER_BACK_BUTTON_HEIGHT},
-          [this]() -> uint8_t {
-            this->deleteLater();
-            return 1;
-          },
-          NO_FOCUS | FORM_NO_BORDER);
-#endif
-    }
-
-#if defined(DEBUG_WINDOWS)
-    std::string getName() const override
-    {
-      return "SetupWidgetsPage(idx=" + std::to_string(customScreenIdx) + ")";
-    }
-#endif
-
-    void deleteLater(bool detach = true, bool trash = true) override
-    {
-#if defined(HARDWARE_TOUCH)
-      Keyboard::hide();
-#endif
-      auto screen = customScreens[customScreenIdx];
-      if (screen) {
-        auto viewMain = ViewMain::instance();
-        viewMain->setCurrentMainView(savedView);
-      }
-      FormWindow::deleteLater(detach, trash);
-
-      // restore screen setting tab on top
-      menu->bringToTop();
-      Layer::pop(this);
-    }
-
-#if defined(HARDWARE_TOUCH)
-    bool onTouchSlide(coord_t x, coord_t y, coord_t startX, coord_t startY,
-                      coord_t slideX, coord_t slideY) override
-    {                     
-      FormWindow::onTouchSlide(x, y, startX, startY, slideX, slideY);
-      return true;
-    }
-
-    bool onTouchEnd(coord_t x, coord_t y) override
-    {
-      FormWindow::onTouchEnd(x, y);
-      return true;
-    }
-#endif
-
-#if defined(HARDWARE_KEYS)
-    void onEvent(event_t event) override
-    {
-      TRACE_WINDOWS("%s received event 0x%X", getWindowDebugString().c_str(), event);
-
-      if (event == EVT_KEY_LONG(KEY_EXIT) || event == EVT_KEY_BREAK(KEY_EXIT)) {
-        killEvents(event);
-        deleteLater();
-      }
-    }
-#endif
-
-  protected:
-    ScreenMenu* menu;
-    uint8_t customScreenIdx;
-    unsigned savedView = 0;
-
-    // void paint(BitmapBuffer * dc) override
-    // {
-    //   //ViewMain::instance()->fullPaint(dc);
-    //   //dc->clear(COLOR_THEME_SECONDARY3);
-    // }
 };
 
 ScreenAddPage::ScreenAddPage(ScreenMenu * menu, uint8_t pageIndex):
