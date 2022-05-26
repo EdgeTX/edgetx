2b33e32c5450ff3c4aedda0aade1d7d19c82e0c5
Raphael Coeffic <raphael.coeffic@frafos.com>
2022-05-26 08:56:33 +0200

    fix: FlashDialog and popups

--- radio/src/gui/colorlcd/popups.h
+++ radio/src/gui/colorlcd/popups.h
@@ -19,45 +19,11 @@
  * GNU General Public License for more details.
  */
 
-#ifndef _POPUPS_H_
-#define _POPUPS_H_
+#pragma once
 
-#include "libopenui.h"
 #include <functional>
 
 typedef std::function<void(const char *, const char *, int, int)> ProgressHandler;
 
-inline MessageDialog * createPopupInformation(const char * message)
-{
-  return new MessageDialog(MainWindow::instance(), "Message", message);
-}
-
-inline MessageDialog * createPopupWarning(const char * message)
-{
-  return new MessageDialog(MainWindow::instance(), "Warning", message);
-}
-
-inline void POPUP_INFORMATION(const char * message)
-{
-  auto popup = createPopupInformation(message);
-  while (popup->getParent()) {
-    WDG_RESET();
-    MainWindow::instance()->run(false);
-    LvglWrapper::instance()->run();
-  }
-}
-
-inline void POPUP_WARNING(const char * message, const char * info = nullptr)
-{
-  auto popup = createPopupWarning(message);
-  if (info) {
-    popup->setInfoText(std::string(info));
-  }
-  while (popup->getParent()) {
-    WDG_RESET();
-    MainWindow::instance()->run(false);
-    LvglWrapper::instance()->run();
-  }
-}
-
-#endif // _POPUPS_H_
+void POPUP_INFORMATION(const char * message);
+void POPUP_WARNING(const char * message, const char * info = nullptr);
