b9b31a659f77eaaf6a066ff060c0b990c1d2f31f
Raphael Coeffic <raphael.coeffic@frafos.com>
2022-07-03 09:00:11 +0200

    Fix NV14 simulation

--- radio/src/gui/colorlcd/module_setup.cpp
+++ radio/src/gui/colorlcd/module_setup.cpp
@@ -514,7 +514,7 @@ void ModuleSubTypeChoice::update()
     setGetValueHandler(GET_DEFAULT(md->subType));
     setSetValueHandler(SET_DEFAULT(md->subType));
 
-#if defined(PCBNV14)
+#if defined(PCBNV14) && !defined(SIMU)
     if (moduleIdx == INTERNAL_MODULE) {
       if (hardwareOptions.pcbrev == PCBREV_NV14) {
         md->subType = FLYSKY_SUBTYPE_AFHDS2A;
@@ -524,9 +524,11 @@ void ModuleSubTypeChoice::update()
         setAvailableHandler([](int v) { return v == FLYSKY_SUBTYPE_AFHDS3; });
       }
     }
-#else
+#elif !defined(SIMU)
     md->subType = FLYSKY_SUBTYPE_AFHDS3;
     setAvailableHandler([](int v) { return v == FLYSKY_SUBTYPE_AFHDS3; });
+#else
+    setAvailableHandler(nullptr);
 #endif
   }
 #endif
