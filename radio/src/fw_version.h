EdgeTX/edgetx

commit ee0c37157d8e56152415503586f7afda51c3a535

Remove legacy fonts and make the bootloader compile with LVGL

radio/src/fw_version.h
@@ -19,34 +19,24 @@
  * GNU General Public License for more details.
  */
 
-#ifndef _LCD_H_
-#define _LCD_H_
+#pragma once
+#include "stamp.h"
 
-#include "bitmapbuffer.h"
-#include "opentx_types.h"
-
-#include "colors.h"
-
-#define DISPLAY_PIXELS_COUNT           (LCD_W * LCD_H)
-#define DISPLAY_BUFFER_SIZE            (DISPLAY_PIXELS_COUNT)
-
-#if defined(BOOT)
-  #define BLINK_ON_PHASE               (0)
+#if defined(COLORLCD)
+extern const char fw_stamp[];
+extern const char vers_stamp[];
+extern const char date_stamp[];
+extern const char time_stamp[];
+extern const char cfgv_stamp[];
 #else
-  #define FAST_BLINK_ON_PHASE          (g_blinkTmr10ms & (1<<5))
-  #define BLINK_ON_PHASE               (g_blinkTmr10ms & (1<<6))
-  #define SLOW_BLINK_ON_PHASE          (g_blinkTmr10ms & (1<<7))
+extern const char vers_stamp[];
 #endif
 
-// Init LVGL and its display driver
-void lcdInitDisplayDriver();
-
-// Patch the draw context to allow for direct drawing
-void lcdInitDirectDrawing();
-
-inline void lcdClear() { lcd->clear(); }
-
-void lcdRefresh();
-void lcdFlushed();
-
-#endif // _LCD_H_
+#if defined(STM32)
+/**
+ * Tries to find opentx version in the first 1024 byte of either firmware/bootloader
+ * (the one not running) or the buffer
+ * @param buffer If non-null find the firmware version in the buffer instead
+ */
+const char * getFirmwareVersion(const char * buffer = nullptr);
+#endif

radio/src/gui/colorlcd/radio_version.cpp
@@ -23,6 +23,8 @@
 #include "opentx.h"
 #include "options.h"
 #include "libopenui.h"
+#include "fw_version.h"
+
 #if defined(CROSSFIRE)
   #include "mixer_scheduler.h"
 #endif

radio/src/stamp.cpp
@@ -19,8 +19,11 @@
  * GNU General Public License for more details.
  */
 
-#include "opentx.h"
-#include "stamp.h"
+#include "definitions.h"
+#include "board.h"
+#include "fw_version.h"
+
+#include <string.h>
 
 #define STR2(s) #s
 #define DEFNUMSTR(s)  STR2(s)
@@ -112,5 +115,10 @@ const char * getFirmwareVersion(const char * buffer)
   }
 
   return "no version found";
+}
+#else
+const char * getFirmwareVersion(const char * buffer)
+{
+  return "edgetx-" FLAVOUR "-" VERSION DISPLAY_VERSION " (" GIT_STR ")";
 }
 #endif

commit f2805c5930da0ebfaf4847aa3d18eff6aff51dff

fix: Remove CFGV as no longer needed Since it related to the EEPROM config value, which is no longer relevant. Additionally, this broke after ee0c371

radio/src/fw_version.h
@@ -27,7 +27,6 @@ extern const char fw_stamp[];
 extern const char vers_stamp[];
 extern const char date_stamp[];
 extern const char time_stamp[];
-extern const char cfgv_stamp[];
 #else
 extern const char vers_stamp[];
 #endif

radio/src/gui/colorlcd/radio_version.cpp
@@ -272,7 +272,6 @@ void RadioVersionPage::build(FormWindow * window)
   version += vers_stamp + nl;
   version += date_stamp + nl;
   version += time_stamp + nl;
-  version += cfgv_stamp + nl;
   version += "OPTS: ";
 
   for (uint8_t i = 0; options[i]; i++) {

radio/src/stamp.cpp
@@ -28,8 +28,6 @@
 #define STR2(s) #s
 #define DEFNUMSTR(s)  STR2(s)
 
-#define CFGV_STR DEFNUMSTR(EEPROM_VER);
-
 #define TAB "\037\033"
 
 #if defined(FRSKY_RELEASE)
@@ -59,18 +57,17 @@
   #endif
   const char date_stamp[]   = "DATE" TAB ": " DATE;
   const char time_stamp[]   = "TIME" TAB ": " TIME;
-  const char cfgv_stamp[]   = "CFGV" TAB ": " CFGV_STR;
 #elif defined(BOARD_NAME)
-  const char vers_stamp[]   = "FW" TAB ": edgetx-" BOARD_NAME "\036VERS" TAB ": " VERSION DISPLAY_VERSION " (" GIT_STR ")" "\036DATE" TAB ": " DATE " " TIME "\036CFGV" TAB ": " CFGV_STR;
+  const char vers_stamp[]   = "FW" TAB ": edgetx-" BOARD_NAME "\036VERS" TAB ": " VERSION DISPLAY_VERSION " (" GIT_STR ")" "\036DATE" TAB ": " DATE " " TIME;
 #elif defined(RADIOMASTER_RELEASE)
-  const char vers_stamp[]   = "FW" TAB ": edgetx-" FLAVOUR    "\036VERS" TAB ": RM Factory (" GIT_STR ")" "\036BUILT BY : EdgeTX" "\036DATE" TAB ": " DATE " " TIME "\036CFGV" TAB ": " CFGV_STR;
+  const char vers_stamp[]   = "FW" TAB ": edgetx-" FLAVOUR    "\036VERS" TAB ": RM Factory (" GIT_STR ")" "\036BUILT BY : EdgeTX" "\036DATE" TAB ": " DATE " " TIME;
 #elif defined(JUMPER_RELEASE)
-  const char vers_stamp[]   = "FW" TAB ": edgetx-" FLAVOUR    "\036VERS" TAB ": Factory (" GIT_STR ")" "\036BUILT BY : EdgeTX" "\036DATE" TAB ": " DATE " " TIME "\036CFGV" TAB ": " CFGV_STR;
+  const char vers_stamp[]   = "FW" TAB ": edgetx-" FLAVOUR    "\036VERS" TAB ": Factory (" GIT_STR ")" "\036BUILT BY : EdgeTX" "\036DATE" TAB ": " DATE " " TIME;
 #else
   #if defined(VERSION_TAG)
-    const char vers_stamp[]   = "FW" TAB ": edgetx-" FLAVOUR    "\036VERS" TAB ": " VERSION_TAG DISPLAY_VERSION "\036NAME" ": " CODENAME "\036DATE" TAB ": " DATE " " TIME "\036CFGV" TAB ": " CFGV_STR;
+    const char vers_stamp[]   = "FW" TAB ": edgetx-" FLAVOUR    "\036VERS" TAB ": " VERSION_TAG DISPLAY_VERSION "\036NAME" ": " CODENAME "\036DATE" TAB ": " DATE " " TIME;
   #else
-    const char vers_stamp[]   = "FW" TAB ": edgetx-" FLAVOUR    "\036VERS" TAB ": " VERSION "-" VERSION_SUFFIX DISPLAY_VERSION "\036GIT#" TAB ": " GIT_STR "\036DATE" TAB ": " DATE " " TIME "\036CFGV" TAB ": " CFGV_STR;
+    const char vers_stamp[]   = "FW" TAB ": edgetx-" FLAVOUR    "\036VERS" TAB ": " VERSION "-" VERSION_SUFFIX DISPLAY_VERSION "\036GIT#" TAB ": " GIT_STR "\036DATE" TAB ": " DATE " " TIME;
   #endif
 #endif
 