6028c24f360ff99a6a8e95cc6187914f7af8762b
Peter Feerick <peter.feerick@gmail.com>
2022-08-16 20:41:09 +1000

    chore: Update paths in legacy build scripts

--- tools/build-imrc.py
+++ tools/build-imrc.py
@@ -35,7 +35,7 @@ def build(board, translation, srcdir):
         suffix = "" if index == 0 else "_%d" % index
         filename = "output/ghost_firm_%s_%s_%s%s.bin" % (board.lower(), translation.lower(), timestamp(), suffix)
         if not os.path.exists(filename):
-            shutil.copy("%s/firmware.bin" % path, filename)
+            shutil.copy("%s/arm-none-eabi/firmware.bin" % path, filename)
             break
         index += 1
     shutil.rmtree(path)
