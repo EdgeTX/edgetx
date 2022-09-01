EdgeTX/edgetx

commit 62692dba6abc5b5b5cb48c65e9eb191a5d384055

feat: More rotary encoder options for B&W screens (see #2045)
* First build

* Fix X9

* Update api_general.cpp

* Update translations.h

* clean up translations

* fix: B&W option handling, remove magic numbers, cleanup
- Remove references to invertRotary, change to RotEncDir or similar
- Add compatabilty importing to companion, firmware
- Rename Lua API call
- Move magic numbers to enum
- Improve B&W option handling so it doesn't apply changes while in menu

* chore: yaml regen

* fix: Add missing guards for X9D/X9D+

* fix: Wrong enum name in simu

* fix(cpn): Use correct names for UI elements

* fix: Translations

* fix: Allow `rotEncMode` in all yaml due to x9d var
Of the three X9D variants, the X9D+2019 is the only one that
has a rotary encoder as standard. However, as the base config
file is for the X9D, this meant it wasn't reading or writing the
config value. Rather than add a seperate config file for it for
a single variable, simple to just allow it across all targets.

* fix: Companion string, change colorlcd to `Choice`

Co-authored-by: Peter Feerick <peter.feerick@gmail.com>

radio/src/gui/navigation/navigation_x9d.cpp
@@ -439,7 +439,16 @@ void check(event_t event, uint8_t curr, const MenuHandlerFunc *menuTab, uint8_t 
       }
 
       do {
-        INC(l_posVert, MENU_FIRST_LINE_EDIT(horTab, horTabMax), rowcount-1);
+#if defined(ROTARY_ENCODER_NAVIGATION)
+        if (g_eeGeneral.rotEncMode >=
+            ROTARY_ENCODER_MODE_INVERT_VERT_HORZ_NORM) {
+          DEC(l_posVert, MENU_FIRST_LINE_EDIT(horTab, horTabMax), rowcount - 1);
+        } else {
+          INC(l_posVert, MENU_FIRST_LINE_EDIT(horTab, horTabMax), rowcount - 1);
+        }
+#else
+        DEC(l_posVert, MENU_FIRST_LINE_EDIT(horTab, horTabMax), rowcount - 1);
+#endif
       } while (CURSOR_NOT_ALLOWED_IN_ROW(l_posVert));
 
       s_editMode = 0; // if we go down, we must be in this mode
@@ -458,17 +467,31 @@ void check(event_t event, uint8_t curr, const MenuHandlerFunc *menuTab, uint8_t 
           DEC(l_posHorz, 0, maxcol);
           break;
         }
-      }
-      else if (l_posHorz > 0) {
+      } else if (l_posHorz > 0) {
         l_posHorz--;
         break;
-      }
-      else {
+      } else {
+#if defined(ROTARY_ENCODER_NAVIGATION)
+        l_posHorz =
+            g_eeGeneral.rotEncMode == ROTARY_ENCODER_MODE_INVERT_VERT_HORZ_ALT
+                ? 0
+                : 0xff;
+#else
         l_posHorz = 0xff;
+#endif
       }
 
       do {
-        DEC(l_posVert, MENU_FIRST_LINE_EDIT(horTab, horTabMax), rowcount-1);
+#if defined(ROTARY_ENCODER_NAVIGATION)
+        if (g_eeGeneral.rotEncMode >=
+            ROTARY_ENCODER_MODE_INVERT_VERT_HORZ_NORM) {
+          INC(l_posVert, MENU_FIRST_LINE_EDIT(horTab, horTabMax), rowcount - 1);
+        } else {
+          DEC(l_posVert, MENU_FIRST_LINE_EDIT(horTab, horTabMax), rowcount - 1);
+        }
+#else
+          DEC(l_posVert, MENU_FIRST_LINE_EDIT(horTab, horTabMax), rowcount - 1);
+#endif
       } while (CURSOR_NOT_ALLOWED_IN_ROW(l_posVert));
 
       s_editMode = 0; // if we go up, we must be in this mode

companion/src/firmwares/edgetx/yaml_generalsettings.cpp
@@ -202,7 +202,7 @@ Node convert<GeneralSettings>::encode(const GeneralSettings& rhs)
   node["noJitterFilter"] = (int)rhs.noJitterFilter;
   node["disableRtcWarning"] = (int)rhs.rtcCheckDisable;  // TODO: verify
   node["keysBacklight"] = (int)rhs.keysBacklight;
-  node["rotEncDirection"] = (int)rhs.rotEncDirection;
+  node["rotEncMode"] = (int)rhs.rotEncMode;
   node["imperial"] = rhs.imperial;
   node["ttsLanguage"] = rhs.ttsLanguage;
   node["beepVolume"] = rhs.beepVolume + 2;
@@ -396,7 +396,8 @@ bool convert<GeneralSettings>::decode(const Node& node, GeneralSettings& rhs)
   node["noJitterFilter"] >> rhs.noJitterFilter; // new, but don't write old
   node["disableRtcWarning"] >> rhs.rtcCheckDisable;  // TODO: verify
   node["keysBacklight"] >> rhs.keysBacklight;
-  node["rotEncDirection"] >> rhs.rotEncDirection;
+  node["rotEncDirection"] >> rhs.rotEncMode;    // PR2045: read old name and
+  node["rotEncMode"] >> rhs.rotEncMode;         // new, but don't write old
   node["imperial"] >> rhs.imperial;
   node["ttsLanguage"] >> rhs.ttsLanguage;
   node["beepVolume"] >> ioffset_int(rhs.beepVolume, 2);

companion/src/firmwares/generalsettings.h
@@ -238,7 +238,7 @@ class GeneralSettings {
     bool noJitterFilter;
     bool rtcCheckDisable;
     bool keysBacklight;
-    bool rotEncDirection;
+    unsigned int rotEncMode;
     unsigned int imperial;
     char ttsLanguage[TTS_LANGUAGE_LEN + 1];
     int beepVolume;

companion/src/firmwares/opentx/opentxeeprom.cpp
@@ -3035,11 +3035,10 @@ OpenTxGeneralData::OpenTxGeneralData(GeneralSettings & generalData, Board::Type 
   internalField.Append(new BoolField<1>(this, generalData.rtcCheckDisable));
   if (IS_JUMPER_T18(board)) {
     internalField.Append(new BoolField<1>(this, generalData.keysBacklight));
-    internalField.Append(new BoolField<1>(this, generalData.rotEncDirection));
+    internalField.Append(new SpareBitsField<1>(this));
   }
   else {
-    internalField.Append(new SpareBitsField<1>(this));
-    internalField.Append(new  BoolField<1>(this, generalData.rotEncDirection));
+    internalField.Append(new SpareBitsField<2>(this));
   }
 
   for (int i=0; i<4; i++) {
@@ -3350,6 +3349,7 @@ OpenTxGeneralData::OpenTxGeneralData(GeneralSettings & generalData, Board::Type 
 
   if (version >= 220) {
     internalField.Append(new SignedField<2>(this, generalData.uartSampleMode, "Uart Sample Mode"));
+    internalField.Append(new UnsignedField<2>(this, generalData.rotEncMode));
     //internalField.Append(new SpareBitsField<6>(this));  // may need padding to end of 8 byte boundary
   }
 }

companion/src/generaledit/generalsetup.cpp
@@ -126,11 +126,11 @@ ui(new Ui::GeneralSetup)
   }
 
   if (!firmware->getCapability(RotaryEncoderNavigation)) {
-    ui->invertRotary_CB->hide();
-    ui->invertRotary_label->hide();
+    ui->rotEncMode_CB->hide();
+    ui->rotEncMode_label->hide();
   }
   else {
-    ui->invertRotary_CB->setChecked(generalSettings.rotEncDirection);
+    populateRotEncModeCB();
   }
 
   if (!firmware->getCapability(HasPxxCountry)) {
@@ -483,10 +483,29 @@ void GeneralSetupPanel::on_faimode_CB_stateChanged(int)
   emit modified();
 }
 
-void GeneralSetupPanel::on_invertRotary_CB_stateChanged(int)
+void GeneralSetupPanel::populateRotEncModeCB()
 {
-  generalSettings.rotEncDirection = ui->invertRotary_CB->isChecked();
-  emit modified();
+  QComboBox * b = ui->rotEncMode_CB;
+  QString strings[] = { tr("Normal"), tr("Inverted"), tr("Vertical Inverted, Horizontal Normal"), tr("Vertical Inverted, Horizontal Alternate") };
+  int itemCount = 4;
+
+  if (Boards::getCapability(firmware->getBoard(), Board::HasColorLcd)) {
+    itemCount = 2;
+  }
+
+  b->clear();
+  for (uint8_t i=0; i < itemCount; i++) {
+    b->addItem(strings[i], 0);
+  }
+  b->setCurrentIndex(generalSettings.rotEncMode);
+}
+
+void GeneralSetupPanel::on_rotEncMode_CB_currentIndexChanged(int index)
+{
+  if (!lock) {
+    generalSettings.rotEncMode = index;
+    emit modified();
+  }
 }
 
 void GeneralSetupPanel::on_speakerPitchSB_editingFinished()

companion/src/generaledit/generalsetup.h
@@ -49,7 +49,7 @@ class GeneralSetupPanel : public GeneralPanel
     void on_countrycode_CB_currentIndexChanged(int index);
     void on_units_CB_currentIndexChanged(int index);
     void on_faimode_CB_stateChanged(int );
-    void on_invertRotary_CB_stateChanged(int);
+    void on_rotEncMode_CB_currentIndexChanged(int index);
     void on_speakerPitchSB_editingFinished();
     void on_timezoneSB_editingFinished();
     void on_adjustRTC_stateChanged(int);
@@ -100,6 +100,7 @@ class GeneralSetupPanel : public GeneralPanel
     void populateBacklightCB();
     void populateVoiceLangCB();
     void populateRotEncCB(int reCount);
+    void populateRotEncModeCB();
     void updateVarioPitchRange();
 };
 

companion/src/generaledit/generalsetup.ui
@@ -1146,21 +1146,14 @@ p, li { white-space: pre-wrap; }
       <widget class="QComboBox" name="re_CB"/>
      </item>
      <item row="21" column="0">
-      <widget class="QLabel" name="invertRotary_label">
+      <widget class="QLabel" name="rotEncMode_label">
        <property name="text">
-        <string>Invert Rotary</string>
+        <string>Rotary Encoder Mode</string>
        </property>
       </widget>
      </item>
      <item row="21" column="1">
-      <widget class="QCheckBox" name="invertRotary_CB">
-       <property name="toolTip">
-        <string>Invert the direction of the rotary encoder</string>
-       </property>
-       <property name="text">
-        <string/>
-       </property>
-      </widget>
+      <widget class="QComboBox" name="rotEncMode_CB"/>
      </item>
     </layout>
    </item>

radio/src/datastructs_private.h
@@ -812,7 +812,7 @@ PACK(struct TrainerData {
 #if defined(BUZZER)
   #define BUZZER_FIELD int8_t buzzerMode:2    // -2=quiet, -1=only alarms, 0=no keys, 1=all (only used on AVR radios without audio hardware)
 #else
-  #define BUZZER_FIELD int8_t spare1:2 SKIP
+  #define BUZZER_FIELD int8_t spare2:2 SKIP
 #endif
 
 PACK(struct RadioData {
@@ -830,7 +830,7 @@ PACK(struct RadioData {
   int8_t antennaMode:2 ENUM(AntennaModes);
   uint8_t disableRtcWarning:1;
   uint8_t keysBacklight:1;
-  int8_t rotEncDirection:1;
+  NOBACKUP(uint8_t spare1:1 SKIP);
   NOBACKUP(uint8_t internalModule ENUM(ModuleType));
   NOBACKUP(TrainerData trainer);
   NOBACKUP(uint8_t view);            // index of view in main screen
@@ -899,13 +899,17 @@ PACK(struct RadioData {
 
   char ownerRegistrationID[PXX2_LEN_REGISTRATION_ID];
 
+  CUST_ATTR(rotEncDirection, r_rotEncDirection, nullptr);
+  NOBACKUP(uint8_t  rotEncMode:2);
+
   NOBACKUP(int8_t   uartSampleMode:2); // See UartSampleModes
+
 #if defined(STICK_DEAD_ZONE)
   NOBACKUP(uint8_t  stickDeadZone:3);
-  NOBACKUP(uint8_t  spare2:3 SKIP);
 #else
-  NOBACKUP(uint8_t  spare2:6 SKIP);
+  NOBACKUP(uint8_t  stickDeadZoneSpare:3 SKIP);
 #endif
+  NOBACKUP(uint8_t  spare4:1 SKIP);
 
 #if defined(IMU)
   NOBACKUP(int8_t imuMax);

commit a79f00f675d887cde32481c83896f1c20d427c99

fix(x9d): navigation in both directions using +/-
This issue was introduced in 62692dba6a.

radio/src/gui/navigation/navigation_x9d.cpp
@@ -447,7 +447,7 @@ void check(event_t event, uint8_t curr, const MenuHandlerFunc *menuTab, uint8_t 
           INC(l_posVert, MENU_FIRST_LINE_EDIT(horTab, horTabMax), rowcount - 1);
         }
 #else
-        DEC(l_posVert, MENU_FIRST_LINE_EDIT(horTab, horTabMax), rowcount - 1);
+        INC(l_posVert, MENU_FIRST_LINE_EDIT(horTab, horTabMax), rowcount - 1);
 #endif
       } while (CURSOR_NOT_ALLOWED_IN_ROW(l_posVert));
 