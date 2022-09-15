596a2e36e6ec075f6226e8b248e70dacc1214a9c
jfrickmann <jesper.frickmann@hotmail.com>
2022-09-14 21:13:28 -0400

    Replaced isFresh() by !isOld() as requested in https://github.com/EdgeTX/edgetx/pull/2316#issuecomment-1246759370

--- radio/src/lua/api_general.cpp
+++ radio/src/lua/api_general.cpp
@@ -784,7 +784,7 @@ static int luaGetSourceValue(lua_State * L)
             lua_pushinteger(L, value);
           break;
       }
-      lua_pushboolean(L, telemetryItems[qr.quot].isFresh());
+      lua_pushboolean(L, !telemetryItems[qr.quot].isOld());
     }
     else { // telemetry is not available
       lua_pushnil(L);
