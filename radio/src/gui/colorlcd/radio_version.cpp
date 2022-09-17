50ee50827e4162a39cb83174eca2d87c42064d57
Michael <101.37584@germanynet.de>
2022-09-17 11:03:54 +0200

    cosmetics

--- radio/src/gui/colorlcd/radio_version.cpp
+++ radio/src/gui/colorlcd/radio_version.cpp
@@ -41,8 +41,7 @@ char *getVersion(char *str, PXX2Version version)
   }
 }
 
-static const lv_coord_t col_dsc[] = {LV_GRID_FR(1), /*LV_GRID_FR(1),*/ LV_GRID_FR(2),
-                                     LV_GRID_TEMPLATE_LAST};
+static const lv_coord_t col_dsc[] = {LV_GRID_FR(1), LV_GRID_FR(2), LV_GRID_TEMPLATE_LAST};
 static const lv_coord_t row_dsc[] = {LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
 
 class VersionDialog : public Dialog
