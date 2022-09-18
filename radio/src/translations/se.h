1be73c10bff6b1cb3668471e0912747682c11b99
Peter Feerick <peter.feerick@gmail.com>
2022-09-18 15:36:37 +1000

    fix: Re-add missing `TR_SF_SET_SCREEN`

--- radio/src/translations/se.h
+++ radio/src/translations/se.h
@@ -171,6 +171,7 @@
 #define TR_SF_SCREENSHOT                "Skärmbild"
 #define TR_SF_RACING_MODE               "Tävlingsläge"
 #define TR_SF_DISABLE_TOUCH             "Ej pekskärm"
+#define TR_SF_SET_SCREEN                "Set Main Screen"
 #define TR_SF_RESERVE                   "[reserv]"
 
 #define TR_VFSWFUNC                     TR_SF_SAFETY,"Lärare","Spara trimmar","Nollställ","Sätt ",TR_ADJUST_GVAR,"Volym","Sätt failsafe","Range check","Module bind",TR_SOUND,TR_PLAY_TRACK,TR_PLAY_VALUE, TR_SF_RESERVE,TR_SF_PLAY_SCRIPT,TR_SF_RESERVE,TR_SF_BG_MUSIC,TR_VVARIO,TR_HAPTIC,TR_SDCLOGS,"Belysning",TR_SF_SCREENSHOT,TR_SF_RACING_MODE,TR_SF_DISABLE_TOUCH,TR_SF_SET_SCREEN TR_SF_TEST
