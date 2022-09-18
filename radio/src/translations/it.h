538dfb73797f7b53d578e0a316b3e0fe64de43db
Marco Robustini <robustinimarco@gmail.com>
2022-09-18 14:05:18 +0200

    feat: Updates to Italian translation on radio 🇮🇹 (#2330)
    * Update italian translation
    
    Minor fix
    
    * Update italian translation
    
    Other fix
    
    * Revert to label
    
    Alignment as in the Companion
    
    * Correct label here too

--- radio/src/translations/it.h
+++ radio/src/translations/it.h
@@ -48,7 +48,7 @@
 #define TR_COUNTRY_CODES       TR("US","America"),TR("JP","Japan"),TR("EU","Europa")
 #define TR_USBMODES            TR("Chied","Chiedi"),TR("Joyst","Joystick"),TR("SDCard","Storage"),TR("Serial","Seriale")
 #define TR_JACK_MODES          "Chiedi","Audio","Trainer"
-#define TR_TELEMETRY_PROTOCOLS         "FrSky S.PORT","FrSky D","FrSky D (cable)","TBS Crossfire","Spektrum","AFHDS2A IBUS","Multi Telemetry"
+#define TR_TELEMETRY_PROTOCOLS         "FrSky S.PORT","FrSky D","FrSky D (cable)","TBS Crossfire","Spektrum","AFHDS2A IBUS","Telemetria Multi"
 
 #define TR_SBUS_INVERSION_VALUES       "normal","not inverted"
 #define TR_MULTI_TELEMETRY_MODE        "Off","On","Off+Aux","On+Aux"
@@ -118,12 +118,12 @@
 #endif
 
 #if defined(VOICE)
-    #define TR_PLAY_TRACK      "SuonaTraccia"
-  #define TR_PLAY_BOTH         "Play Entr."
-  #define TR_PLAY_VALUE        TR("LeggiVal", "LeggiValore")
+    #define TR_PLAY_TRACK      "Suona Traccia"
+  #define TR_PLAY_BOTH         "Suona Entr."
+  #define TR_PLAY_VALUE        TR("LeggiVal", "Leggi Valore")
 #else
   #define TR_PLAY_TRACK        "[Brano]"
-  #define TR_PLAY_BOTH         "[Play Entr.]"
+  #define TR_PLAY_BOTH         "[Suona Entr.]"
   #define TR_PLAY_VALUE        "[LeggiValore]"
 #endif
 
@@ -164,10 +164,10 @@
 #define TR_SF_SCREENSHOT       "Screenshot"
 #define TR_SF_RACING_MODE      "Modo Racing"
 #define TR_SF_DISABLE_TOUCH    "No Touch"
-#define TR_SF_SET_SCREEN       "Set Main Screen"
+#define TR_SF_SET_SCREEN       "Setta Schermo Princ."
 #define TR_SF_RESERVE          "[riserva]"
 
-#define TR_VFSWFUNC            TR_SF_SAFETY,"Maestro","Trim Instant.","Azzera","Set",TR_ADJUST_GVAR,"Volume","SetFailsafe","RangeCheck","ModuleBind",TR_SOUND,TR_PLAY_TRACK,TR_PLAY_VALUE,TR_SF_RESERVE,TR_SF_PLAY_SCRIPT,TR_SF_RESERVE,TR_SF_BG_MUSIC,TR_VVARIO,TR_HAPTIC,TR_SDCLOGS,"Retroillum.",TR_SF_SCREENSHOT,TR_SF_RACING_MODE,TR_SF_DISABLE_TOUCH,TR_SF_SET_SCREEN TR_SF_TEST
+#define TR_VFSWFUNC            TR_SF_SAFETY,"Maestro","Trim Instant.","Azzera","Set",TR_ADJUST_GVAR,"Volume","SettaFailsafe","RangeCheck","BindModulo",TR_SOUND,TR_PLAY_TRACK,TR_PLAY_VALUE,TR_SF_RESERVE,TR_SF_PLAY_SCRIPT,TR_SF_RESERVE,TR_SF_BG_MUSIC,TR_VVARIO,TR_HAPTIC,TR_SDCLOGS,"Retroillum.",TR_SF_SCREENSHOT,TR_SF_RACING_MODE,TR_SF_DISABLE_TOUCH,TR_SF_SET_SCREEN TR_SF_TEST
 
 #define TR_FSW_RESET_TELEM   TR("Telm", "Telemetr.")
 
@@ -253,9 +253,9 @@
 #define TR_ON_ONE_SWITCHES     "ON","One"
 
 #if defined(COLORLCD)
-  #define TR_ROTARY_ENC_OPT         "Normal","Inverted"
+  #define TR_ROTARY_ENC_OPT         "Normale","Invertito"
 #else
-  #define TR_ROTARY_ENC_OPT         "Normal","Inverted","V-I H-N","V-I H-A"
+  #define TR_ROTARY_ENC_OPT         "Normale","Invertito","V-I H-N","V-I H-A"
 #endif
 
 #if defined(IMU)
@@ -283,13 +283,13 @@
 #define TR_VTRAINER_BLUETOOTH          "Master/" TR("BT","Bluetooth"), "Slave/" TR("BT","Bluetooth")
 #define TR_VTRAINER_MULTI              "Master/Multi"
 #define TR_VFAILSAFE                   "Non settato","Mantieni","Personali","No impulsi","Ricevente"
-#define TR_VSENSORTYPES                "Custom","Calcolato"
+#define TR_VSENSORTYPES                "Person.","Calcolato"
 #define TR_VFORMULAS                   "Somma","Media","Min","Max","Moltipl","Totalizza","Cella","Consumo","Distanza"
 #define TR_VPREC                       "0.--","0.0 ","0.00"
 #define TR_VCELLINDEX                  "Minore","1","2","3","4","5","6","Maggiore","Delta"
-#define TR_TEXT_SIZE                   "Standard","Tiny","Small","Mid","Double"
+#define TR_TEXT_SIZE                   "Standard","Minusc.","Piccolo","Medio","Doppio"
 #define TR_SUBTRIMMODES                STR_CHAR_DELTA" (center only)","= (symetrical)"
-#define TR_TIMER_DIR                   TR("Remain", "Show Remain"), TR("Elaps.", "Show Elapsed")
+#define TR_TIMER_DIR                   TR("Riman.", "Mostra Riman."), TR("Trasc.", "Mostra Trascorso")
 
 #if defined(COLORLCD)
   #define INDENT               "\007"
@@ -342,21 +342,21 @@
 #define TR_COPYINGMODEL        "Copia in corso.."
 #define TR_MOVINGMODEL         "Spostamento..."
 #define TR_LOADINGMODEL        "Caricamento..."
-#define TR_UNLABELEDMODEL              "Unlabeled"
+#define TR_UNLABELEDMODEL      "Senza etic."
 #define TR_NAME                "Nome"
 #define TR_MODELNAME            TR("Nome Mode.", "Nome Modello")
-#define TR_PHASENAME           "Nome fase"
-#define TR_MIXNAME             "Nome mix"
+#define TR_PHASENAME           "Nome Fase"
+#define TR_MIXNAME             "Nome Mix"
 #define TR_INPUTNAME           "Nome Ingr."
-  #define TR_EXPONAME          "Nome expo"
+  #define TR_EXPONAME          "Nome Expo"
 #define TR_BITMAP              "Immagine"
 #define TR_TIMER               TR("Timer", "Timer ")
 #define TR_START                       "Start"
-#define TR_ELIMITS             TR("Limiti.E", "Limiti Estesi")
-#define TR_ETRIMS              TR("Trim Ext", "Trim Estesi")
+#define TR_ELIMITS             TR("Lim. Est.", "Limiti Estesi")
+#define TR_ETRIMS              TR("Trim Est.", "Trim Estesi")
 #define TR_TRIMINC             "Passo Trim"
-#define TR_DISPLAY_TRIMS       TR("Mos. Trims", "Mostra Trims")
-#define TR_TTRACE              TR("So. motore", INDENT "Sorgente Motore")
+#define TR_DISPLAY_TRIMS       TR("Mos.Trims", "Mostra Trims")
+#define TR_TTRACE              TR("Sor. Mot.", INDENT "Sorgente Motore")
 #define TR_TTRIM               TR("Trim Mot.", INDENT "Trim Motore")
 #define TR_TTRIM_SW            TR("T-Trim-Sw", INDENT "Trim switch")
 #define TR_BEEPCTR             TR("Beep al c.", "Beep al centro")
@@ -632,20 +632,20 @@
 #define TR_VOLTAGE             TR(INDENT "Voltagg.",INDENT "Voltaggio")
 #define TR_CURRENT             TR(INDENT "Corrente",INDENT "Corrente")
 #define TR_SELECT_MODEL        TR("Scegli mod.", "Scegli Modello")
-#define TR_MODELS                      "Modelli"
-#define TR_SELECT_MODE                 "Seleziona modo"
+#define TR_MODELS              "Modelli"
+#define TR_SELECT_MODE         "Seleziona modo"
 #define TR_CREATE_MODEL        "Crea Modello"
-#define TR_FAVORITE_LABEL       "Favorites"
-#define TR_NEW_MODEL            "New Model"
-#define TR_EDIT_LABELS          "Edit Labels"
-#define TR_ENTER_LABEL          "Enter Label"
-#define TR_LABEL                "Label"
-#define TR_LABELS               "Labels"
-#define TR_CURRENT_MODEL        "Current"
-#define TR_NEW                  "New"
-#define TR_NEW_LABEL            "New Label"
-#define TR_RENAME_LABEL         "Rename Label"
-#define TR_DELETE_LABEL         "Delete Label"
+#define TR_FAVORITE_LABEL       "Favoriti"
+#define TR_NEW_MODEL            "Nuovo Modello"
+#define TR_EDIT_LABELS          "Edita Etichette"
+#define TR_ENTER_LABEL          "Edita Etichetta"
+#define TR_LABEL                "Etichetta"
+#define TR_LABELS               "Etichette"
+#define TR_CURRENT_MODEL        "Corrente"
+#define TR_NEW                  "Nuovo"
+#define TR_NEW_LABEL            "Nuova Etichetta"
+#define TR_RENAME_LABEL         "Rinomina Etichetta"
+#define TR_DELETE_LABEL         "Cancella Etichetta"
 #define TR_DUPLICATE_MODEL     "Duplica Modello"
 #define TR_COPY_MODEL          "Copia Modello"
 #define TR_MOVE_MODEL          "Sposta Modello"
@@ -776,8 +776,8 @@
 #define TR_BLUETOOTH_DIST_ADDR  "Ind. Dist."
 #define TR_BLUETOOTH_LOCAL_ADDR "Ind. Loc."
 #define TR_BLUETOOTH_PIN_CODE   "Codice PIN"
-#define TR_BLUETOOTH_NODEVICES  "No Devices Found"
-#define TR_BLUETOOTH_SCANNING   "Scanning..."
+#define TR_BLUETOOTH_NODEVICES  "Nessun Dispositivo Trovato"
+#define TR_BLUETOOTH_SCANNING   "Scansione..."
 #define TR_BLUETOOTH_BAUDRATE   "BT Baudrate"
 #if defined(PCBX9E)
 #define TR_BLUETOOTH_MODES      "---","Attivo"
@@ -1137,7 +1137,7 @@
 #define TR_MENU_INPUTS          STR_CHAR_INPUT "Ingressi"
 #define TR_MENU_LUA             STR_CHAR_LUA "Lua scripts"
 #define TR_MENU_STICKS           STR_CHAR_STICK "Sticks"
-#define TR_MENU_POTS            STR_CHAR_POT "Pots"
+#define TR_MENU_POTS            STR_CHAR_POT "Potenziometri"
 #define TR_MENU_MAX             STR_CHAR_FUNCTION "MAX"
 #define TR_MENU_HELI            STR_CHAR_CYC "Ciclico"
 #define TR_MENU_TRIMS            STR_CHAR_TRIM "Trims"
@@ -1146,7 +1146,7 @@
 #define TR_MENU_TRAINER         STR_CHAR_TRAINER "Trainer"
 #define TR_MENU_CHANNELS        STR_CHAR_CHANNEL "Canali"
 #define TR_MENU_GVARS           STR_CHAR_SLIDER "GVars"
-#define TR_MENU_TELEMETRY       STR_CHAR_TELEMETRY "Telemetry"
+#define TR_MENU_TELEMETRY       STR_CHAR_TELEMETRY "Telemetria"
 #define TR_MENU_DISPLAY        "DISPLAY"
 #define TR_MENU_OTHER          "Altro"
 #define TR_MENU_INVERT         "Inverti"
