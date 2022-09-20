bcaf0c1d6225c3660c7633fd0f2ff933a8e60210
ulfhedlund <35467815+ulfhedlund@users.noreply.github.com>
2022-09-20 17:22:29 +0200

    Corrected spelling mistake for TR_MOVE_UP

--- radio/src/translations/se.h
+++ radio/src/translations/se.h
@@ -171,7 +171,7 @@
 #define TR_SF_SCREENSHOT                "Skärmbild"
 #define TR_SF_RACING_MODE               "Tävlingsläge"
 #define TR_SF_DISABLE_TOUCH             "Ej pekskärm"
-#define TR_SF_SET_SCREEN                "Set Main Screen"
+#define TR_SF_SET_SCREEN                "Sätt huvudskärm"
 #define TR_SF_RESERVE                   "[reserv]"
 
 #define TR_VFSWFUNC                     TR_SF_SAFETY,"Lärare","Spara trimmar","Nollställ","Sätt ",TR_ADJUST_GVAR,"Volym","Sätt failsafe","Range check","Module bind",TR_SOUND,TR_PLAY_TRACK,TR_PLAY_VALUE, TR_SF_RESERVE,TR_SF_PLAY_SCRIPT,TR_SF_RESERVE,TR_SF_BG_MUSIC,TR_VVARIO,TR_HAPTIC,TR_SDCLOGS,"Belysning",TR_SF_SCREENSHOT,TR_SF_RACING_MODE,TR_SF_DISABLE_TOUCH,TR_SF_SET_SCREEN TR_SF_TEST
@@ -373,8 +373,8 @@
 #define TR_MIXNAME                      TR("Namn","Mixernamn")
 #define TR_INPUTNAME                    TR("Input","Inputnamn")
 #define TR_EXPONAME                     TR("Namn","Radnamn")
-#define TR_BITMAP                       "Modellikon"
 #define TR_NO_PICTURE                   "Ingen bild"
+#define TR_BITMAP                       "Modellikon"
 #define TR_TIMER                        TR("Timer","Timer ")
 #define TR_START                        "Start"
 #define TR_ELIMITS                      TR("Gränser++","Utökade gränser")
@@ -679,7 +679,7 @@
 #define TR_NEW_MODEL                    "Ny modell"
 #define TR_INVALID_MODEL                "Ogiltig modell"
 #define TR_EDIT_LABELS                  "Redigera etiketter"
-#define TR_MOVE_UP                      "Flytta uppp"
+#define TR_MOVE_UP                      "Flytta upp"
 #define TR_MOVE_DOWN                    "Flytta ner"
 #define TR_ENTER_LABEL                  "Ange etikett"
 #define TR_LABEL                        "Etikett"
