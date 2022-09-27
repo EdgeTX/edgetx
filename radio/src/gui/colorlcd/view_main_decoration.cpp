4d85f67a00ac322c70faaf6c68365314b2ad8f8d
Gábor Bányász <36520141+banyaszg@users.noreply.github.com>
2022-09-27 03:43:17 +0200

    fix(color): Remove Main View - Main Zone left side offset (#2374)

--- radio/src/gui/colorlcd/view_main_decoration.cpp
+++ radio/src/gui/colorlcd/view_main_decoration.cpp
@@ -101,7 +101,7 @@ rect_t ViewMainDecoration::getMainZone() const
   // update layout first
   lv_obj_update_layout(parent->getLvObj());
 
-  auto x_left = lv_obj_get_x2(w_ml->getLvObj()) + 1;
+  auto x_left = lv_obj_get_x2(w_ml->getLvObj());
   auto x_right = lv_obj_get_x(w_mr->getLvObj());
 
   lv_coord_t bottom = LCD_H;
