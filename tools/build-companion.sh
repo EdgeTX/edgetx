7e03728de5a408d7a1db05381a70502b3613ccc6
Peter Feerick <peter.feerick@gmail.com>
2022-09-29 21:18:40 +1000

    fix: Build simulator plugin

--- tools/build-companion.sh
+++ tools/build-companion.sh
@@ -66,7 +66,7 @@ cd build
 declare -a simulator_plugins=(x9lite x9lites
                               x7 x7-access
                               t8 t12 tx12 tx12mk2
-                              zorro
+                              zorro commando8
                               tlite tpro lr3pro
                               x9d x9dp x9dp2019 x9e
                               xlite xlites
@@ -154,6 +154,9 @@ do
             ;;
         nv14)
             BUILD_OPTIONS+="-DPCB=NV14"
+            ;;
+        commando8)
+            BUILD_OPTIONS+="-DPCB=X7 -DPCBREV=COMMANDO8"
             ;;
     esac
 
