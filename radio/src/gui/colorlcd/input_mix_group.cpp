d9a7d90c0cf10ef3694fa958ecdc2626d9115041
Raphael Coeffic <raphael.coeffic@frafos.com>
2022-10-04 12:08:04 +0200

    fix: input display and more space for names

--- radio/src/gui/colorlcd/input_mix_group.cpp
+++ radio/src/gui/colorlcd/input_mix_group.cpp
@@ -29,7 +29,7 @@
 #include <algorithm>
 
 static const lv_coord_t col_dsc[] = {
-  LV_DPI_DEF / 2, LV_GRID_FR(1), LV_GRID_TEMPLATE_LAST,
+  lv_coord_t(LV_DPI_DEF * 0.65), LV_GRID_FR(1), LV_GRID_TEMPLATE_LAST,
 };
 
 static const lv_coord_t row_dsc[] = {
@@ -53,21 +53,23 @@ InputMixGroup::InputMixGroup(Window* parent, mixsrc_t idx) :
   lv_obj_add_event_cb(lvobj, InputMixGroup::value_changed,
                       LV_EVENT_VALUE_CHANGED, nullptr);
 
-  chText = lv_label_create(lvobj);
-  lv_label_set_text_fmt(chText, "%02u: ", idx - MIXSRC_CH1 + 1);
-  lv_obj_set_style_text_font(chText, getFont(FONT(XS)), 0);
-  lv_obj_set_style_pad_top(chText, 0, 0);
-  lv_obj_set_grid_cell(chText,
-                       LV_GRID_ALIGN_START, 0, 1,
-                       LV_GRID_ALIGN_START, 0, 1);
+  lv_obj_t* chText = nullptr;
+  if (idx >= MIXSRC_FIRST_CH && idx <= MIXSRC_LAST_CH) {
+    chText = lv_label_create(lvobj);
+    lv_label_set_text_fmt(chText, "%02u: ", idx - MIXSRC_CH1 + 1);
+    lv_obj_set_style_text_font(chText, getFont(FONT(XS)), 0);
+    lv_obj_set_style_pad_top(chText, 0, 0);
+    lv_obj_set_grid_cell(chText,
+                         LV_GRID_ALIGN_START, 0, 1,
+                         LV_GRID_ALIGN_START, 0, 1);
+  }
 
   label = lv_label_create(lvobj);
   lv_label_set_text(label, getSourceString(idx));
   lv_obj_set_style_pad_top(label, 3, 0);
   lv_obj_set_style_pad_bottom(label, 0, 0);
-  lv_obj_set_grid_cell(label,
-                       LV_GRID_ALIGN_START, 0, 1,
-                       LV_GRID_ALIGN_END, 0, 1);
+  lv_obj_set_grid_cell(label, LV_GRID_ALIGN_START, 0, 1,
+                       chText ? LV_GRID_ALIGN_END : LV_GRID_ALIGN_START, 0, 1);
 
   auto box = window_create(lvobj);
   lv_obj_set_size(box, lv_pct(100), LV_SIZE_CONTENT);
