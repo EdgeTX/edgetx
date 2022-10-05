93b3143ff426cb32fbbb308258af85831b29387d
Michael <101.37584@germanynet.de>
2022-10-05 08:00:44 +0200

    removed unnecessary SET_DIRTY() for ModelTextEdit() as it is already handled by ModelTextEdit()
    changed indent for SET_DIRTY() in lambda for Choice()

--- radio/src/gui/colorlcd/widget_settings.cpp
+++ radio/src/gui/colorlcd/widget_settings.cpp
@@ -102,7 +102,6 @@ WidgetSettings::WidgetSettings(Window* parent, Widget* widget) :
         new ModelTextEdit(line, rect_t{},
                           widget->getOptionValue(optIdx)->stringValue,
                           sizeof(widget->getOptionValue(optIdx)->stringValue));
-        SET_DIRTY();
         break;
 
       case ZoneOption::File:
@@ -131,7 +130,7 @@ WidgetSettings::WidgetSettings(Window* parent, Widget* widget) :
             [=](int newValue) {  // setValue
               widget->getOptionValue(optIdx)->unsignedValue =
                   (uint32_t)newValue;
-            SET_DIRTY();
+              SET_DIRTY();
             });
 
         tmChoice->setTextHandler([](int value) {
