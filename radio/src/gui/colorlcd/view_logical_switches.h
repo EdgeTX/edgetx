b438f184b61e5703341afa7da4102c8a874c506e
shane-droid <61736830+shane-droid@users.noreply.github.com>
2022-10-09 15:27:34 +1100

    display friendly LS name on viewlayout

--- radio/src/gui/colorlcd/view_logical_switches.h
+++ radio/src/gui/colorlcd/view_logical_switches.h
@@ -51,7 +51,17 @@ class LogicalSwitchDisplayFooter : public Window
     LogicalSwitchData* cs = lswAddress(index);
 
     LcdFlags textColor = COLOR_THEME_PRIMARY2;
-    dc->drawTextAtIndex(10, 1, STR_VCSWFUNC, cs->func, textColor);
+    
+    
+    
+    //LS friendly name
+    const char* chrs = cs->custName;
+    if(strlen(chrs) > 0){
+      dc->drawText(15, 1, cs->custName, textColor);
+    }else{
+      dc->drawTextAtIndex(5, 1, STR_VCSWFUNC, cs->func, textColor);
+    }
+    
     // CSW params
     unsigned int cstate = lswFamily(cs->func);
 
