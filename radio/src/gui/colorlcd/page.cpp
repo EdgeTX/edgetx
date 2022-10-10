16dfc61e40d31f2bb70ce67700137a3ffeae3fa7
Raphael Coeffic <raphael.coeffic@frafos.com>
2022-10-10 18:15:30 +0200

    fix(color): window tree preventing superfluous drawing
    This prevents the main view to be redrawn if fully covered by a tab group, a page or a full screen dialog.

--- radio/src/gui/colorlcd/page.cpp
+++ radio/src/gui/colorlcd/page.cpp
@@ -53,7 +53,7 @@ static constexpr rect_t _get_body_rect()
 }
 
 Page::Page(unsigned icon):
-  Window(MainWindow::instance(), {0, 0, LCD_W, LCD_H}, OPAQUE),
+  Window(Layer::back(), {0, 0, LCD_W, LCD_H}, OPAQUE),
   header(this, icon),
   body(this, _get_body_rect(), FORM_FORWARD_FOCUS)
 {
