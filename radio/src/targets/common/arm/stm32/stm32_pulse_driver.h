f881d7e11cb639d70bc9f681a7149435edcf036a
Raphael Coeffic <raphael.coeffic@frafos.com>
2022-10-14 17:31:03 +0200

    PPM trainer output: update delay on each cycle

--- radio/src/targets/common/arm/stm32/stm32_pulse_driver.h
+++ radio/src/targets/common/arm/stm32/stm32_pulse_driver.h
@@ -51,6 +51,8 @@ void stm32_pulse_config_output(const stm32_pulse_timer_t* tim, bool polarity,
 
 void stm32_pulse_set_polarity(const stm32_pulse_timer_t* tim, bool polarity);
 
+void stm32_pulse_set_cmp_val(const stm32_pulse_timer_t* tim, uint32_t cmp_val);
+
 // return true if it could be disabled without interrupting a pulse train, false otherwise
 bool stm32_pulse_if_not_running_disable(const stm32_pulse_timer_t* tim);
 
