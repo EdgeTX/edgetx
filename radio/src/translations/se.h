08f89dbbdfb35470ebfdf82dc1ee31039ba74b63
Raphael Coeffic <raphaelcoeffic@users.noreply.github.com>
2022-10-14 02:55:27 +0200

    fix(B&W): `a~x` not being displayed correctly (#2539)

--- radio/src/translations/se.h
+++ radio/src/translations/se.h
@@ -102,7 +102,7 @@
 #endif
 
 #define TR_CSWEQUAL                     "a=x"
-#define TR_VCSWFUNC                     "---",TR_CSWEQUAL,"a~x","a>x","a<x",TR_CSWRANGE,"|a|>x","|a|<x","AND","OR","XOR",TR_CSWSTAY,"a=b","a>b","a<b","Δ≥x","|Δ|≥x",TR_CSWTIMER,TR_CSWSTICKY
+#define TR_VCSWFUNC                     "---",TR_CSWEQUAL,"a" STR_CHAR_TILDE "x","a>x","a<x",TR_CSWRANGE,"|a|>x","|a|<x","AND","OR","XOR",TR_CSWSTAY,"a=b","a>b","a<b",STR_CHAR_DELTA "≥x","|" STR_CHAR_DELTA "|≥x",TR_CSWTIMER,TR_CSWSTICKY
 
 #define TR_TEXT_SIZE                    "Std","XS","S","M","L"
 
