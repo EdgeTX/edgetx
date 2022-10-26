b4a9fbc008670d13a22c1139005c77c15a8fd6a8
Raphael Coeffic <raphael.coeffic@frafos.com>
2022-10-26 08:44:59 +0200

    Fix warnings about bitfield offsets and GCC 4.4
    This is caused by bitfields that cross type boundaries. In this particular case, it happens as the bitfield is not defined the same in `rtc_backup.cpp`.

--- radio/src/datastructs_private.h
+++ radio/src/datastructs_private.h
@@ -825,7 +825,7 @@ PACK(struct RadioData {
   int8_t antennaMode:2 ENUM(AntennaModes);
   uint8_t disableRtcWarning:1;
   uint8_t keysBacklight:1;
-  NOBACKUP(uint8_t spare1:1 SKIP);
+  uint8_t spare1:1 SKIP;
   NOBACKUP(uint8_t internalModule ENUM(ModuleType));
   NOBACKUP(TrainerData trainer);
   NOBACKUP(uint8_t view);            // index of view in main screen
