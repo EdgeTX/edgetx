76228f479a0b5a6d3260fd15c6d6e6e2e2e930cb
raphaelcoeffic <raphael.coeffic@frafos.com>
2022-11-11 09:21:58 +0100

    chore(compilation): silence some warning on apple silicon
    When compiling `simu`, `clang` complains about all the StdPeriph headers declaring assembly with invalid operand width.

--- cmake/toolchain/native.cmake
+++ cmake/toolchain/native.cmake
@@ -1,2 +1,6 @@
 # Native toolchain
 
+if(APPLE)
+  set(CMAKE_C_FLAGS "-Wno-asm-operand-widths -Wno-deprecated-declarations")
+  set(CMAKE_CXX_FLAGS "-Wno-asm-operand-widths -Wno-deprecated-declarations")
+endif()
