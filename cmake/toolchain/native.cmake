3aa71c8e2fff7c2faac96a0589744468ac15252c
raphaelcoeffic <raphael.coeffic@frafos.com>
2022-12-19 08:01:35 +0100

    chore: remove support for binary storage format

--- cmake/toolchain/native.cmake
+++ cmake/toolchain/native.cmake
@@ -3,6 +3,8 @@
 if(APPLE)
   set(CMAKE_C_FLAGS "-Wno-asm-operand-widths -Wno-deprecated-declarations")
   set(CMAKE_CXX_FLAGS "-Wno-asm-operand-widths -Wno-deprecated-declarations")
+  set(CMAKE_C_FLAGS_DEBUG "-Wno-asm-operand-widths -Wno-deprecated-declarations")
+  set(CMAKE_CXX_FLAGS_DEBUG "-Wno-asm-operand-widths -Wno-deprecated-declarations")
 endif()
 
 if(MINGW OR WIN32)
--- radio/src/storage/sdcard_yaml.cpp
+++ radio/src/storage/sdcard_yaml.cpp
@@ -38,8 +38,6 @@
  #include "storage/eeprom_rlc.h"
 #endif
 
-#include "storage/conversions/conversions.h"
-
 const char * readYamlFile(const char* fullpath, const YamlParserCalls* calls, void* parser_ctx, ChecksumResult* checksum_result)
 {
     FIL  file;
@@ -177,30 +175,7 @@ const char * loadRadioSettings()
     if ( (f_stat(RADIO_SETTINGS_YAML_PATH, &fno) != FR_OK) && ((f_stat(RADIO_SETTINGS_TMPFILE_YAML_PATH, &fno) != FR_OK)) ) {
       // If neither the radio configuraion YAML file or the temporary file generated on write exist, this must be a first run with YAML support.
       // - thus requiring a conversion from binary to YAML.
-#if STORAGE_CONVERSIONS < 221
-#if defined(STORAGE_MODELSLIST)
-      uint8_t version;
-      const char* error = loadFileBin(RADIO_SETTINGS_PATH, nullptr, 0, &version);
-      if (error) {
-        TRACE("loadRadioSettings error=%s", error);
-        return error;
-      }
-      convertBinRadioData(RADIO_SETTINGS_PATH, version);
-#elif defined(EEPROM_RLC)
-      // Load from EEPROM
-      uint8_t versions[3];
-      uint16_t* variant = (uint16_t*)&versions[1];
-      if (!eepromOpen() || (eeLoadGeneralSettingsData(versions, 3) != 3)
-          || (*variant != EEPROM_VARIANT)) {
-        return "ERROR";
-      }
-      eeConvert(versions[0]);
-#else
-  #error "Unsupported conversion format"
-#endif
-#else
       return "no radio settings";
-#endif
     }
 
 #if defined(DEFAULT_INTERNAL_MODULE)
