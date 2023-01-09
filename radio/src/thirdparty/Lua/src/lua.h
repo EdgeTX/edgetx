50c2e1b547a3ed1eece704ff47476ece857bb670
raphaelcoeffic <raphael.coeffic@frafos.com>
2023-01-09 07:42:04 +0100

    Ensure byte-code compatibility

--- radio/src/thirdparty/Lua/src/lua.h
+++ radio/src/thirdparty/Lua/src/lua.h
@@ -77,15 +77,15 @@ typedef void * (*lua_Alloc) (void *ud, void *ptr, size_t osize, size_t nsize);
 
 #define LUA_TNIL		0
 #define LUA_TBOOLEAN		1
-#define LUA_TLIGHTUSERDATA      2
-#define LUA_TNUMBER             3
-#define LUA_TSTRING             4
-#define LUA_TTABLE              5
-#define LUA_TFUNCTION           6
-#define LUA_TUSERDATA           7
-#define LUA_TTHREAD             8
+#define LUA_TLIGHTUSERDATA      4
+#define LUA_TNUMBER             5
+#define LUA_TSTRING             6
+#define LUA_TTABLE              7
+#define LUA_TFUNCTION           8
+#define LUA_TUSERDATA           9
+#define LUA_TTHREAD             10
 
-#define LUA_NUMTAGS		9
+#define LUA_NUMTAGS		11
 
 
 
