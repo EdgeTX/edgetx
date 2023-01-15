2465f2fb2fdc54d034395749dc8125a9fb112a6a
Matthias Hryniszak <padcom@gmail.com>
2023-01-15 03:02:03 +0100

    feat(diags): Red LED before RTC data, blue LED after (#3006)
    * Added turning the red LED before RTC data and blue LED afterwards
    
    With the LED turned RED if the RTC data retrieval fails it will stay red.
    This will at least be an indication that the RTC startup encountered
    a problem. For now there is no indication that this problem occurred.
    
    * GH-3005 Moved LED manipulation to conditionally compiled when RTC operation happens

--- radio/src/targets/horus/board.cpp
+++ radio/src/targets/horus/board.cpp
@@ -243,11 +243,12 @@ void boardInit()
   sportUpdateInit();
 #endif
 
-  ledBlue();
-
 #if defined(RTCLOCK) && !defined(COPROCESSOR)
+  ledRed();
   rtcInit(); // RTC must be initialized before rambackupRestore() is called
 #endif
+
+  ledBlue();
 }
 
 void boardOff()
@@ -283,8 +284,8 @@ void boardOff()
   RTC->BKP0R = SHUTDOWN_REQUEST;
 
   pwrOff();
-  
-  // We reach here only in forced power situations, such as hw-debugging with external power  
+
+  // We reach here only in forced power situations, such as hw-debugging with external power
   // Enter STM32 stop mode / deep-sleep
   // Code snippet from ST Nucleo PWR_EnterStopMode example
 #define PDMode             0x00000000U
@@ -298,7 +299,7 @@ void boardOff()
 
 /* Set SLEEPDEEP bit of Cortex System Control Register */
   SET_BIT(SCB->SCR, ((uint32_t)SCB_SCR_SLEEPDEEP_Msk));
-  
+
   // To avoid HardFault at return address, end in an endless loop
   while (1) {
 
