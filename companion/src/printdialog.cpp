acfc7c471fd70a88f00e961a906741aca7e1e347
elecpower <neilh713@tpg.com.au>
2023-02-27 08:08:05 +1100

    Fix printdialog qt functions

--- companion/src/printdialog.cpp
+++ companion/src/printdialog.cpp
@@ -57,7 +57,7 @@ PrintDialog::~PrintDialog()
 void PrintDialog::on_printButton_clicked()
 {
   QPrinter printer;
-  printer.setPageMargins(10.0,10.0,10.0,10.0,printer.Millimeter);
+  printer.setPageMargins(QMarginsF(10.0, 10.0, 10.0, 10.0), QPageLayout::Millimeter);
   QPrintDialog *dialog = new QPrintDialog(&printer, this);
   dialog->setWindowTitle(tr("Print Document"));
   if (dialog->exec() != QDialog::Accepted)
@@ -74,7 +74,7 @@ void PrintDialog::on_printFileButton_clicked()
     fn += ".pdf"; // default
   if (fn.endsWith(".pdf", Qt::CaseInsensitive)) {
     QPrinter printer;
-    printer.setPageMargins(10.0,10.0,10.0,10.0,printer.Millimeter);
+    printer.setPageMargins(QMarginsF(10.0, 10.0, 10.0, 10.0), QPageLayout::Millimeter);
     printer.setOutputFormat(QPrinter::PdfFormat);
     printer.setColorMode(QPrinter::Color);
     printer.setOutputFileName(fn);
@@ -94,7 +94,7 @@ void PrintDialog::printToFile()
     printfilename += ".pdf"; // default
   if (printfilename.endsWith(".pdf", Qt::CaseInsensitive)) {
     QPrinter printer;
-    printer.setPageMargins(10.0,10.0,10.0,10.0,printer.Millimeter);
+    printer.setPageMargins(QMarginsF(10.0, 10.0, 10.0, 10.0), QPageLayout::Millimeter);
     printer.setOutputFormat(QPrinter::PdfFormat);
     printer.setColorMode(QPrinter::Color);
     printer.setOutputFileName(printfilename);
