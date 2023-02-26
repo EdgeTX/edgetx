11134d132a5f4c764b073560a2e967df28569a8b
elecpower <neilh713@tpg.com.au>
2023-02-27 08:07:34 +1100

    Fix labels qt functions

--- companion/src/labels.cpp
+++ companion/src/labels.cpp
@@ -98,7 +98,7 @@ QVariant LabelsModel::data(const QModelIndex &index, int role) const
   } else if (role == Qt::CheckStateRole) {
     if (index.column() == 0 && selectedModel >= 0 &&
       selectedModel < (int)radioData->models.size()) {
-      QStringList modelLabels = QString(radioData->models.at(selectedModel).labels).split(',',QString::SkipEmptyParts);
+      QStringList modelLabels = QString(radioData->models.at(selectedModel).labels).split(',',Qt::SkipEmptyParts);
       label = RadioData::escapeCSV(label);
       return modelLabels.indexOf(label)==-1?Qt::Unchecked:Qt::Checked;
     } else if (index.column() == 0 && selectedModel == -1) {
@@ -125,7 +125,7 @@ QModelIndex LabelsModel::parent(const QModelIndex &index) const
 
 int LabelsModel::rowCount(const QModelIndex &parent) const
 {
-  Q_UNUSED(parent);  
+  Q_UNUSED(parent);
   return labels.size();
 }
 
