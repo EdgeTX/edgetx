c48839a7423c0540e8da9e4723f9dc095936e99f
Phil Mitchell <phil.a.mitchell@gmail.com>
2023-03-08 17:48:15 +1100

    Only create a new model if the user actually selects a template. Cancelling from the template select page will no longer create a blank model.

--- radio/src/gui/colorlcd/model_templates.h
+++ radio/src/gui/colorlcd/model_templates.h
@@ -27,37 +27,42 @@
 
 class TemplatePage : public Page
 {
- public:
-  TemplatePage();
+  public:
+    TemplatePage();
 
-  void updateInfo();
+    void updateInfo();
 
 #if defined(HARDWARE_KEYS)
-  void onEvent(event_t event) override;
+    void onEvent(event_t event) override;
 #endif
 
 #if defined(DEBUG_WINDOWS)
-  std::string getName() const { return "TemplatePage"; }
+    std::string getName() const { return "TemplatePage"; }
 #endif
 
- protected:
-  FormWindow* listWindow = nullptr;
-  lv_obj_t* infoLabel = nullptr;
+  protected:
+    FormWindow* listWindow = nullptr;
+    lv_obj_t* infoLabel = nullptr;
 
-  static constexpr size_t LEN_INFO_TEXT = 300;
-  static constexpr size_t LEN_PATH =
-      sizeof(TEMPLATES_PATH) + TEXT_FILENAME_MAXLEN;
-  static constexpr size_t LEN_BUFFER =
-      sizeof(TEMPLATES_PATH) + 2 * TEXT_FILENAME_MAXLEN + 1;
+    static constexpr size_t LEN_INFO_TEXT = 300;
+    static constexpr size_t LEN_PATH = sizeof(TEMPLATES_PATH) + TEXT_FILENAME_MAXLEN;
+    static constexpr size_t LEN_BUFFER = sizeof(TEMPLATES_PATH) + 2 * TEXT_FILENAME_MAXLEN + 1;
 
-  char buffer[LEN_BUFFER + 1] = "";
-  char infoText[LEN_INFO_TEXT + 1] = "";
-  static std::function<void(void)> update;
+    char buffer[LEN_BUFFER + 1] = "";
+    char infoText[LEN_INFO_TEXT + 1] = "";
 };
 
 class SelectTemplateFolder : public TemplatePage
 {
  public:
-  SelectTemplateFolder(std::function<void(void)> update);
-  ~SelectTemplateFolder();
+    SelectTemplateFolder(std::function<void(std::string, std::string)> update);
+
+    void doUpdate(std::string folder, std::string name) {
+      deleteLater();
+      if (update)
+        update(folder, name);
+    }
+
+  protected:
+    std::function<void(std::string, std::string)> update;
 };
