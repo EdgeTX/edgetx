EdgeTX/edgetx

commit 16a7f0498dc9626c6fc7ff2b53f4c7bb1ac6c8f4

feat: Check MCU type at start (#3272)
* Check MCU type

* MCU check

* MCU check

* chore: Change capitalisation

radio/src/opentx.cpp
@@ -140,6 +140,27 @@ static void readKeysAndTrims()
   }
 }
 
+void checkValidMCU(void)
+{
+  // Checks the radio MCU type matches intended firmware type
+  uint32_t idcode = DBGMCU->IDCODE & 0xFFF;
+
+#if defined(STM32F205xx)
+  #define TARGET_IDCODE   0x411
+#elif defined(STM32F407xx)
+  #define TARGET_IDCODE   0x413
+#elif defined(STM32F429xx)
+  #define TARGET_IDCODE   0x419
+#else
+  // Ensure new radio get registered :)
+  #define TARGET_IDCODE   0x0
+#endif
+
+  if(idcode != TARGET_IDCODE) {
+    runFatalErrorScreen("Wrong MCU");
+  }
+}
+
 void per10ms()
 {
   g_tmr10ms++;
@@ -1667,6 +1688,8 @@ int main()
 
   boardInit();
   pulsesInit();
+
+  checkValidMCU();
 
 #if defined(PCBHORUS)
   if (!IS_FIRMWARE_COMPATIBLE_WITH_BOARD()) {

radio/src/gui/common/stdlcd/radio_version.cpp
@@ -270,10 +270,7 @@ void menuRadioVersion(event_t event)
 
   coord_t y = MENU_HEADER_HEIGHT + 2;
   lcdDrawText(FW, y, vers_stamp, SMLSIZE);
-  y += 5 * (FH - 1);
-
-
-  y += 2;
+  y += 5 * (FH - 1) + 2;
 
 #if defined(PCBTARANIS)
   lcdDrawText(INDENT_WIDTH, y, BUTTON(TR_FIRMWARE_OPTIONS), menuVerticalPosition == ITEM_RADIO_FIRMWARE_OPTIONS ? INVERS : 0);

commit fe4a3df1ffd25593407f695703bcef5b6645ecfc

Fix simulator crash introduced in PR #3272

radio/src/opentx.cpp
@@ -142,6 +142,7 @@ static void readKeysAndTrims()
 
 void checkValidMCU(void)
 {
+#if !defined(SIMU)
   // Checks the radio MCU type matches intended firmware type
   uint32_t idcode = DBGMCU->IDCODE & 0xFFF;
 
@@ -159,6 +160,7 @@ void checkValidMCU(void)
   if(idcode != TARGET_IDCODE) {
     runFatalErrorScreen("Wrong MCU");
   }
+#endif
 }
 
 void per10ms()