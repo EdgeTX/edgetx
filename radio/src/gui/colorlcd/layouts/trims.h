667cc845f8e589fe7484f081a0f811331ced92c7
philmoz <philmoz@users.noreply.github.com>
2023-03-14 14:08:28 +1100

    feat(color): Implement DISPLAY_TRIMS_CHANGE option (#3288)
    * Implement DISPLAY_TRIMS_CHANGE option for color LCD trim widget.
    Refactor code.
    
    * Change data type to prevent build tests failing.
    
    * Display trim value to the side like B&W radios.

--- radio/src/gui/colorlcd/layouts/trims.h
+++ radio/src/gui/colorlcd/layouts/trims.h
@@ -26,17 +26,25 @@
 class MainViewTrim : public Window
 {
   public:
-    MainViewTrim(Window * parent, const rect_t & rect, uint8_t idx):
-      Window(parent, rect),
-      idx(idx)
-    {
-    }
+    MainViewTrim(Window * parent, const rect_t & rect, uint8_t idx);
 
     void checkEvents() override;
+    void paint(BitmapBuffer * dc) override;
 
   protected:
     uint8_t idx;
     int value = 0;
+    bool showChange = false;
+    int trimMin = 0, trimMax = 0;
+
+    void setRange();
+
+    virtual coord_t sx() { return 0; }
+    virtual coord_t sy() { return 0; }
+
+    virtual void drawLine(BitmapBuffer * dc) = 0;
+    virtual void drawMarkerLines(BitmapBuffer * dc, coord_t x, coord_t y) = 0;
+    virtual void drawValue(BitmapBuffer * dc) = 0;
 };
 
 class MainViewHorizontalTrim : public MainViewTrim
@@ -44,12 +52,22 @@ class MainViewHorizontalTrim : public MainViewTrim
   public:
     using MainViewTrim::MainViewTrim;
     MainViewHorizontalTrim(Window* parent, uint8_t idx);
-    void paint(BitmapBuffer * dc) override;
+
+  protected:
+    coord_t sx() override;
+    void drawLine(BitmapBuffer * dc) override;
+    void drawMarkerLines(BitmapBuffer * dc, coord_t x, coord_t y) override;
+    void drawValue(BitmapBuffer * dc) override;
 };
 
 class MainViewVerticalTrim : public MainViewTrim
 {
   public:
     MainViewVerticalTrim(Window* parent, uint8_t idx);
-    void paint(BitmapBuffer * dc) override;
+
+  protected:
+    coord_t sy() override;
+    void drawLine(BitmapBuffer * dc) override;
+    void drawMarkerLines(BitmapBuffer * dc, coord_t x, coord_t y) override;
+    void drawValue(BitmapBuffer * dc) override;
 };
