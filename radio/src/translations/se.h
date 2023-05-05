be4d1e4646d42b038f0a7cbbd7ab14e3beba5ee4
philmoz <philmoz@users.noreply.github.com>
2023-05-05 19:49:53 +1000

    fix: Debug key names for Boxer (#3536)

--- radio/src/translations/se.h
+++ radio/src/translations/se.h
@@ -253,7 +253,7 @@
   #define TR_VKEYS                     "Avbryt","Enter","PGUP","PGDN","SYS","MDL","TELE"
 #elif defined(RADIO_T8) || defined(RADIO_COMMANDO8)
   #define TR_VKEYS                     "RTN","ENTER","PGUP","PGDN","SYS","MDL","UPP","NER"
-#elif defined(RADIO_ZORRO)
+#elif defined(RADIO_ZORRO) || defined(RADIO_BOXER)
   #define TR_VKEYS                     "RTN","ENTER","PGUP","PGDN","SYS","MDL","TELE"
 #elif defined(PCBTARANIS)
   #define TR_VKEYS                     "Meny","Avbryt","Enter","Sida","Plus","Minus"
