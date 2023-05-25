6a8e197bbc12363ab1d28e875d42a2d18733f936
3djc <lesitewebdejc@hotmail.com>
2023-05-25 13:41:06 +0200

    XJT Lite and R9MP require usart

--- radio/src/gui/gui_common.cpp
+++ radio/src/gui/gui_common.cpp
@@ -889,6 +889,12 @@ bool isExternalModuleAvailable(int moduleType)
     return false;
 #endif
 
+#if defined(HARDWARE_EXTERNAL_MODULE_SIZE_SML) and !defined(EXTMODULE_USART_GPIO)
+  if (moduleType == MODULE_TYPE_XJT_LITE_PXX2 ||
+      moduleType == MODULE_TYPE_R9M_PXX2)
+    return false;
+#endif
+
 #if !defined(HARDWARE_EXTERNAL_MODULE_SIZE_STD)
   if (moduleType == MODULE_TYPE_R9M_PXX1 ||
       moduleType == MODULE_TYPE_R9M_PXX2 ||
