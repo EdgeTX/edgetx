50b8d26b5033bfd9c0afb1af3a7a9d93cbdba918
zyren <585668@qq.com>
2023-06-04 16:44:14 +0800

    Solve the messy display after F407 B&W Radios

--- radio/src/targets/taranis/board.cpp
+++ radio/src/targets/taranis/board.cpp
@@ -307,7 +307,7 @@ void boardOff()
   }
 #endif
 
-#if defined(RADIO_ZORRO) || defined(RADIO_TX12MK2) || defined(RADIO_BOXER)
+#if defined(MANUFACTURER_RADIOMASTER) && defined(STM32F407xx)
   lcdInit(); 
 #endif
 
