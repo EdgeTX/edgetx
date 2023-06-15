1399b3123b304121b554e6b6ce0bdd6d7ccb77ab
ulfhedlund <35467815+ulfhedlund@users.noreply.github.com>
2023-06-15 19:06:26 +0200

    Update se.h

--- radio/src/translations/se.h
+++ radio/src/translations/se.h
@@ -1256,7 +1256,7 @@
 #define TR_GREEN                        "Grön"
 #define TR_COLOR_PICKER                 "Färgväljare"
 #define TR_EDIT_THEME_DETAILS           "Ändra temainformation"
-#define TR_THEME_COLOR_DEFAULT          "FÖRVALD"
+#define TR_THEME_COLOR_DEFAULT          "DEFAULT"
 #define TR_THEME_COLOR_PRIMARY1         "PRIMÄR1"
 #define TR_THEME_COLOR_PRIMARY2         "PRIMÄR2"
 #define TR_THEME_COLOR_PRIMARY3         "PRIMÄR3"
@@ -1352,4 +1352,3 @@
 #define TR_ENABLED_FEATURES             "Aktiverade funktioner"
 #define TR_RADIO_MENU_TABS              "Radiomenyflikar"
 #define TR_MODEL_MENU_TABS              "Modellmenyflikar"
- 
