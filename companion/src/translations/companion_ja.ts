b27c5ed2ac7a2721e5fe212db165e4b8f9781292
Michael <101.37584@germanynet.de>
2023-07-08 10:48:05 +0200

    updated comanion and radio language JP

--- companion/src/translations/companion_ja.ts
+++ companion/src/translations/companion_ja.ts
@@ -2054,7 +2054,7 @@ Do you want to import settings from a file?</source>
     <message>
         <location filename="../firmwares/customfunctiondata.cpp" line="129"/>
         <source>Audio Amp Off</source>
-        <translation type="unfinished"></translation>
+        <translation>オーディオアンプ OFF</translation>
     </message>
     <message>
         <location filename="../firmwares/customfunctiondata.cpp" line="216"/>
