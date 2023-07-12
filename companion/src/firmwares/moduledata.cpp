3ad2767d6f6008326a29d3be33fbabbb73669674
elecpower <neilh713@tpg.com.au>
2023-07-13 08:37:20 +1000

    fix(cpn): limit available internal afhds protocols to those supported by hardware

--- companion/src/firmwares/moduledata.cpp
+++ companion/src/firmwares/moduledata.cpp
@@ -93,7 +93,7 @@ bool ModuleData::isAvailable(PulsesProtocol proto, int port)
           case PULSES_FLYSKY_AFHDS2A:
             return IS_FLYSKY_NV14(board);
           case PULSES_FLYSKY_AFHDS3:
-            return IS_FLYSKY_EL18(board);
+            //return IS_FLYSKY_EL18(board);
           default:
             return false;
         }
@@ -578,7 +578,9 @@ bool ModuleData::isProtocolAvailable(int moduleidx, unsigned int protocol, Gener
               case MODULE_TYPE_MULTIMODULE:
               case MODULE_TYPE_GHOST:
               case MODULE_TYPE_FLYSKY_AFHDS2A:
+                return IS_FLYSKY_NV14(board);
               case MODULE_TYPE_FLYSKY_AFHDS3:
+                //return IS_FLYSKY_EL18(board);
               case MODULE_TYPE_LEMON_DSMP:
                 return true;
               default:
