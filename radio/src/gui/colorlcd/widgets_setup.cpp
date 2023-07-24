6711cb96c2aee806eb814ed818b92cebb6b6bb1e
Phil Mitchell <phil.a.mitchell@gmail.com>
2023-07-24 14:36:47 +1000

    Ignore SYS & MDL buttons on widget setup page.

--- radio/src/gui/colorlcd/widgets_setup.cpp
+++ radio/src/gui/colorlcd/widgets_setup.cpp
@@ -138,7 +138,8 @@ void SetupWidgetsPage::deleteLater(bool detach, bool trash)
 void SetupWidgetsPage::onEvent(event_t event)
 {
 #if defined(HARDWARE_KEYS)
-  if (event == EVT_KEY_FIRST(KEY_PAGEUP) || event == EVT_KEY_FIRST(KEY_PAGEDN)) {
+  if (event == EVT_KEY_FIRST(KEY_PAGEUP) || event == EVT_KEY_FIRST(KEY_PAGEDN) ||
+      event == EVT_KEY_FIRST(KEY_SYS) || event == EVT_KEY_FIRST(KEY_MODEL)) {
     killEvents(event);
   } else if (event == EVT_KEY_FIRST(KEY_TELE)) {
     onCancel();
