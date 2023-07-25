6048fbee53b53d5f88cb88ba4ebe71b69b0c5229
philmoz <philmoz@users.noreply.github.com>
2023-07-25 15:22:16 +1000

    fix(color): When in screen settings, ignore SYS/MDL, treat TELE as RTN (#3845)
    * Change behaviour of TELE button on widget setup page to work like RTN button.
    Prevents recursive loading of widget setup page.
    
    * Ignore SYS & MDL buttons on widget setup page.

--- radio/src/gui/colorlcd/widgets_setup.cpp
+++ radio/src/gui/colorlcd/widgets_setup.cpp
@@ -137,9 +137,16 @@ void SetupWidgetsPage::deleteLater(bool detach, bool trash)
 
 void SetupWidgetsPage::onEvent(event_t event)
 {
-  if (event == EVT_KEY_FIRST(KEY_PGUP) || event == EVT_KEY_FIRST(KEY_PGDN)) {
+#if defined(HARDWARE_KEYS)
+  if (event == EVT_KEY_FIRST(KEY_PGUP) || event == EVT_KEY_FIRST(KEY_PGDN) ||
+      event == EVT_KEY_FIRST(KEY_SYS) || event == EVT_KEY_FIRST(KEY_MODEL)) {
     killEvents(event);
+  } else if (event == EVT_KEY_FIRST(KEY_TELE)) {
+    onCancel();
   } else {
     FormWindow::onEvent(event);
   }
+#else
+  FormWindow::onEvent(event);
+#endif
 }
