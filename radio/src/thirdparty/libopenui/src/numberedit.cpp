0f3828ebd2745730056e8ea5fd2d59c74f94f303
Michael <101.37584@germanynet.de>
2023-07-31 14:06:49 +0200

    change direction for rotary events and trims used as keys

--- radio/src/thirdparty/libopenui/src/numberedit.cpp
+++ radio/src/thirdparty/libopenui/src/numberedit.cpp
@@ -66,7 +66,11 @@ void NumberEdit::onEvent(event_t event)
         auto step = getStep();
         step += (rotaryEncoderGetAccel() * getAccelFactor()) / 8;
         do {
+#if defined(USE_TRIMS_AS_BUTTONS)
+          value -= step;
+#else
           value += step;
+#endif
         } while (isValueAvailable && !isValueAvailable(value) && value <= vmax);
         if (value <= vmax) {
           setValue(value);
@@ -83,7 +87,11 @@ void NumberEdit::onEvent(event_t event)
         auto step = getStep();
         step += (rotaryEncoderGetAccel() * getAccelFactor()) / 8;
         do {
+#if defined(USE_TRIMS_AS_BUTTONS)
+          value += step;
+#else
           value -= step;
+#endif
         } while (isValueAvailable && !isValueAvailable(value) && value >= vmin);
         if (value >= vmin) {
           setValue(value);
