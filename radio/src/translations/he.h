3f10a20a6eae499b8aec0ae5cd5a9e01a58393fe
Michael <101.37584@germanynet.de>
2023-08-07 07:42:33 +0200

    updated HE language translations

--- radio/src/translations/he.h
+++ radio/src/translations/he.h
@@ -1179,7 +1179,7 @@
 #define TR_MENU_LUA                    STR_CHAR_LUA "Lua סקריפטים"
 #define TR_MENU_STICKS                 STR_CHAR_STICK "סטיקים"
 #define TR_MENU_POTS                   STR_CHAR_POT "גלגלות"
-#define TR_MENU_MIN                    STR_CHAR_FUNCTION "MIN"
+#define TR_MENU_MIN                    STR_CHAR_FUNCTION "מינימום"
 #define TR_MENU_MAX                    STR_CHAR_FUNCTION "מקסימום"
 #define TR_MENU_HELI                   STR_CHAR_CYC "Cyclic"
 #define TR_MENU_TRIMS                  STR_CHAR_TRIM "קיזוזים"
