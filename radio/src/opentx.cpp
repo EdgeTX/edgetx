8cb47132a9e84d949190f0b1c5f8bf61412fd829
3djc <lesitewebdejc@hotmail.com>
2023-08-11 11:12:09 +0200

    fix: EM when LUA script is running and radio is turned off

--- radio/src/opentx.cpp
+++ radio/src/opentx.cpp
@@ -1259,6 +1259,10 @@ void opentxClose(uint8_t shutdown)
     pulsesStop();
     AUDIO_BYE();
     // TODO needed? telemetryEnd();
+#if defined(LUA)
+    luaClose(&lsScripts);
+#endif
+
 #if defined(HAPTIC)
     hapticOff();
 #endif
@@ -1301,11 +1305,6 @@ void opentxClose(uint8_t shutdown)
   luaClose(&lsWidgets);
   lsWidgets = 0;
 #endif
-#endif
-#if defined(LUA)
-  // the script context needs to be closed *after*
-  // the widgets, as it has been the first to be opened
-  luaClose(&lsScripts);
 #endif
 
 #if defined(SDCARD)
