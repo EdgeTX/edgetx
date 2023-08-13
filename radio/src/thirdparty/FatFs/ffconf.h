30294046336c8cc169874dfadf3c946553348bd5
raphaelcoeffic <raphael.coeffic@frafos.com>
2023-08-13 22:47:23 +0200

    chore: disable f_mkfs() in ffconf.h

--- radio/src/thirdparty/FatFs/ffconf.h
+++ radio/src/thirdparty/FatFs/ffconf.h
@@ -44,12 +44,12 @@
 /  2: Enable with LF-CRLF conversion. */
 
 
-#define FF_USE_FIND		0
+#define FF_USE_FIND	0
 /* This option switches filtered directory read functions, f_findfirst() and
 /  f_findnext(). (0:Disable, 1:Enable 2:Enable with matching altname[] too) */
 
 
-#define FF_USE_MKFS		1
+#define FF_USE_MKFS	0
 /* This option switches f_mkfs() function. (0:Disable or 1:Enable) */
 
 
