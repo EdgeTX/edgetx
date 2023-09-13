3b416bdb5388ea668f0fab5271b9d21e0fbdc5e1
kobakirill <39222879+kobakirill@users.noreply.github.com>
2023-09-13 14:25:16 +0400

    Update fonts.cpp

--- radio/src/gui/common/stdlcd/fonts.cpp
+++ radio/src/gui/common/stdlcd/fonts.cpp
@@ -43,7 +43,7 @@ const unsigned char font_5x7[]  = {
 #elif defined(TRANSLATIONS_PT)
 #include "font_pt_05x07.lbm"
 #elif defined(TRANSLATIONS_RU)
-#include "font_se_05x07.lbm"
+#include "font_ru_05x07.lbm"
 #elif defined(TRANSLATIONS_SE)
 #include "font_se_05x07.lbm"
 #endif
@@ -75,7 +75,7 @@ const unsigned char font_10x14[]  = {
   #elif defined(TRANSLATIONS_PT)
   #include "font_pt_10x14.lbm"
   #elif defined(TRANSLATIONS_RU)
-  #include "font_se_10x14.lbm"
+  #include "font_ru_10x14.lbm"
   #elif defined(TRANSLATIONS_SE)
   #include "font_se_10x14.lbm"
   #endif
@@ -106,7 +106,7 @@ const unsigned char font_4x6[]  = {
 #elif defined(TRANSLATIONS_PT)
 #include "font_pt_04x06.lbm"
 #elif defined(TRANSLATIONS_RU)
-#include "font_se_04x06.lbm"
+#include "font_ru_04x06.lbm"
 #elif defined(TRANSLATIONS_SE)
 #include "font_se_04x06.lbm"
 #endif
@@ -133,7 +133,7 @@ const unsigned char font_8x10[]  = {
 #elif defined(TRANSLATIONS_PT)
 #include "font_pt_08x10.lbm"
 #elif defined(TRANSLATIONS_RU)
-#include "font_se_08x10.lbm"
+#include "font_ru_08x10.lbm"
 #elif defined(TRANSLATIONS_SE)
 #include "font_se_08x10.lbm"
 #endif
