0e2ca77f51baf279629c6b114fa0953e7ac5048c
philmoz <phil.a.mitchell@gmail.com>
2023-09-19 08:51:18 +1000

    Don't overwrite NV14/EL18 dead zone when loading radio settings.

--- radio/src/storage/storage_common.cpp
+++ radio/src/storage/storage_common.cpp
@@ -100,18 +100,6 @@ void postRadioSettingsLoad()
     g_eeGeneral.internalModule = DEFAULT_INTERNAL_MODULE;
   }
 #endif
-#if defined(STICK_DEAD_ZONE)
-  if (!g_eeGeneral.stickDeadZone) {
-    g_eeGeneral.stickDeadZone = DEFAULT_STICK_DEADZONE;
-  }
-#endif
-#if !defined(DEBUG)
-  // clean up leftovers from a previous DEBUG config
-  for (uint8_t port_nr = 0; port_nr < MAX_AUX_SERIAL; port_nr++) {
-    if (serialGetMode(port_nr) == UART_MODE_DEBUG)
-      serialSetMode(port_nr, UART_MODE_NONE);
-  }
-#endif
 }
 
 static bool sortMixerLines()
