07965986bdd85279aee9bbad54b87b6def542f28
Frankie Arzu <32604366+frankiearzu@users.noreply.github.com>
2023-09-21 17:45:25 -0500

    Fixes #4062
    Update code to populate single sensor GPS with Lat/Long together

--- radio/src/telemetry/flysky_ibus.cpp
+++ radio/src/telemetry/flysky_ibus.cpp
@@ -98,11 +98,11 @@ enum
   AFHDS2A_ID_ODO2 = 0x7D,               // Odometer2
   AFHDS2A_ID_SPE = 0x7E,                // Speed 2 bytes km/h
 
+   
   AFHDS2A_ID_GPS_LAT = 0x80,            // 4bytes signed WGS84 in degrees * 1E7
   AFHDS2A_ID_GPS_LON = 0x81,            // 4bytes signed WGS84 in degrees * 1E7
   AFHDS2A_ID_GPS_ALT = 0x82,            // 4bytes signed!!! GPS alt m*100
-
-  AFHDS2A_ID_ALT = 0x83,                // 4bytes signed!!! Alt m*100
+  AFHDS2A_ID_ALT     = 0x83,            // 4bytes signed!!! Alt m*100
 
   AFHDS2A_ID_RX_SIG_AFHDS3  = 0xF7,    // SIG
   AFHDS2A_ID_RX_SNR_AFHDS3  = 0xF8,    // SNR
@@ -189,8 +189,8 @@ const FlySkySensor flySkySensors[] = {
   FS( AFHDS2A_ID_ODO1,                  STR_SENSOR_ODO1,          UNIT_METERS,            2 ),  // 2 bytes Odometer1 -- some magic with 330 needed
   FS( AFHDS2A_ID_ODO2,                  STR_SENSOR_ODO2,          UNIT_METERS,            2 ),  // 2 bytes Odometer2 -- some magic with 330 needed
   FS( AFHDS2A_ID_SPE,                   STR_SENSOR_ASPD,          UNIT_KMH,               2 ),  // 2 bytes Speed km/h -- some magic with 330 needed
-  FS( AFHDS2A_ID_GPS_LAT,               STR_SENSOR_GPS,           UNIT_RAW,               0 ),  // 4 bytes signed WGS84 in degrees * 1E7
-  FS( AFHDS2A_ID_GPS_LON,               STR_SENSOR_GPS,           UNIT_RAW,               0 ),  // 4 bytes signed WGS84 in degrees * 1E7
+  FS( AFHDS2A_ID_GPS_LAT,               STR_SENSOR_GPS,           UNIT_GPS,               0 ),  // 4 bytes signed WGS84 in degrees * 1E7
+//FS( AFHDS2A_ID_GPS_LON,               STR_SENSOR_GPS,           UNIT_GPS,               0 ),  // 4 bytes signed WGS84 in degrees * 1E7
   FS( AFHDS2A_ID_GPS_ALT,               STR_SENSOR_GPSALT,        UNIT_METERS,            2 ),  // 4 bytes signed GPS alt m*100
   FS( AFHDS2A_ID_ALT,                   STR_SENSOR_ALT,           UNIT_METERS,            2 ),  // 4 bytes signed Alt m*100
   FS( AFHDS2A_ID_RX_SIG_AFHDS3,         STR_SENSOR_RX_QUALITY,    UNIT_PERCENT,           0 ),  // RX error rate
@@ -347,6 +347,7 @@ void processFlySkySensor(const uint8_t * packet, uint8_t type)
   else if (id == AFHDS2A_ID_GPS_FULL) {
     //(AC FRAME)[ID][inst][size][fix][sats][LAT]x4[LON]x4[ALT]x4
     setTelemetryValue(PROTOCOL_TELEMETRY_FLYSKY_IBUS, AFHDS2A_ID_GPS_STATUS, 0, instance, packet[4], UNIT_RAW, 0);
+
     for (uint8_t sensorID = AFHDS2A_ID_GPS_LAT; sensorID <= AFHDS2A_ID_GPS_ALT; sensorID++) {
       int index = 5 + (sensorID - AFHDS2A_ID_GPS_LAT) * 4;
       buffer[0] = sensorID;
@@ -355,7 +356,20 @@ void processFlySkySensor(const uint8_t * packet, uint8_t type)
       memcpy(buffer + 3, packet + index, 4);
       processFlySkySensor(buffer, 0xAC);
     }
+
     return;
+  } 
+  else if (id == AFHDS2A_ID_GPS_LAT) {
+    uint8_t instance2 = 0; // forced to be exactly the same instance. don't think that an RX will have 2 GPSs 
+    value = value / 10;  
+    setTelemetryValue(PROTOCOL_TELEMETRY_FLYSKY_IBUS, AFHDS2A_ID_GPS_LAT, 0, instance2, value, UNIT_GPS_LATITUDE, 0);
+    return;   
+  }
+  else if (id == AFHDS2A_ID_GPS_LON) { // Remapped to single GPS sensor: AFHDS2A_ID_GPS_LAT
+    uint8_t instance2 = 0;
+    value= value / 10;  
+    setTelemetryValue(PROTOCOL_TELEMETRY_FLYSKY_IBUS, AFHDS2A_ID_GPS_LAT, 0, instance2, value, UNIT_GPS_LONGITUDE, 0);
+    return; 
   }
   else if (id == AFHDS2A_ID_VOLT_FULL) {
     //(AC FRAME)[ID][inst][size][ACC_X]x2[ACC_Y]x2[ACC_Z]x2[ROLL]x2[PITCH]x2[YAW]x2
