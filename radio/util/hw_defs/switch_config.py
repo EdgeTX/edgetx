206180222ad8cc551813ac846cc8c282f4a1f364
Peter Feerick <peter.feerick@gmail.com>
2023-09-23 14:21:00 +1000

    fix: Use side-by-side layout, like x9e This is also how it was displayed in 2.9 and earlier

--- radio/util/hw_defs/switch_config.py
+++ radio/util/hw_defs/switch_config.py
@@ -186,10 +186,10 @@ SWITCH_CONFIG = {
         "SE": {"default": "3POS",   "display": [0, 2]},
         "SF": {"default": "2POS",   "display": [0, 3]},
         # right side
-        "SC": {"default": "3POS",   "display": [1, 0]},
-        "SD": {"default": "3POS",   "display": [1, 1]},
-        "SG": {"default": "3POS",   "display": [1, 2]},
-        "SH": {"default": "TOGGLE", "display": [1, 3]}
+        "SC": {"default": "3POS",   "display": [0, 4]},
+        "SD": {"default": "3POS",   "display": [0, 5]},
+        "SG": {"default": "3POS",   "display": [0, 6]},
+        "SH": {"default": "TOGGLE", "display": [0, 7]}
     },
     "x9d+": {
         # left side
@@ -198,10 +198,10 @@ SWITCH_CONFIG = {
         "SE": {"default": "3POS",   "display": [0, 2]},
         "SF": {"default": "2POS",   "display": [0, 3]},
         # right side
-        "SC": {"default": "3POS",   "display": [1, 0]},
-        "SD": {"default": "3POS",   "display": [1, 1]},
-        "SG": {"default": "3POS",   "display": [1, 2]},
-        "SH": {"default": "TOGGLE", "display": [1, 3]}
+        "SC": {"default": "3POS",   "display": [0, 4]},
+        "SD": {"default": "3POS",   "display": [0, 5]},
+        "SG": {"default": "3POS",   "display": [0, 6]},
+        "SH": {"default": "TOGGLE", "display": [0, 7]}
     },
     "x9d+2019": {
         # left side
@@ -211,10 +211,10 @@ SWITCH_CONFIG = {
         "SF": {"default": "2POS",   "display": [0, 3]},
         "SI": {"default": "TOGGLE", "display": [0, 4]},
         # right side
-        "SC": {"default": "3POS",   "display": [1, 0]},
-        "SD": {"default": "3POS",   "display": [1, 1]},
-        "SG": {"default": "3POS",   "display": [1, 2]},
-        "SH": {"default": "TOGGLE", "display": [1, 3]}
+        "SC": {"default": "3POS",   "display": [0, 5]},
+        "SD": {"default": "3POS",   "display": [0, 6]},
+        "SG": {"default": "3POS",   "display": [0, 7]},
+        "SH": {"default": "TOGGLE", "display": [0, 8]}
     },
     "x9e": {
         # left side
