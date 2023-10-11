1d75b16dcffb1b6534392392c9ea7e6fdbf0b261
3djc <lesitewebdejc@hotmail.com>
2023-10-11 09:36:55 +0200

    fix: cosmetics

--- radio/util/hw_defs/switch_config.py
+++ radio/util/hw_defs/switch_config.py
@@ -116,12 +116,12 @@ SWITCH_CONFIG = {
         "SA": {"default": "3POS",   "display": [0, 0]},
         "SC": {"default": "TOGGLE", "display": [0, 1]},
         "SE": {"default": "2POS",   "display": [0, 2]},
-        "SG": {"default": "NONE", "display": [0, 3]},
+        "SG": {"default": "NONE",   "display": [0, 3]},
         # right side
         "SB": {"default": "3POS",   "display": [1, 0]},
         "SD": {"default": "TOGGLE", "display": [1, 1]},
         "SF": {"default": "2POS",   "display": [1, 2]},
-        "SH": {"default": "NONE", "display": [1, 3]},
+        "SH": {"default": "NONE",   "display": [1, 3]},
     },
     "t20": {
         # left side
