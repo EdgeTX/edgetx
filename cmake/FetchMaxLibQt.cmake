05f87232c3d1b50874e3a4927ed3a001cac0dbb6
elecpower <neilh713@tpg.com.au>
2023-10-26 23:08:03 +1100

    Use cmake Fetch to retrieve maxLibQt repo and modify CMakeLists.txt as required

---
+++ cmake/FetchMaxLibQt.cmake
@@ -0,0 +1,17 @@
+# Fetch maxLibQt source code from Github
+
+include(FetchContent)
+
+FetchContent_Declare(
+  maxLibQt
+  GIT_REPOSITORY https://github.com/edgetx/maxLibQt
+  GIT_TAG        61ef4edd # PR#2
+)
+
+FetchContent_MakeAvailable(maxLibQt)
+
+message("Fetched maxLibQt source code from Github: ${maxLibQt_SOURCE_DIR}")
+include_directories(
+  ${maxLibQt_SOURCE_DIR}
+  ${maxLibQt_BINARY_DIR}
+)
