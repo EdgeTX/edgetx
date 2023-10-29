c15570eb4ce9ce4a51137e37d94452e8ad306164
Peter Feerick <peter.feerick@gmail.com>
2023-10-29 13:15:57 +1000

    fix: Erronous quote

--- tools/commit-tests.sh
+++ tools/commit-tests.sh
@@ -152,7 +152,7 @@ do
             BUILD_OPTIONS+="-DPCB=PL18"
             ;;
         pl18ev)
-            BUILD_OPTIONS+="-DPCB=PL18" -DPCBREV=PL18EV"
+            BUILD_OPTIONS+="-DPCB=PL18 -DPCBREV=PL18EV"
             ;;
         commando8)
             BUILD_OPTIONS+="-DPCB=X7 -DPCBREV=COMMANDO8"
