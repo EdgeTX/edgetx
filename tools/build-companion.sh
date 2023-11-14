6e27f1875dcbedfb3af1d3ce34d4adaf3eac9e7e
Richard Li <richard.li@ces.hk>
2023-11-14 15:39:31 +0800

    Added companion build for pl18ev.

--- tools/build-companion.sh
+++ tools/build-companion.sh
@@ -70,7 +70,7 @@ declare -a simulator_plugins=(x9lite x9lites
                               tlite tpro lr3pro
                               x9d x9dp x9dp2019 x9e
                               xlite xlites
-                              nv14 pl18
+                              nv14 pl18 pl18ev
                               x10 x10-access x12s
                               t16 t18 tx16s)
 
@@ -172,6 +172,9 @@ do
             ;;
         pl18)
             BUILD_OPTIONS+="-DPCB=PL18"
+            ;;
+        pl18ev)
+            BUILD_OPTIONS+="-DPCB=PL18 -DPCBREV=PL18EV"
             ;;
         *)
             echo "Unknown target: $target_name"
