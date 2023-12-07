99c4cdb3f18b589723048ad6d81af56a38035ef5
wimalopaan <wilhelm.wm.meier@googlemail.com>
2023-12-07 15:31:43 +0100

    option ALTDATA default off simplified data handling

--- radio/src/altdata.h
+++ radio/src/altdata.h
@@ -11,24 +11,10 @@
 #if defined(ALTDATA) 
 
 namespace AlternateData {
-    template<uint8_t B>
-    struct State {
-        using value_t = uint8_t;
-        static constexpr uint8_t mask = (1 << B) - 1;
-        void operator=(const value_t v) {
-            mValue = (v & mask);    
-        }
-        uint8_t toInt() const {
-            return mValue;
-        }
-    private:
-        value_t mValue{};
-    };  
     
-    template<uint8_t N, typename S>
+    template<uint8_t N, typename S = uint8_t>
     struct Container {
         using value_t = S;
-        using chunk_t = std::array<S, 4>;
         constexpr uint8_t size() const {
             return N;
         }
@@ -41,10 +27,10 @@ namespace AlternateData {
             return mData[item];
         }
         template<typename F>
-        uint8_t pushNextData(const uint8_t numberOfValuesToPush, const F f) {
+        uint8_t pushNextChunk(const uint8_t numberOfValuesToPush, const F f) {
             const uint8_t start = mNextIndex;
             for(uint8_t i = 0; i < numberOfValuesToPush; ++i) {
-                const uint8_t v = mData[mNextIndex].toInt();
+                const uint8_t v = mData[mNextIndex];
                 TRACE("pushNextData: %d = %d", mNextIndex, v);
                 f(v);
                 ++mNextIndex;
@@ -59,10 +45,7 @@ namespace AlternateData {
         std::array<S, N> mData{};
     };
     
-    using state_t = State<2>;
-    using container_t = Container<64, state_t>;
-    using chunk_t = container_t::chunk_t;
-    
+    using container_t = Container<64>;
     extern container_t container;
 }
 
