39f8f197a206fd5256f067cbc6d7ac46a37eab17
Peter Feerick <pfeerick@users.noreply.github.com>
2024-01-16 06:12:18 +0000

    fix: Reconfigure as TX16S again

--- tools/build-companion.sh
+++ tools/build-companion.sh
@@ -192,6 +192,8 @@ do
 done                              
 
 rm -f CMakeCache.txt native/CMakeCache.txt
+cmake ${COMMON_OPTIONS} -DPCB=X10 -DPCBREV=TX16S "${SRCDIR}"
+
 cmake --build . --target native-configure
 if [ "$(uname)" = "Darwin" ]; then
     cmake --build native -j"${JOBS}" --target package
