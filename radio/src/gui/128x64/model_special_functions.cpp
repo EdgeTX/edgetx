bfefa1f09f5d66c72d5ee4b43d832f535b0cd0c0
3djc <3djc@gh.com>
2024-02-08 11:09:19 +0100

    fix: load LUA SF after selection

--- radio/src/gui/128x64/model_special_functions.cpp
+++ radio/src/gui/128x64/model_special_functions.cpp
@@ -76,6 +76,9 @@ void onCustomFunctionsFileSelectionMenu(const char * result)
     // The user choosed a file in the list
     memcpy(cfn->play.name, result, sizeof(cfn->play.name));
     storageDirty(eeFlags);
+    if (func == FUNC_PLAY_SCRIPT || func == FUNC_RGB_LED) {
+      LUA_LOAD_MODEL_SCRIPTS();
+    }
   }
 }
 #endif // SDCARD
@@ -473,7 +476,12 @@ void menuSpecialFunctions(event_t event, CustomFunctionData * functions, CustomF
         
         case 5:
             drawCheckBox(MODEL_SPECIAL_FUNC_5TH_COLUMN_ONOFF, y, CFN_ACTIVE(cfn), attr);
-            if (active) CFN_ACTIVE(cfn) = checkIncDec(event, CFN_ACTIVE(cfn), 0, 1, eeFlags);      
+            if (active) {
+              CFN_ACTIVE(cfn) = checkIncDec(event, CFN_ACTIVE(cfn), 0, 1, eeFlags);
+              if (checkIncDec_Ret && (func == FUNC_PLAY_SCRIPT || func == FUNC_RGB_LED)) {
+                LUA_LOAD_MODEL_SCRIPTS();
+              }
+            }
             break;
       }
     }
