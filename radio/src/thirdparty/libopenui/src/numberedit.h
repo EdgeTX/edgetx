7c8c41313ac562399ad4aeb18c735509da6367cd
Frankie Arzu <32604366+frankiearzu@users.noreply.github.com>
2024-02-10 20:20:20 -0600

    fix(color): EditNumber class not implementing zeroText logic (#4611)

--- radio/src/thirdparty/libopenui/src/numberedit.h
+++ radio/src/thirdparty/libopenui/src/numberedit.h
@@ -118,6 +118,8 @@ class NumberEdit: public FormField
         std::string str;
         if (displayFunction != nullptr) {
           str = displayFunction(currentValue);
+        } else if (!zeroText.empty() && currentValue==0) {
+          str = zeroText;
         } else {
           str = formatNumberAsString(currentValue, textFlags, 0, prefix.c_str(),
                                      suffix.c_str());
@@ -176,6 +178,8 @@ class NumberEdit: public FormField
         std::string str;
         if (displayFunction != nullptr) {
           str = displayFunction(currentValue);
+        } else if (!zeroText.empty() && currentValue==0) {
+          str = zeroText;
         } else {
           str = formatNumberAsString(currentValue, textFlags, 0, prefix.c_str(),
                                      suffix.c_str());
