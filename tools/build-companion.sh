b11a06f65ae1cda79fa26dddb0144a4b5bda7cbd
Peter Feerick <peter.feerick@gmail.com>
2024-03-09 10:10:17 +0000

    chore: add simulator

--- tools/build-companion.sh
+++ tools/build-companion.sh
@@ -67,7 +67,7 @@ declare -a simulator_plugins=(x9lite x9lites
                               x7 x7-access
                               t8 t12 tx12 tx12mk2
                               zorro commando8 boxer pocket
-                              tlite tpro tprov2 lr3pro
+                              tlite tpro tprov2 lr3pro t14
                               x9d x9dp x9dp2019 x9e
                               xlite xlites
                               nv14 el18 pl18 pl18ev
