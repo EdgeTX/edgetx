70d9fe2e0df5e22a97bb2d447ccf2538c87e915b
philmoz <phil.a.mitchell@gmail.com>
2024-03-18 21:49:19 +1100

    FR translation

--- radio/src/translations/fr.h
+++ radio/src/translations/fr.h
@@ -1304,4 +1304,4 @@
 
 #define TR_MIX_SLOW_PREC          TR("Slow prec", "Slow up/dn prec")
 
-#define TR_THEME_EXISTS           "A theme directory with the same name already exists."
+#define TR_THEME_EXISTS           "Un thème avec le même nom existe déjà."
