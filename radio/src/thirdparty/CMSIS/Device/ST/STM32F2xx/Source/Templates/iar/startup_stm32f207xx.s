ec208e33870c1bc4a5d7eda08e94cfb673597dbe
ThomasKuehne <2562574+ThomasKuehne@users.noreply.github.com>
2024-04-05 05:08:10 +0000

    chore(radio) update CMSIS for F2 from V2.2.5 to V2.2.6

--- radio/src/thirdparty/CMSIS/Device/ST/STM32F2xx/Source/Templates/iar/startup_stm32f207xx.s
+++ radio/src/thirdparty/CMSIS/Device/ST/STM32F2xx/Source/Templates/iar/startup_stm32f207xx.s
@@ -11,17 +11,15 @@
 ;*                        calls main()).
 ;*                      After Reset the Cortex-M3 processor is in Thread mode,
 ;*                      priority is Privileged, and the Stack is set to Main.
-;*******************************************************************************
-;* @attention
+;********************************************************************************
 ;*
-;* <h2><center>&copy; Copyright (c) 2017 STMicroelectronics.
-;* All rights reserved.</center></h2>
+;* Copyright (c) 2017-2021 STMicroelectronics.
+;* All rights reserved.
 ;*
-;* This software component is licensed by ST under BSD 3-Clause license,
-;* the "License"; You may not use this file except in compliance with the
-;* License. You may obtain a copy of the License at:
-;*                        opensource.org/licenses/BSD-3-Clause
-;*
+;* This software is licensed under terms that can be found in the LICENSE file
+;* in the root directory of this software component.
+;* If no LICENSE file comes with this software, it is provided AS-IS.
+;
 ;*******************************************************************************
 ;
 ;
@@ -615,4 +613,4 @@ HASH_RNG_IRQHandler
 
 
         END
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+
