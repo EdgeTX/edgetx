60119ae4fae7f2ef17fa5b965fc2a72ebe8c72c2
ThomasKuehne <2562574+ThomasKuehne@users.noreply.github.com>
2024-04-14 23:32:57 +0000

    chore(radio): cleanup -Wundef compiler warnings (#4853)

--- radio/src/dataconstants.h
+++ radio/src/dataconstants.h
@@ -125,6 +125,7 @@ enum CurveType {
   #define LEN_MODEL_NAME               10
   #define LEN_TIMER_NAME               3
   #define LEN_FLIGHT_MODE_NAME         6
+  #define LEN_BITMAP_NAME              0
   #define LEN_EXPOMIX_NAME             6
   #define LEN_CHANNEL_NAME             4
   #define LEN_INPUT_NAME               3
