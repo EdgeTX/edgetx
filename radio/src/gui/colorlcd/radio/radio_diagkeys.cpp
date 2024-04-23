51a036b5e1fb59216ccff40990b3afc6712bee3d
Richard Li <richard.li@ces.hk>
2024-04-23 14:59:11 +0800

    Switch driver, key/trim driver

--- radio/src/gui/colorlcd/radio/radio_diagkeys.cpp
+++ radio/src/gui/colorlcd/radio/radio_diagkeys.cpp
@@ -25,7 +25,9 @@
 #include "libopenui.h"
 #include "edgetx.h"
 
-#if defined(PCBPL18)
+#if defined(RADIO_NB4P)
+static const uint8_t _trimMap[MAX_TRIMS * 2] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+#elif defined(PCBPL18)
 static const uint8_t _trimMap[MAX_TRIMS * 2] = {8, 9, 10, 11, 12, 13, 14, 15,
                                                 2, 3, 4,  5,  0,  1,  6,  7};
 #else
