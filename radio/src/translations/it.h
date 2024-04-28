cba7b4a146bdff03c4f414dbab90eb5a6a9f0658
philmoz <phil.a.mitchell@gmail.com>
2024-04-28 19:57:34 +1000

    IT translation.

--- radio/src/translations/it.h
+++ radio/src/translations/it.h
@@ -1284,8 +1284,8 @@
 
 #define TR_THEME_EXISTS           "Esiste già una cartella di temi con lo stesso nome."
 
-#define TR_DATE_TIME_WIDGET       "Date & Time"
-#define TR_RADIO_INFO_WIDGET      "Radio Info"
-#define TR_LOW_BATT_COLOR         "Low battery"
-#define TR_MID_BATT_COLOR         "Mid battery"
-#define TR_HIGH_BATT_COLOR        "High battery"
+#define TR_DATE_TIME_WIDGET       "Data & Ora"
+#define TR_RADIO_INFO_WIDGET      "Radio Info" 
+#define TR_LOW_BATT_COLOR         "Batteria scarica"
+#define TR_MID_BATT_COLOR         "Batteria media"
+#define TR_HIGH_BATT_COLOR        "Batteria carica"
