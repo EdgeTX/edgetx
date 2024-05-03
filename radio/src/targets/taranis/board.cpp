e0aca6433ec7dc949491c0479403c0f1b85fe1ea
Peter Feerick <pfeerick@users.noreply.github.com>
2024-05-04 08:31:57 +1000

    fix(debug): don't initalise SP_AUX1 if not exist (#4962)

--- radio/src/targets/taranis/board.cpp
+++ radio/src/targets/taranis/board.cpp
@@ -215,7 +215,7 @@ void boardInit()
   ws2812_update(&_led_timer);
 #endif
 
-#if defined(DEBUG)
+#if defined(DEBUG) && defined(AUX_SERIAL)
   serialSetMode(SP_AUX1, UART_MODE_DEBUG);                // indicate AUX1 is used
   serialInit(SP_AUX1, UART_MODE_DEBUG);                   // early AUX1 init
 #endif
