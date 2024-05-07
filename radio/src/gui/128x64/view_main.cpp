8a9d095cb09e8929a5f1586184d938af54b7d2af
philmoz <philmoz@users.noreply.github.com>
2024-05-07 16:01:07 +1000

    fix(bw): adjust 128x64 layout to accommodate long switch names (#4976)

--- radio/src/gui/128x64/view_main.cpp
+++ radio/src/gui/128x64/view_main.cpp
@@ -597,13 +597,14 @@ void menuMainView(event_t event)
                                 5 * FH + 1, 4, i);
               }
               else {
-                coord_t x = switch_display.col == 0 ? 2 * FW - 2 : 16 * FW + 7;
+                coord_t x = switch_display.col == 0 ? 3 * FW + 3 : 18 * FW + 1;
                 coord_t y = 33 + switch_display.row * FH;
                 getvalue_t val = getValue(MIXSRC_FIRST_SWITCH + i);
+                if (val == 0) x -= 1;
                 getvalue_t sw =
                     ((val < 0) ? 3 * i + 1
                                : ((val == 0) ? 3 * i + 2 : 3 * i + 3));
-                drawSwitch(x, y, sw, 0, false);
+                drawSwitch(x, y, sw, CENTERED, false);
               }
             }
           }
