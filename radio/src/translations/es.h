12031c44c2cbcc019c38022f535107d76748c520
philmoz <philmoz@users.noreply.github.com>
2024-06-17 08:50:50 +1000

    fix(bootloader): use ASCII only strings for NV14/EL18 (#5178)

--- radio/src/translations/es.h
+++ radio/src/translations/es.h
@@ -1068,6 +1068,8 @@
   #define TR_BL_SELECT_KEY            "[R TRIM] to select file"
   #define TR_BL_FLASH_KEY             "Hold [R TRIM] long to flash"
   #define TR_BL_EXIT_KEY              " [L TRIM] to exit"
+  #define TR_BL_ENABLE                "Enable"
+  #define TR_BL_DISABLE               "Disable"
 #elif defined(PCBPL18)
    // Bootloader PL18 specific - Ascii only
   #define TR_BL_RF_USB_ACCESS         "RF USB access"
