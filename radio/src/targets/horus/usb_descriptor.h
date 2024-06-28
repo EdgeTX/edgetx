ae721d19766b53c981fcbe23326bbb95199254d2
Peter Feerick <peter.feerick@gmail.com>
2024-06-28 06:08:39 +0000

    chore: use full name for USB descriptor

--- radio/src/targets/horus/usb_descriptor.h
+++ radio/src/targets/horus/usb_descriptor.h
@@ -43,7 +43,7 @@
   #define USB_MANUFACTURER             'R', 'M', '_', 'T', 'X', ' ', ' ', ' '  /* 8 bytes */
   #define USB_PRODUCT                  'R', 'M', ' ', 'T', 'X', '1', '6', 'S'  /* 8 Bytes */
 #elif defined(RADIO_F16)
-  #define USB_NAME                     "FF F16"
+  #define USB_NAME                     "Fatfish F16"
   #define USB_MANUFACTURER             'F', 'A', 'T', 'F', 'I', 'S', 'H', ' '  /* 8 bytes */
   #define USB_PRODUCT                  'F', 'F', ' ', 'F', '1', '6', ' ', ' '  /* 8 Bytes */
 #elif defined(PCBX10)
