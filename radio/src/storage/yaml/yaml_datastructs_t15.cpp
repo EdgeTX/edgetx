23166630bab7df806ddc2d309939b38780683a66
Peter Feerick <peter.feerick@gmail.com>
2024-06-30 09:48:07 +0000

    chore: generate yaml

--- radio/src/storage/yaml/yaml_datastructs_t15.cpp
+++ radio/src/storage/yaml/yaml_datastructs_t15.cpp
@@ -86,6 +86,7 @@ const struct YamlIdStr enum_Functions[] = {
   {  FUNC_SET_SCREEN, "SET_SCREEN"  },
   {  FUNC_DISABLE_AUDIO_AMP, "DISABLE_AUDIO_AMP"  },
   {  FUNC_RGB_LED, "RGB_LED"  },
+  {  FUNC_LCD_TO_VIDEO, "LCD_TO_VIDEO"  },
   {  FUNC_TEST, "TEST"  },
   {  0, NULL  }
 };
