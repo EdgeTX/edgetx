f0db91886cf0662d55691adc453d9ed8c6d5fbc8
Peter Feerick <peter.feerick@gmail.com>
2024-07-01 06:40:35 +0000

    fix: missing translation strings

--- radio/src/translations/da.h
+++ radio/src/translations/da.h
@@ -121,6 +121,7 @@
 #define TR_SF_DISABLE_TOUCH            "Ikke berøringsaktiv"
 #define TR_SF_DISABLE_AUDIO_AMP        TR("Sluk audio amp", "Sluk audio amplifier")
 #define TR_SF_SET_SCREEN               "Vælg hoved skærm"
+#define TR_SF_LCD_TO_VIDEO             "LCD to Video"
 
 #define TR_FSW_RESET_TELEM             TR("Telm", "Telemetri")
 #define TR_FSW_RESET_TIMERS            "Tid1","Tid2","Tid3"
