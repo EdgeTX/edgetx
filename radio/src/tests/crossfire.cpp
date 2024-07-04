e3fd1d8001e3071426f8eea79c6cb9d0d0fbe909
raphaelcoeffic <1050031+raphaelcoeffic@users.noreply.github.com>
2024-07-04 08:31:46 +0200

    fix: new crsf tests

--- radio/src/tests/crossfire.cpp
+++ radio/src/tests/crossfire.cpp
@@ -19,7 +19,9 @@
  * GNU General Public License for more details.
  */
 
+#include "gtest/gtest.h"
 #include "gtests.h"
+#include "telemetry/telemetry.h"
 
 #if defined(CROSSFIRE)
 
@@ -49,15 +51,36 @@ TEST(Crossfire, crc8)
 #if defined(HARDWARE_EXTERNAL_MODULE)
 #include "pulses/crossfire.h"
 
-#define FP_TEST_INIT void* ctx = CrossfireDriver.init(EXTERNAL_MODULE);\
-  if (!ctx) return;\
-  init_lua_fifo();\
-  uint8_t len = 0
+struct crsf_frame_test {
+  void* ctx = nullptr;
 
-#define FP_PASS_FRAME(frame) CrossfireDriver.processFrame(ctx, frame, sizeof(frame),telemetry_rx_buffer, &len)
+  uint8_t buffer[TELEMETRY_RX_PACKET_SIZE];
+  uint8_t len = 0;
 
+  crsf_frame_test()
+  {
+    ctx = CrossfireDriver.init(EXTERNAL_MODULE);
+    if (!luaInputTelemetryFifo) {
+      luaInputTelemetryFifo =
+          new Fifo<uint8_t, LUA_TELEMETRY_INPUT_FIFO_SIZE>();
+      assert(luaInputTelemetryFifo != nullptr);
+    } else {
+      luaInputTelemetryFifo->clear();
+    }
+  }
 
-static uint8_t telemetry_rx_buffer[TELEMETRY_RX_PACKET_SIZE];
+  template<unsigned Len>
+  void process(uint8_t (&frame)[Len]) {
+    CrossfireDriver.processFrame(ctx, frame, Len, buffer, &len);    
+  }
+
+  ~crsf_frame_test()
+  {
+    if (ctx != nullptr) {
+      CrossfireDriver.deinit(ctx);
+    }
+  }
+};
 
 static uint8_t incomplete_frame[] = {
     // first frame
@@ -80,28 +103,20 @@ static uint8_t cont_frame[] = {
     0xEA, 0x0A, 0x0B
 };
 
-static void init_lua_fifo() {
-  if (!luaInputTelemetryFifo) {
-    luaInputTelemetryFifo = new Fifo<uint8_t, LUA_TELEMETRY_INPUT_FIFO_SIZE>();
-    ASSERT_TRUE(luaInputTelemetryFifo != 0);
-  } else {
-    luaInputTelemetryFifo->clear();
-  }
-}
-
 TEST(Crossfire, frameParser_incompleteFrames)
 {
-  FP_TEST_INIT;
+  crsf_frame_test ft;
+  if (!ft.ctx) return;
   
-  FP_PASS_FRAME(incomplete_frame);
-  EXPECT_EQ(len, 7);
-  EXPECT_EQ(telemetry_rx_buffer[0], 0xEA);
-  EXPECT_EQ(telemetry_rx_buffer[1], 0x1F);
+  ft.process(incomplete_frame);
+  EXPECT_EQ(ft.len, 7);
+  EXPECT_EQ(ft.buffer[0], 0xEA);
+  EXPECT_EQ(ft.buffer[1], 0x1F);
 
-  FP_PASS_FRAME(cont_frame);
-  EXPECT_EQ(len, 3);
-  EXPECT_EQ(telemetry_rx_buffer[0], 0xEA);
-  EXPECT_EQ(telemetry_rx_buffer[1], 0x0A);
+  ft.process(cont_frame);
+  EXPECT_EQ(ft.len, 3);
+  EXPECT_EQ(ft.buffer[0], 0xEA);
+  EXPECT_EQ(ft.buffer[1], 0x0A);
 
   uint8_t* lua_buffer = luaInputTelemetryFifo->buffer();
   EXPECT_EQ(luaInputTelemetryFifo->size(), 0x14 + 0x21 + 0x1F);
@@ -115,8 +130,6 @@ TEST(Crossfire, frameParser_incompleteFrames)
 
   EXPECT_EQ(lua_buffer[offset], 0x1F);
   EXPECT_EQ(lua_buffer[offset + 0x1F - 1], 0xFE);
-
-  CrossfireDriver.deinit(ctx);
 }
 
 static uint8_t length_error[] = {
@@ -127,14 +140,14 @@ static uint8_t length_error2[] = {
     // first frame
     0xEA, 0x09, 0xFF, 0x11, 0xFD, 0x05, 0x00, 0x00, 0x13, 0x01, 0x8C,
     // 2nd incomplete frame
-    0x2A, 0x3F, 0x5F, 0x00,
+    0x2A, 0xFE, 0x5F, 0x00,
 };
 
 static uint8_t length_error3[] = {
     // first frame
     0xEA, 0x09, 0xFF, 0x11, 0xFD, 0x05, 0x00, 0x00, 0x13, 0x01, 0x8C,
     // 2nd invalid, too long frame, also truncated 
-    0x2A, 0x3F, 0x5F, 0x00, 
+    0x2A, 0xFE, 0x5F, 0x00,
     // third valid frame
     0xEA, 0x09, 0xFF, 0x11, 0xFD, 0x05, 0x00, 0x00, 0x13, 0x01, 0x8C,
 };
@@ -142,36 +155,52 @@ static uint8_t length_error3[] = {
 
 TEST(Crossfire, frameParser_length)
 {
-  FP_TEST_INIT;
+  crsf_frame_test ft;
+  if (!ft.ctx) return;
+
+  uint8_t* lua_buffer = luaInputTelemetryFifo->buffer();
+
   // Check that a frame that is too big is rejected even if incomplete
-  FP_PASS_FRAME(length_error);
-  EXPECT_LE(len, 2);
+  ft.process(length_error);
+  EXPECT_EQ(ft.len, 0);
 
   // Check that a frame that is too big is rejected if positioned
   // after a complete frame
-  FP_PASS_FRAME(length_error2);
-  EXPECT_LE(len, 2);
+  ft.process(length_error2);
+  EXPECT_EQ(ft.len, 0);
 
-  FP_PASS_FRAME(length_error3);
-  EXPECT_EQ(len, 0);
+  // the first complete frame should have been processed
+  EXPECT_EQ(luaInputTelemetryFifo->size(), 0x09);
 
-  uint8_t* lua_buffer = luaInputTelemetryFifo->buffer();
-  EXPECT_EQ(luaInputTelemetryFifo->size(), 0x09 + 0x09 + 0x09);
-  unsigned offset = 0x09;
+  ft.process(length_error3);
+  EXPECT_EQ(ft.len, 0);
+
+  // only the first frame has been processed, as the rest
+  // of the input buffer is thrown away due to length error
+  EXPECT_EQ(luaInputTelemetryFifo->size(), 0x09 + 0x09);
+
+  // check first frame
+  unsigned offset = 0;
   EXPECT_EQ(lua_buffer[offset], 0x09);
   offset += 0x09;
+
+  // check second frame
   EXPECT_EQ(lua_buffer[offset], 0x09);
-  EXPECT_EQ(lua_buffer[offset+0x09-1], 0x01);
-  CrossfireDriver.deinit(ctx);
+  EXPECT_EQ(lua_buffer[offset + 0x09 - 1], 0x01);
 }
 
 static uint8_t invalid_frames[] = {
     // first frame
     0xEA, 0x14, 0xFF, 0x11, 0xFD, 0x05, 0x00, 0x00, 0x13, 0x01, 0x01,
     0x2A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x5D, 0x98, 0xB4,
+};
+
+static uint8_t invalid_frames2[] = {
     // random bytes
     0x00, 0x35, 0xA4,
+};
 
+static uint8_t invalid_frames3[] = {
     // second frame
     0xEA, 0x21, 0xFF, 0x1E, 0xFD, 0x12, 0x00, 0x00, 0x14, 0x01, 0x01,
     0x4A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -179,7 +208,7 @@ static uint8_t invalid_frames[] = {
     0x3F, 0x31,
     // invalid CRC frame
     0xEA, 0x02, 0x00, 0x01,
-    //  third frame
+    // third frame
     0xEA, 0x1F, 0xFF, 0x1C, 0xFD, 0x10, 0x00,0x00, 0x15, 0x01, 0x01, 0x24, 0x00, 0x00, 0x35, 0x7D, 0xF2, 0x40,
     0xE8, 0x03, 0xE8, 0x03, 0xDC, 0x05, 0xDC, 0x05, 0xE1, 0x05, 0xE1,
     0x05, 0x2A, 0xFE, 0x5F,
@@ -188,10 +217,17 @@ static uint8_t invalid_frames[] = {
 TEST(Crossfire, frameParser_badFrames)
 {
   //check if frameParser correctly skips bad frames (too long, bad CRC) and does't lose following packets 
-  FP_TEST_INIT;
+  crsf_frame_test ft;
+  if (!ft.ctx) return;
 
-  FP_PASS_FRAME(invalid_frames);
-  EXPECT_EQ(len,0);
+  ft.process(invalid_frames);
+  EXPECT_EQ(ft.len,0);
+
+  ft.process(invalid_frames2);
+  EXPECT_EQ(ft.len,0);
+
+  ft.process(invalid_frames3);
+  EXPECT_EQ(ft.len,0);
 
   uint8_t* lua_buffer = luaInputTelemetryFifo->buffer();
   EXPECT_EQ(luaInputTelemetryFifo->size(), 0x14 + 0x21 + 0x1F);
@@ -230,20 +266,24 @@ static uint8_t jumboFrame2[]={ //jumbo frame 2
 
 TEST(Crossfire, frameParser_multipleJumboFrames)
 {
-  FP_TEST_INIT;
-  FP_PASS_FRAME(jumboFrame1);
-  EXPECT_EQ(len,63);
-  EXPECT_EQ(telemetry_rx_buffer[0], 0xEA);
-  EXPECT_EQ(telemetry_rx_buffer[1], 0x3E);
+  crsf_frame_test ft;
+  if (!ft.ctx) return;
 
-  FP_PASS_FRAME(jumboFrame2);
-  EXPECT_EQ(len,0);
+  ft.process(jumboFrame1);
+  EXPECT_EQ(ft.len, 63);
+  EXPECT_EQ(ft.buffer[0], 0xEA);
+  EXPECT_EQ(ft.buffer[1], 0x3E);
+
+  ft.process(jumboFrame2);
+  EXPECT_EQ(ft.len,0);
+
   uint8_t* lua_buffer = luaInputTelemetryFifo->buffer();
   EXPECT_EQ(luaInputTelemetryFifo->size(), 62 + 61);
+
+  unsigned offset = 0;
   EXPECT_EQ(lua_buffer[offset], 0x3E);
   EXPECT_EQ(lua_buffer[offset + 0x3E - 1], 0xFB);
 
-  unsigned offset = 0;
   EXPECT_EQ(lua_buffer[offset], 0x3E);
   offset += 0x3E;
   
