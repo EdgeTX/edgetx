7b0affda764735bf12fb7b1b6bf39c57e38489a6
elecpower <elecpower@users.noreply.github.com>
2024-07-12 11:43:30 +1000

    define has only local scope so move from global scope and rename include source inversion fix

--- companion/src/firmwares/customfunctiondata.cpp
+++ companion/src/firmwares/customfunctiondata.cpp
@@ -284,7 +284,7 @@ QString CustomFunctionData::resetToString(const int value, const ModelData * mod
 
   if (value < step) {
     if (value < firmware->getCapability(Timers))
-      return RawSource(SOURCE_TYPE_TIMER, value).toString(model);
+      return RawSource(SOURCE_TYPE_TIMER, value + 1).toString(model);
     else
       return QString(CPN_STR_UNKNOWN_ITEM);
   }
@@ -299,17 +299,19 @@ QString CustomFunctionData::resetToString(const int value, const ModelData * mod
     return tr("Trims");
 
   if (value < step + firmware->getCapability(Sensors))
-    return RawSource(SOURCE_TYPE_TELEMETRY, 3 * (value - step + 1)).toString(model);
+    return RawSource(SOURCE_TYPE_TELEMETRY, 3 * (value - step) + 1).toString(model);
 
   return QString(CPN_STR_UNKNOWN_ITEM);
 }
+
+#define RESET_SENSORS_START  CPN_MAX_TIMERS + 3   // keep in sync with above
 
 //  static
 int CustomFunctionData::resetParamCount()
 {
   Firmware * firmware = getCurrentFirmware();
 
-  return CPN_MAX_TIMERS + CPN_MAX_RESET_FUNCTIONS + firmware->getCapability(Sensors);
+  return RESET_SENSORS_START + firmware->getCapability(Sensors);
 }
 
 //  static
@@ -323,10 +325,10 @@ bool CustomFunctionData::isResetParamAvailable(const int index, const ModelData 
     else
       return false;
   }
-  else if (index < CPN_MAX_TIMERS + CPN_MAX_RESET_FUNCTIONS)
+  else if (index < RESET_SENSORS_START)
     return true;
   else if (model && index < resetParamCount())
-    return model->sensorData[index - (CPN_MAX_TIMERS + CPN_MAX_RESET_FUNCTIONS)].isAvailable();
+    return model->sensorData[index - (RESET_SENSORS_START)].isAvailable();
 
   return false;
 }
