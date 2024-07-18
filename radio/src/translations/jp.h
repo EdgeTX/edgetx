3fc0648a30f35a4fb7090e28b81f06f65319aa09
philmoz <phil.a.mitchell@gmail.com>
2024-07-18 20:51:54 +1000

    JP translation.

--- radio/src/translations/jp.h
+++ radio/src/translations/jp.h
@@ -1303,4 +1303,4 @@
 #define TR_MID_BATT_COLOR         "バッテリー中"
 #define TR_HIGH_BATT_COLOR        "バッテリー高"
 
-#define TR_WIDGET_SIZE            "Widget size"
+#define TR_WIDGET_SIZE            "ウィジェットサイズ"
