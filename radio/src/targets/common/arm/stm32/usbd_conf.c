fa71cf3d88a66f850654a617ce6cc08c4edf5210
raphaelcoeffic <1050031+raphaelcoeffic@users.noreply.github.com>
2024-07-23 15:28:36 +0200

    chore: update ST USB middleware to v2.11.3
    Summary of changes:
    - the CDC changes, notably the removal of the SOF callback, have been removed compared to ST's version.
    - DFU class support has been added.

--- radio/src/targets/common/arm/stm32/usbd_conf.c
+++ radio/src/targets/common/arm/stm32/usbd_conf.c
@@ -48,7 +48,6 @@ PCD_HandleTypeDef hpcd_USB_OTG_FS;
 void Error_Handler(void){}
 
 /* External functions --------------------------------------------------------*/
-void SystemClock_Config(void);
 
 /* USER CODE BEGIN 0 */
 
