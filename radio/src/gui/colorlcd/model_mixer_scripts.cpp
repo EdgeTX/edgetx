326c58a7fd2c68b8d00e816058791dd51de18270
Michael <mha1@users.noreply.github.com>
2024-07-25 23:53:52 +0200

    fix: inconsistency between SD play value, model custom script screen and value widget (#4391)
    Co-authored-by: Peter Feerick <peter.feerick@gmail.com>

--- radio/src/gui/colorlcd/model_mixer_scripts.cpp
+++ radio/src/gui/colorlcd/model_mixer_scripts.cpp
@@ -157,7 +157,7 @@ class ScriptEditWindow : public Page
             COLOR_THEME_PRIMARY1);
         lbl->padLeft(PAD_LARGE);
         new DynamicNumber<int16_t>(
-            line, rect_t{}, [=]() { return calcRESXto1000(so->value); });
+            line, rect_t{}, [=]() { return calcRESXto1000(so->value); }, PREC1);
       }
     }
   }
