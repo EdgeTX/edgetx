d8e4185e3121df0b15481b5eb189aa98c3d0f2f9
philmoz <philmoz@users.noreply.github.com>
2024-08-05 10:40:54 +1000

    fix(color): min/max highlight in output list swapped if inverted (#5367)
    Use code from output edit page to highlight min/max values on output list page for consistency

--- radio/src/gui/colorlcd/model/output_edit.h
+++ radio/src/gui/colorlcd/model/output_edit.h
@@ -24,6 +24,11 @@
 #include "page.h"
 #include "gvar_numberedit.h"
 
+// deadband in % for switching direction of Min/Max text and value field highlighting
+// 0 = no deadband
+// 1..100 = [-DEADBAND; DEADBAND]
+#define DEADBAND 0
+
 class OutputEditStatusBar;
 
 class OutputEditWindow : public Page
