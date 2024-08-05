EdgeTX/edgetx

commit 159db764e0b3769e28c80191f6d21294851cde9e

fix(bw): Remaining/elapsed display on BW telem topbar (#3871)

radio/src/gui/212x64/lcd.cpp
@@ -546,13 +546,25 @@ void drawTelemetryTopBar()
   uint8_t att = (IS_TXBATT_WARNING() ? BLINK : 0);
   putsVBat(12*FW, 0, att);
   if (g_model.timers[0].mode) {
-    att = (timersStates[0].val<0 ? BLINK : 0);
-    drawTimer(22*FW, 0, timersStates[0].val, att, att);
+    TimerData *timer =  &g_model.timers[0];
+    int32_t val = 0;
+    if (g_model.timers[0].showElapsed)
+      val = timer->start - timersStates[0].val;
+    else
+      val = timersStates[0].val;
+    LcdFlags att = (val < 0 ? BLINK : 0) | TIMEHOUR;
+    drawTimer(22*FW, 0, val, att, att);
     lcdDrawText(22*FW, 0, "T1:", RIGHT);
   }
   if (g_model.timers[1].mode) {
-    att = (timersStates[1].val<0 ? BLINK : 0);
-    drawTimer(31*FW, 0, timersStates[1].val, att, att);
+    TimerData *timer =  &g_model.timers[1];
+    int32_t val = 0;
+    if (g_model.timers[1].showElapsed)
+      val = timer->start - timersStates[1].val;
+    else
+      val = timersStates[1].val;
+    LcdFlags att = (val < 0 ? BLINK : 0) | TIMEHOUR;
+    drawTimer(31*FW, 0, val, att, att);
     lcdDrawText(31*FW, 0, "T2:", RIGHT);
   }
   lcdInvertLine(0);

radio/src/gui/128x64/lcd.cpp
@@ -628,8 +628,14 @@ void lcdDrawFilledRect(coord_t x, coord_t y, coord_t w, coord_t h, uint8_t pat, 
 void drawTelemetryTopBar()
 {
   if (g_model.timers[0].mode) {
-    uint8_t att = (timersStates[0].val<0 ? BLINK : 0);
-    drawTimer(0, 0, timersStates[0].val, att, att);
+    TimerData *timer =  &g_model.timers[0];
+    int32_t val = 0;
+    if (g_model.timers[0].showElapsed)
+      val = timer->start - timersStates[0].val;
+    else
+      val = timersStates[0].val;
+    LcdFlags att = (val < 0 ? BLINK : 0) | TIMEHOUR;
+    drawTimer(0, 0, val, att, att);
   } else {
     drawModelName(0, 0, g_model.header.name, g_eeGeneral.currModel, 0);
   }

commit 237bf396d9a67b4a3b7a0cfd0eb2af9883cec63c

fix(bw212): T2 being cut off on top bar of telemetry screen Fixes #5369. Regression from #3871.

radio/src/gui/212x64/lcd.cpp
@@ -550,8 +550,8 @@ void drawTelemetryTopBar()
     else
       val = timersStates[0].val;
     LcdFlags att = (val < 0 ? BLINK : 0) | TIMEHOUR;
-    drawTimer(22*FW, 0, val, att, att);
-    lcdDrawText(22*FW, 0, "T1:", RIGHT);
+    drawTimer(18*FW, 0, val, att, att);
+    lcdDrawText(18*FW, 0, "T1:", RIGHT);
   }
   if (g_model.timers[1].mode) {
     TimerData *timer =  &g_model.timers[1];
@@ -561,8 +561,8 @@ void drawTelemetryTopBar()
     else
       val = timersStates[1].val;
     LcdFlags att = (val < 0 ? BLINK : 0) | TIMEHOUR;
-    drawTimer(31*FW, 0, val, att, att);
-    lcdDrawText(31*FW, 0, "T2:", RIGHT);
+    drawTimer(28*FW, 0, val, att, att);
+    lcdDrawText(28*FW, 0, "T2:", RIGHT);
   }
   lcdInvertLine(0);
 }