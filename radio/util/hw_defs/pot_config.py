79b492e136dffd26bd10d675364dc3b397d6409b
Peter Feerick <peter.feerick@gmail.com>
2024-10-10 03:20:04 +0000

    fix: P1/P2 are pots with detents

--- radio/util/hw_defs/pot_config.py
+++ radio/util/hw_defs/pot_config.py
@@ -51,8 +51,8 @@ POT_CONFIG = {
         "P2": {"default": "POT_CENTER"}
     },
     "bumblebee": {
-        "P1": {"default": "SLIDER"},
-        "P2": {"default": "SLIDER"}
+        "P1": {"default": "POT_CENTER"},
+        "P2": {"default": "POT_CENTER"}
     },
     "t20": {
         "P1":  {"default": "POT_CENTER"},
