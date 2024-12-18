09f3af962f4cdc0f8a93548bd3a0aad6bfcda033
3djc <3djc@gh.com>
2024-12-18 10:54:56 +0100

    fix: colorlcd compilation

--- radio/src/edgetx.h
+++ radio/src/edgetx.h
@@ -701,6 +701,7 @@ union ReusableBuffer
   PXX2HardwareAndSettings hardwareAndSettings; // radio_version
 #endif
 
+#if defined(NUM_BODY_LINES)
   struct scriptInfo{
       uint8_t index;
       char label[(LCD_W / FW) + 1];
@@ -708,6 +709,7 @@ union ReusableBuffer
       void (* tool)(event_t);
       char path[LEN_FILE_PATH_MAX+10+1];
   };
+#endif
 
   struct {
 #if defined(NUM_BODY_LINES)
