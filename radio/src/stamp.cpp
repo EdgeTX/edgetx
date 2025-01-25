61003a0b0753c0736b8892cc76e40cfefef09aa7
raphaelcoeffic <1050031+raphaelcoeffic@users.noreply.github.com>
2025-01-25 15:36:00 +0100

    getFirmwareVersion() should return null if no version

--- radio/src/stamp.cpp
+++ radio/src/stamp.cpp
@@ -106,12 +106,12 @@ const char * getFirmwareVersion(const uint8_t* buffer)
 #endif
   }
 
-  for (int i = 0; i < 2024; i++) {
+  for (int i = 0; i < 1024; i++) {
     if ((memcmp(buffer + i, "edgetx-", 7) == 0)
         || memcmp(buffer + i, "opentx-", 7) == 0) {
       return (const char*)buffer + i;
     }
   }
-  return "no version found";
+  return nullptr;
 }
 #endif
