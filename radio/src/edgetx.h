b4a7960696e66e3ba0757f9192d8e43940d8b66c
philmoz <philmoz@users.noreply.github.com>
2025-02-03 10:38:07 +1100

    fix(sim): flight mode name incorrect in the simulator window title bar (#5854)

--- radio/src/edgetx.h
+++ radio/src/edgetx.h
@@ -794,30 +794,6 @@ union ReusableBuffer
 
 extern ReusableBuffer reusableBuffer;
 
-uint8_t zlen(const char *str, uint8_t size);
-bool zexist(const char *str, uint8_t size);
-char * strcat_zchar(char *dest, const char *name, uint8_t size, const char spaceSym = 0, const char *defaultName=nullptr, uint8_t defaultNameSize=0, uint8_t defaultIdx=0);
-#define strcatFlightmodeName(dest, idx) strcat_zchar(dest, g_model.flightModeData[idx].name, LEN_FLIGHT_MODE_NAME, 0, STR_FM, PSIZE(TR_FM), idx+1)
-
-#if !defined(STORAGE_MODELSLIST)
-
-#define strcat_modelname(dest, idx, spaceSym)                                     \
-  strcat_zchar(dest, modelHeaders[idx].name, LEN_MODEL_NAME, spaceSym, STR_MODEL, \
-               PSIZE(TR_MODEL), idx + 1)
-
-#define strcat_currentmodelname(dest, spaceSym)      \
-  strcat_modelname(dest, g_eeGeneral.currModel, spaceSym)
-
-#else
-
-#define strcat_currentmodelname(dest, spaceSym)                         \
-  strcat_zchar(dest, g_model.header.name, LEN_MODEL_NAME, spaceSym)
-
-#endif
-
-#define ZLEN(s) zlen(s, sizeof(s))
-#define ZEXIST(s) zexist(s, sizeof(s))
-
 // Stick tolerance varies between transmitters, Higher is better
 #define STICK_TOLERANCE 64
 
