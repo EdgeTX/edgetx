d769e37025fdd4841015194784122d72dba8d5f0
Peter Feerick <peter.feerick@gmail.com>
2025-02-05 01:27:16 +0000

    feat: experiment into swapping power on sequence

--- radio/src/targets/horus/board.cpp
+++ radio/src/targets/horus/board.cpp
@@ -263,7 +263,12 @@ void boardInit()
   rtcInit(); // RTC must be initialized before rambackupRestore() is called
 #endif
 
+#if !defined(POWER_BLUE)
   ledBlue();
+#else
+  ledGreen();
+#endif
+
 #if !defined(LCD_VERTICAL_INVERT)
   lcdSetInitalFrameBuffer(lcdFront->getData());
 #elif defined(RADIO_F16)
