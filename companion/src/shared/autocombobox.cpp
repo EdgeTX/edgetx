c8bbd8d98e30559311a5e3a38d1e86b73a7812d2
elecpower <elecpower@users.noreply.github.com>
2025-03-15 08:43:53 +1100

    Fix type-punned warning

--- companion/src/shared/autocombobox.cpp
+++ companion/src/shared/autocombobox.cpp
@@ -24,17 +24,24 @@
 AutoComboBox::AutoComboBox(QWidget * parent):
   QComboBox(parent),
   AutoWidget(),
-  m_field(nullptr),
   m_next(0),
-  m_hasModel(false),
-  m_rawSource(nullptr),
-  m_rawSwitch(nullptr)
+  m_hasModel(false)
 {
+  initField();
   connect(this, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &AutoComboBox::onCurrentIndexChanged);
 }
 
 AutoComboBox::~AutoComboBox()
 {
+}
+
+void AutoComboBox::initField()
+{
+  m_field = nullptr;
+  m_rawSource = nullptr;
+  m_rawSwitch = nullptr;
+  m_curveType = nullptr;
+  m_flexType = nullptr;
 }
 
 void AutoComboBox::clear()
@@ -79,40 +86,50 @@ void AutoComboBox::addItem(const QString & item, int value)
 
 void AutoComboBox::setField(unsigned int & field, GenericPanel * panel)
 {
-  m_field = (int *)&field;
-  m_rawSource = nullptr;
-  m_rawSwitch = nullptr;
   setFieldInit(panel);
+  m_field = (int *)&field;
+  updateValue();
 }
 
 void AutoComboBox::setField(int & field, GenericPanel * panel)
 {
-  m_field = &field;
-  m_rawSource = nullptr;
-  m_rawSwitch = nullptr;
   setFieldInit(panel);
+  m_field = &field;
+  updateValue();
 }
 
 void AutoComboBox::setField(RawSource & field, GenericPanel * panel)
 {
-  m_rawSource = &field;
-  m_rawSwitch = nullptr;
-  m_field = nullptr;
   setFieldInit(panel);
+  m_rawSource = &field;
+  updateValue();
 }
 
 void AutoComboBox::setField(RawSwitch & field, GenericPanel * panel)
 {
-  m_rawSwitch = &field;
-  m_rawSource = nullptr;
-  m_field = nullptr;
   setFieldInit(panel);
+  m_rawSwitch = &field;
+  updateValue();
+}
+
+void AutoComboBox::setField(CurveData::CurveType & field, GenericPanel * panel)
+{
+  setFieldInit(panel);
+  m_curveType = &field;
+  updateValue();
+}
+
+void AutoComboBox::setField(Board::FlexType & field, GenericPanel * panel)
+{
+  setFieldInit(panel);
+  m_flexType = &field;
+  updateValue();
 }
 
 void AutoComboBox::setFieldInit(GenericPanel * panel)
 {
+  initField();
   setPanel(panel);
-  updateValue();
 }
 
 void AutoComboBox::setModel(QAbstractItemModel * model)
@@ -131,14 +148,12 @@ void AutoComboBox::setAutoIndexes()
 
   for (int i = 0; i < count(); ++i)
     setItemData(i, i);
+
   updateValue();
 }
 
 void AutoComboBox::updateValue()
 {
-  if (!m_field && !m_rawSource && !m_rawSwitch)
-    return;
-
   setLock(true);
 
   if (m_field)
@@ -147,6 +162,10 @@ void AutoComboBox::updateValue()
     setCurrentIndex(findData(m_rawSource->toValue()));
   else if (m_rawSwitch)
     setCurrentIndex(findData(m_rawSwitch->toValue()));
+  else if (m_curveType)
+    setCurrentIndex(findData(*m_curveType));
+  else if (m_flexType)
+    setCurrentIndex(findData(*m_flexType));
 
   setLock(false);
 }
@@ -169,6 +188,12 @@ void AutoComboBox::onCurrentIndexChanged(int index)
   }
   else if (m_rawSwitch && m_rawSwitch->toValue() != val) {
     *m_rawSwitch = RawSwitch(val);
+  }
+  else if (m_curveType && *m_curveType != val) {
+    *m_curveType = (CurveData::CurveType)val;
+  }
+  else if (m_flexType && *m_flexType != val) {
+    *m_flexType = (Board::FlexType)val;
   }
   else
     return;
