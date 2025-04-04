c39f311e363ac8c6427a7acdada08fd60baca573
philmoz <phil.a.mitchell@gmail.com>
2025-04-05 09:25:12 +1100

    Add UI to set color.

--- radio/src/targets/simu/switch_driver.cpp
+++ radio/src/targets/simu/switch_driver.cpp
@@ -30,6 +30,10 @@
 struct hw_switch_def {
   const char*  name;
   SwitchHwType type;
+#if defined(SWITCH_LED_COUNT)
+  uint8_t      ledIdx;
+  uint8_t      ledOfst;
+#endif
 };
 
 #include "simu_switches.inc"
@@ -96,6 +100,11 @@ uint8_t boardGetMaxSwitches() { return n_switches; }
 uint8_t boardGetMaxFctSwitches() { return n_fct_switches; }
 
 swconfig_t boardSwitchGetDefaultConfig() { return _switch_default_config; }
+
+#if defined(SWITCH_LED_COUNT)
+uint8_t boardGetSwitchLedIdx(uint8_t idx) { return _switch_defs[idx].ledIdx; }
+uint8_t boardGetSwitchLedOfst(uint8_t idx) { return _switch_defs[idx].ledOfst; }
+#endif
 
 switch_display_pos_t switchGetDisplayPosition(uint8_t idx)
 {
