07ea98db4a39ece98e306dd324cb461898a62fdb
3djc <lesitewebdejc@hotmail.com>
2025-04-04 04:44:53 +0200

    fix(radio): prevent dedicated RGB LEDs flickering (#6045)

--- radio/src/boards/generic_stm32/rgb_leds.cpp
+++ radio/src/boards/generic_stm32/rgb_leds.cpp
@@ -47,6 +47,8 @@ void rgbSetLedColor(uint8_t led, uint8_t r, uint8_t g, uint8_t b)
 {
 #if defined(SIXPOS_SWITCH_INDEX)
   ws2812_set_color(led + 6, r, g, b);
+#elif defined(RGB_LED_OFFSET)
+  ws2812_set_color(led + RGB_LED_OFFSET, r, g, b);
 #else
   ws2812_set_color(led, r, g, b);
 #endif
