57abfcd886d9e850321e67fe4b2e880c4a69b7de
philmoz <phil.a.mitchell@gmail.com>
2025-05-01 13:08:37 +1000

    Fix high CPU usage on flight modes, logical switches, mixer scripts, special functions and USB joystick pages.

--- radio/src/gui/colorlcd/model/special_functions.h
+++ radio/src/gui/colorlcd/model/special_functions.h
@@ -144,7 +144,7 @@ class FunctionsPage : public PageTab
   void rebuild(Window* window);
   void newSF(Window* window, bool pasteSF);
   void editSpecialFunction(Window* window, uint8_t index,
-                           ButtonBase* button);
+                           FunctionLineButton* button);
   void pasteSpecialFunction(Window* window, uint8_t index,
                             ButtonBase* button);
   void plusPopup(Window* window);
