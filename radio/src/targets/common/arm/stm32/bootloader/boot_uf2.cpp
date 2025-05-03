f46edd21d7ffce5a09f65a0eec97a5f8c78556f3
philmoz <phil.a.mitchell@gmail.com>
2025-05-03 14:15:28 +1000

    Fix handling of empty FIRMWARE folder. Add RTN key handling for H7 radios. Add missing copyright header to files.

--- radio/src/targets/common/arm/stm32/bootloader/boot_uf2.cpp
+++ radio/src/targets/common/arm/stm32/bootloader/boot_uf2.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) EdgeTX
+ *
+ * Based on code named
+ *   opentx - https://github.com/opentx/opentx
+ *   th9x - http://code.google.com/p/th9x
+ *   er9x - http://code.google.com/p/er9x
+ *   gruvin9x - http://code.google.com/p/gruvin9x
+ *
+ * License GPLv2: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include "boot.h"
 
 #include "hal/usb_driver.h"
@@ -26,6 +47,12 @@ void bootloaderUF2()
   uint32_t next_frame = time_get_ms();
 
   for (;;) {
+    event_t event = getEvent();
+
+    if (state != ST_FLASHING && event == EVT_KEY_LONG(KEY_EXIT)) {
+      // Start the main application
+      state = ST_REBOOT;
+    }
 
     if (time_get_ms() - next_frame >= FRAME_INTERVAL_MS) {
       next_frame += FRAME_INTERVAL_MS;
