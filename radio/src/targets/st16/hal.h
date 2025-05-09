8a58e03edd58dce612ec4a788cc8e890ab816c94
philmoz <phil.a.mitchell@gmail.com>
2025-05-09 20:44:16 +1000

    Add new Lua function for overriding CFS LED colors.

--- radio/src/targets/st16/hal.h
+++ radio/src/targets/st16/hal.h
@@ -420,7 +420,10 @@
 
 // LED Strip
 #define LED_STRIP_LENGTH                  24
-#define LED_STRIP_RESERVED_AT_END         12
+#define BLING_LED_STRIP_START             0
+#define BLING_LED_STRIP_LENGTH            12
+#define CFS_LED_STRIP_START               12
+#define CFS_LED_STRIP_LENGTH              6
 #define LED_STRIP_GPIO                    GPIO_PIN(GPIOA, 15)  // PA.15 / TIM2_CH1
 #define LED_STRIP_GPIO_AF                 LL_GPIO_AF_1         // TIM1/2/16/17
 #define LED_STRIP_TIMER                   TIM2
