9647bc999eb24935ff49d7f045612b85594c3b77
Richard Li <richard.li@ces.hk>
2025-06-03 11:57:54 +0800

    Fixed Flysky targets boot problem

--- radio/src/targets/pl18/board.cpp
+++ radio/src/targets/pl18/board.cpp
@@ -59,6 +59,8 @@
 // Common ADC driver
 extern const etx_hal_adc_driver_t _adc_driver;
 
+extern "C" void SDRAM_Init();
+
 #if defined(SEMIHOSTING)
 extern "C" void initialise_monitor_handles();
 #endif
@@ -176,12 +178,18 @@ void boardBLEarlyInit()
 #endif
 }
 
-#if defined(RADIO_NB4P)
 void boardBLPreJump()
 {
+  SDRAM_Init();
+#if defined(RADIO_NB4P)
   LL_ADC_Disable(ADC_MAIN);
-}
 #endif
+}
+
+void boardBLInit()
+{
+  SDRAM_Init();
+}
 
 static void monitorInit()
 {
