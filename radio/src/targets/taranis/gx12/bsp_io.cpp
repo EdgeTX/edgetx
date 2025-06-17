a4c3dbe6da9a655463c0fb84764f301d9262d01b
3djc <lesitewebdejc@hotmail.com>
2025-06-17 04:09:48 +0200

    fix(gx12): use PCA95xx reset pin in case of read issues (#6358)

--- radio/src/targets/taranis/gx12/bsp_io.cpp
+++ radio/src/targets/taranis/gx12/bsp_io.cpp
@@ -31,6 +31,7 @@
 #include "stm32_i2c_driver.h"
 #include "stm32_ws2812.h"
 #include "timers_driver.h"
+#include "delays_driver.h"
 
 struct bsp_io_expander {
   pca95xx_t exp;
@@ -54,6 +55,11 @@ static uint32_t _read_io_expander(bsp_io_expander* io)
   uint16_t value = 0;
   if (pca95xx_read(&io->exp, io->mask, &value) == 0) {
     io->state = value;
+  } else {
+    gpio_clear(IO_RESET_GPIO);
+    TRACE("PCA95 was reset");
+    delay_us(1);  // Only 6ns are needed according to PCA datasheet, but lets be safe
+    gpio_set(IO_RESET_GPIO);
   }
   return io->state;  
 }
