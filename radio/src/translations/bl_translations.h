7693f49f8e081cba3a42e5aba21a664f12a0f8c2
MatCron <pancronos@gmail.com>
2025-07-05 00:53:14 +0200

    Update Italian

--- radio/src/translations/bl_translations.h
+++ radio/src/translations/bl_translations.h
@@ -245,7 +245,7 @@
     #define TR_BL_EXIT                    "Esci"
     #define TR_BL_DIR_MISSING             "Cartella non trovata"
     #define TR_BL_DIR_EMPTY               "Cartella vuota"
-    #define TR_BL_WRITING_FW              "Scrittura Firmware..."
+    #define TR_BL_WRITING_FW              "Scrittura..."
     #define TR_BL_WRITING_COMPL           "Scrittura completata"
     #define TR_BL_ENABLE                  "Abilita"
     #define TR_BL_DISABLE                 "Disabilita"
@@ -271,12 +271,12 @@
     #define TR_BL_ERASE_FLASH              "Erase Flash Storage"
     #define TR_BL_ERASE_FLASH_MSG          "This may take up to 200s"
     #define TR_BL_RF_USB_ACCESS            "RF USB access"
-    #define TR_BL_CURRENT_FW               "Current firmware:"
+    #define TR_BL_CURRENT_FW               "Firmware corrente:"
 
     #define TR_BL_SELECT_KEY               BL_SELECT_KEY " per scegliere il file"
     #define TR_BL_FLASH_KEY                "Tenere premuto " BL_SELECT_KEY " per scrivere"
-    #define TR_BL_ERASE_KEY                "Hold " BL_SELECT_KEY " long to erase"
-    #define TR_BL_TOGGLE_KEY               BL_SELECT_KEY " to toggle"
+    #define TR_BL_ERASE_KEY                "Premi " BL_SELECT_KEY " a lungo per cancellare"
+    #define TR_BL_TOGGLE_KEY               BL_SELECT_KEY " per cambiare"
     #define TR_BL_EXIT_KEY                 BL_EXIT_KEY " per uscire"
 
 #elif defined(TRANSLATIONS_PL)
