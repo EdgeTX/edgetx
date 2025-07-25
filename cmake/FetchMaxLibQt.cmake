38d4afc392b8116508dfc39bccdc5c3986804b17
Neil Horne <15316949+elecpower@users.noreply.github.com>
2025-07-25 16:30:50 +1000

    fix(cpn): update maxlibqt commit reference (#6465)

--- cmake/FetchMaxLibQt.cmake
+++ cmake/FetchMaxLibQt.cmake
@@ -5,7 +5,7 @@ include(FetchContent)
 FetchContent_Declare(
   maxLibQt
   GIT_REPOSITORY https://github.com/edgetx/maxLibQt
-  GIT_TAG        6dfbfc41e258cd2446e1fd1f547e6c5d742aa2c0
+  GIT_TAG        98f5482fb8b836f48a6a5ee199911882fa420f2b
 )
 
 FetchContent_MakeAvailable(maxLibQt)
