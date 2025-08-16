6a119cb2086f184ba80d2e691bcf10f32e9ba7d2
raphaelcoeffic <1050031+raphaelcoeffic@users.noreply.github.com>
2025-08-16 21:03:42 +0200

    fix: better architecture / OS detection

--- cmake/Fetchrs_dfu.cmake
+++ cmake/Fetchrs_dfu.cmake
@@ -1,30 +1,34 @@
 # Fetch rs_dfu
+cmake_minimum_required(VERSION 3.14)
 
 include(FetchContent)
 
-string(TOLOWER ${CMAKE_SYSTEM_PROCESSOR} sys_proc)
-
-if(sys_proc MATCHES "x86_64")
-  set(rs_dfu_target_arch "x86_64")
-elseif(sys_proc MATCHES "arm64")
-  set(rs_dfu_target_arch "aarch64")
+# Determine platform and architecture
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+  set(RS_DFU_PLATFORM "unknown-linux-gnu")
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+  set(RS_DFU_PLATFORM "apple-darwin")
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+  set(RS_DFU_PLATFORM "pc-windows-msvc")
 else()
-  message(FATAL_ERROR "rs_dfu does not have support for architecture ${CMAKE_SYSTEM_PROCESSOR}!")
+  message(FATAL_ERROR "Unsupported platform: ${CMAKE_SYSTEM_NAME}")
 endif()
 
-if(WIN32)
-  set(rs_dfu_target_os "pc-windows-msvc")
-elseif(APPLE)
-  set(rs_dfu_target_os "apple-darwin")
+if(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64|amd64|AMD64")
+  set(RS_DFU_ARCH "x86_64")
+elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64|arm64")
+  set(RS_DFU_ARCH "aarch64")
 else()
-  set(rs_dfu_target_os "unknown-linux-gnu")
+  message(FATAL_ERROR "Unsupported architecture: ${CMAKE_SYSTEM_PROCESSOR}")
 endif()
 
-set(rs_dfu_asset "rs_dfu-${rs_dfu_target_arch}-${rs_dfu_target_os}.tar.gz")
+set(RS_DFU_BASE_URL "https://github.com/EdgeTX/rs-dfu/releases/latest/download")
+set(RS_DFU_FILENAME "rs_dfu-${RS_DFU_ARCH}-${RS_DFU_PLATFORM}")
+set(RS_DFU_URL "${RS_DFU_BASE_URL}/${RS_DFU_FILENAME}.tar.gz")
 
 FetchContent_Declare(
   rs_dfu
-  URL     "https://github.com/edgetx/rs-dfu/releases/latest/download/${rs_dfu_asset}"
+  URL ${RS_DFU_URL}
 )
 
 FetchContent_MakeAvailable(rs_dfu)
