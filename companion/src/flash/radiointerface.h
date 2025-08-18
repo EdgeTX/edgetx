bdf7c7a8ddf942d412157d58cfdd21e2679ea78c
elecpower <elecpower@users.noreply.github.com>
2025-08-18 12:09:54 +0000

    Replace radiointerface with RCs

--- companion/src/flash/radiointerface.h
+++ companion/src/flash/radiointerface.h
@@ -21,22 +21,73 @@
 
 #pragma once
 
+#include "rs_dfu.h"
+
+#include <QThread>
 #include <QString>
 #include <QStringList>
 
+using SliceU8 = rust::Slice<const uint8_t>;
+
+class FirmwareReaderWorker : public QThread
+{
+  Q_OBJECT
+
+ private:
+  QByteArray data;
+
+ public:
+  explicit FirmwareReaderWorker(QObject *parent = nullptr);
+  ~FirmwareReaderWorker();
+
+ protected:
+  void run() override;
+
+ signals:
+  void progressChanged(int value, int total);
+  void statusChanged(const QString &status);
+  void error(const QString &error);
+  void complete();
+};
+
+class FirmwareWriterWorker : public QThread
+{
+  Q_OBJECT
+
+ private:
+  QString firmwareFilePath;
+  // std::atomic<bool> shouldStop{false};
+
+ public:
+  explicit FirmwareWriterWorker(const QString &filePath,
+                                QObject *parent = nullptr);
+  ~FirmwareWriterWorker();
+
+ protected:
+  void run() override;
+
+ signals:
+  void progressChanged(int value, int total);
+  void statusChanged(const QString &status);
+  void error(const QString &error);
+  void complete();
+
+ private:
+  void writeUf2(DfuDevice &device, const SliceU8 &data);
+  void writeRegion(const DfuDevice &device, uint32_t addr, const SliceU8 &data);
+  void updateEraseStatus(size_t page, size_t pages);
+  void updateDloadStatus(size_t bytes, size_t total);
+
+  template <typename Duration>
+  void rebootAndRediscover(DfuDevice &device, uint32_t addr,
+                             const SliceU8 &data, uint32_t reboot_addr,
+                             Duration timeout);
+};
+
+
 class ProgressWidget;
 
-QString getRadioInterfaceCmd();
-
-QString findMassstoragePath(const QString &filename, bool onlyPath = false, ProgressWidget *progress = nullptr);
-
-QStringList getSambaArgs(const QString &tcl);
-QStringList getDfuArgs(const QString &cmd, const QString &filename);
-
-QStringList getReadEEpromCmd(const QString &filename);
-QStringList getWriteEEpromCmd(const QString &filename);
-QStringList getReadFirmwareArgs(const QString &filename);
-QStringList getWriteFirmwareArgs(const QString &filename);
+QString findMassStoragePath(const QString &filename, bool onlyPath = false, ProgressWidget *progress = nullptr);
 
 bool readFirmware(const QString &filename, ProgressWidget *progress);
 bool writeFirmware(const QString &filename, ProgressWidget *progress);
