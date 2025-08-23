15ab29afb23a8585ec368087ce41214c6c2bbbd3
elecpower <elecpower@users.noreply.github.com>
2025-08-23 23:55:07 +0000

    Read and write checklist files

--- companion/src/firmwares/modeldata.cpp
+++ companion/src/firmwares/modeldata.cpp
@@ -2045,8 +2045,5 @@ const Board::SwitchType ModelData::getSwitchType(int sw, const GeneralSettings &
 
 QString ModelData::getChecklistFilename() const
 {
-  QString name = name;
-  name.replace(" ", "_");
-  name.append(".txt");
-  return name;
+  return QString(name).replace(" ", "_").append(".txt");
 }
