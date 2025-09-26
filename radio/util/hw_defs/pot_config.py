6829219c66cc057959ead2a9282eabba3e1ba1ea
radiosky <839034809@qq.com>
2025-09-26 17:49:51 +0800

    Initial support of v12p

--- radio/util/hw_defs/pot_config.py
+++ radio/util/hw_defs/pot_config.py
@@ -158,6 +158,10 @@ POT_CONFIG = {
         "P1": {"default": "POT_CENTER"},
         "P2": {"default": "POT_CENTER"},
     },
+    "v12p": {
+        "P1": {"default": "POT_CENTER"},
+        "P2": {"default": "POT_CENTER"},
+    },
     "tx16s": {
         "P1": {"default": "POT_CENTER"},
         "P2": {"default": "MULTIPOS"},
