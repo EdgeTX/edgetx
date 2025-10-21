a75fa00d49fbec7eb0a024692f1f7dffaa060ba1
philmoz <phil.a.mitchell@gmail.com>
2025-10-22 07:37:26 +1100

    Ignore folder if .lua file with same name exists.

--- radio/src/gui/colorlcd/radio/radio_tools.cpp
+++ radio/src/gui/colorlcd/radio/radio_tools.cpp
@@ -126,6 +126,12 @@ static void scanLuaTools(std::list<ToolEntry>& scripts)
       char path[FF_MAX_LFN + 1] = SCRIPTS_TOOLS_PATH "/";
       strcat(path, fno.fname);
       if (inFolder) {
+        // check if .lua with same name exists - skip folder to avoid duplicate entries
+        auto plen = strlen(path);
+        strcat(path, ".lua");
+        if (f_stat(path, nullptr) == FR_OK)
+          continue;
+        path[plen] = 0;
         strcat(path, "/main.lua");
         if (f_stat(path, nullptr) != FR_OK)
           continue;
