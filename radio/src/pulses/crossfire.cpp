16f4127ee9600e8f262f4279a8c6a8c3421d8eda
Michael <101.37584@germanynet.de>
2026-01-15 20:30:29 +0100

    fix merge error

--- radio/src/pulses/crossfire.cpp
+++ radio/src/pulses/crossfire.cpp
@@ -488,4 +488,5 @@ const etx_proto_driver_t CrossfireDriver = {
   .processData = nullptr,
   .processFrame = crossfireProcessFrame,
   .onConfigChange = nullptr,
+  .txCompleted = modulePortSerialTxCompleted,
 };
