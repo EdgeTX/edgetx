b9370af692eda81bb99bf60da09dce221ba35439
philmoz <philmoz@users.noreply.github.com>
2026-01-20 19:06:57 +1100

    fix(color): subtrim value on Outputs page not updated when copying all trims to sub trims (#6998)

--- radio/src/gui/colorlcd/model/model_outputs.h
+++ radio/src/gui/colorlcd/model/model_outputs.h
@@ -21,6 +21,8 @@
 
 #pragma once
 
+#include <vector>
+
 #include "pagegroup.h"
 
 class OutputLineButton;
@@ -49,5 +51,7 @@ class ModelOutputsPage : public PageGroupItem
   static constexpr coord_t TRIMB_W = LCD_W - PAD_SMALL * 2;
 
  protected:
+  std::vector<OutputLineButton*> outputButtons;
+
   void editOutput(uint8_t channel, OutputLineButton* btn);
 };
