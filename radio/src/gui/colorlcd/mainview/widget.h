c3a1bb88856775b4eb7ec720a4564552f6bd4af4
philmoz <phil.a.mitchell@gmail.com>
2026-02-27 10:02:52 +1100

    Call update function for built in widgets.

--- radio/src/gui/colorlcd/mainview/widget.h
+++ radio/src/gui/colorlcd/mainview/widget.h
@@ -106,7 +106,7 @@ class Widget : public ButtonBase
   virtual bool enableFullScreenRE() const { return true; }
 
   // Called when the widget options have changed
-  virtual void updateWithoutRefresh() {}
+  virtual void updateWithoutRefresh() { update(); }
   virtual void update() {}
 
   // Called at regular time interval if the widget is hidden or off screen
