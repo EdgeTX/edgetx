f1f0bcc943395311843e559eee9944ed6589fb83
3djc <3djc@gh.com>
2026-03-18 16:08:25 +0100

    fix: compilation (legacy was missing)

--- radio/src/targets/gx15/hal.h
+++ radio/src/targets/gx15/hal.h
@@ -263,18 +263,14 @@ TIM17:	ROTARY_ENCODER_TIMER
 #define ADC_VREF_PREC2                  330
 
 #define ADC_DIRECTION {       	 \
-    0,-1,0,-1,  /* gimbals */    \
-    -1,-1,   	/* pots */       \
-    0,-1,     	/* sliders */    \
-    0,	     	/* vbat */       \
-    0,       	/* rtc_bat */    \
-    0,       	/* SWA */        \
-    0,       	/* SWB */        \
-    0,       	/* SWC */        \
-    0,       	/* SWD */        \
-    0,       	/* SWE */        \
-    0        	/* SWF */        \
-  }
+ 0,-1,0,-1,  /* gimbals */    \
+ -1,-1,   	/* pots */       \
+ 0,-1,     	/* sliders */    \
+ 0,0,        /* ext */        \
+ 0,	     	/* vbat */       \
+ 0,       	/* rtc_bat */    \
+ 0	     	/* lux sensor */ \
+}
 
 #define USE_EXTI9_5_IRQ // used for I2C port extender interrupt
 #define EXTI9_5_IRQ_Priority 5
