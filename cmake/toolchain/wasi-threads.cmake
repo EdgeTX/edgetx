ef0a95e0d7847fcf5d991b74b23e5196b1118536
raphaelcoeffic <1050031+raphaelcoeffic@users.noreply.github.com>
2026-03-21 08:35:56 +0100

    fix: add WASI_SDK_PREFIX to CMAKE_TRY_COMPILE_PLATFORM_VARIABLES

--- cmake/toolchain/wasi-threads.cmake
+++ cmake/toolchain/wasi-threads.cmake
@@ -1,5 +1,9 @@
 # Wrapper around wasi-sdk's pthread toolchain that uses the non-deprecated
 # wasm32-wasip1-threads triple (compatible with SDK 25+).
+
+# Propagate WASI_SDK_PREFIX into try_compile sub-projects (used during ABI detection)
+list(APPEND CMAKE_TRY_COMPILE_PLATFORM_VARIABLES WASI_SDK_PREFIX)
+
 if(NOT WASI_SDK_PREFIX)
   if(DEFINED ENV{WASI_SDK_PATH})
     set(WASI_SDK_PREFIX $ENV{WASI_SDK_PATH})
@@ -7,7 +11,9 @@ if(NOT WASI_SDK_PREFIX)
     set(WASI_SDK_PREFIX "/opt/wasi-sdk")
   endif()
 endif()
+
 include(${WASI_SDK_PREFIX}/share/cmake/wasi-sdk-pthread.cmake)
+
 set(triple wasm32-wasip1-threads)
 set(CMAKE_C_COMPILER_TARGET ${triple})
 set(CMAKE_CXX_COMPILER_TARGET ${triple})
