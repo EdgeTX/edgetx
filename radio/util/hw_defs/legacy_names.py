2b36249ff3242a81cdda080782f08975260bebda
Peter Feerick <peter.feerick@gmail.com>
2026-03-22 02:29:34 +0000

    fix: incorrect switch and pot settings

--- radio/util/hw_defs/legacy_names.py
+++ radio/util/hw_defs/legacy_names.py
@@ -776,7 +776,53 @@ LEGACY_NAMES = [
         }
     },
     {
-        "targets": {"v12", "v14"},
+        "targets": {"v12"},
+        "inputs": {
+            "LH": {
+                "yaml": "Rud",
+                "lua": "rud",
+                "description": "Rudder"
+            },
+            "LV": {
+                "yaml": "Ele",
+                "lua": "ele",
+                "description": "Elevator"
+            },
+            "RV": {
+                "yaml": "Thr",
+                "lua": "thr",
+                "description": "Throttle"
+            },
+            "RH": {
+                "yaml": "Ail",
+                "lua": "ail",
+                "description": "Aileron"
+            },
+            "P1": {
+                "yaml": "POT1",
+                "lua": "s1",
+                "label": "P1",
+                "short_label": "1",
+                "description": "Potentiometer 1"
+            },
+            "P2": {
+                "yaml": "POT2",
+                "lua": "s2",
+                "label": "P2",
+                "short_label": "2",
+                "description": "Potentiometer 2"
+            },
+            "P3": {
+                "yaml": "POT3",
+                "lua": "s3",
+                "label": "S3",
+                "short_label": "3",
+                "description": "6 pos"
+            }
+        }
+    },
+    {
+        "targets": {"v14"},
         "inputs": {
             "LH": {
                 "yaml": "Rud",
