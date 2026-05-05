ea38c76080b9c0b6fad277430e994b230580359d
HThuren <thuren.henrik@gmail.com>
2026-05-05 21:02:27 +0200

    SE

--- radio/src/translations/bl_translations.h
+++ radio/src/translations/bl_translations.h
@@ -360,10 +360,10 @@
     #define TR_BL_EXIT_KEY                 BL_EXIT_KEY " foer att avbryta"
 
     // Bootloader UF2 specific - ASCII characters only
-    #define TR_BL_DFU_MODE                "DFU mode"
-    #define TR_BL_PLUG_USB                "Plug USB cable"
-    #define TR_BL_COPY_UF2                "Copy firmware.uf2 to EDGETX_UF2 drive"
-    #define TR_BL_NO_VERSION              "No version"
+    #define TR_BL_DFU_MODE                "DFU-laege"
+    #define TR_BL_PLUG_USB                "Plugga in USB-kabeln"
+    #define TR_BL_COPY_UF2                "Kopiera firmware.uf2 till enhet EDGETX_UF2"
+    #define TR_BL_NO_VERSION              "Ingen version"
 
 #else
 
