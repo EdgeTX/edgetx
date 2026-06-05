f8c91f51bf1094a5bb3d650708950ce362e1f408
richardclli <richard.li@ces.hk>
2026-06-05 15:00:59 +0800

    fix(pl18): prevent hang when pressing power button during USB charging (#7404)

--- radio/src/targets/pl18/board.cpp
+++ radio/src/targets/pl18/board.cpp
@@ -249,6 +249,7 @@ void boardInit()
         if ((now - press_start) > POWER_ON_DELAY) {
           break;
         }
+        delay_ms(10);
       } else if (!isChargerActive()) {
         boardOff();
       } else {
