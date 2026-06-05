ea59010b58f22c953cc7d8b491f0b35e135b8e39
elecpower <elecpower@users.noreply.github.com>
2026-06-06 07:11:37 +1000

    Refactor app settings edit

---
+++ companion/src/shared/autocolorselectbtn.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) EdgeTX
+ *
+ * Based on code named
+ *   opentx - https://github.com/opentx/opentx
+ *   th9x - http://code.google.com/p/th9x
+ *   er9x - http://code.google.com/p/er9x
+ *   gruvin9x - http://code.google.com/p/gruvin9x
+ *
+ * License GPLv2: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#pragma once
+
+#include "autopushbutton.h"
+
+#include <QColorDialog>
+
+class AutoColorSelectBtn : public AutoPushButton
+{
+  Q_OBJECT
+
+  public:
+    explicit AutoColorSelectBtn(QWidget * parent = nullptr);
+    virtual ~AutoColorSelectBtn();
+
+    virtual void updateValue() override {}
+
+    void setup(QColor color, QWidget * sample = nullptr);
+
+  signals:
+    void colorChanged(QColor color);
+
+  private:
+    QWidget *m_sample;
+    QColor m_color;
+};
