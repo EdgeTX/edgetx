ef813c88c7ee379af065b25010c15140e60dfc5a
3djc <3djc@gh.com>
2026-06-05 12:28:48 +0200

    tentative: rgbleds H5 handling

--- radio/src/targets/common/arm/stm32/stm32_ws2812.cpp
+++ radio/src/targets/common/arm/stm32/stm32_ws2812.cpp
@@ -138,7 +138,9 @@ static void _update_dma_buffer(const stm32_pulse_timer_t* tim, uint8_t tc)
 
     LL_DMA_DisableIT_TC(tim->DMAx, tim->DMA_Stream);
     LL_DMA_DisableIT_HT(tim->DMAx, tim->DMA_Stream);
-#if !defined(STM32H7RS) && !defined(STM32H5)
+#if defined(STM32H7RS) || defined(STM32H5)
+    LL_DMA_DisableChannel(tim->DMAx, tim->DMA_Stream);
+#else
     LL_DMA_DisableStream(tim->DMAx, tim->DMA_Stream);
 
     uint32_t timeout = 1000;
@@ -164,27 +166,98 @@ void ws2812_dma_isr(const stm32_pulse_timer_t* tim)
   }
 }
 
-static void _led_set_dma_periph_addr(const stm32_pulse_timer_t* tim)
+static volatile uint32_t* _led_cmp_reg(const stm32_pulse_timer_t* tim)
 {
-  volatile uint32_t* cmp_reg = nullptr;
   switch(tim->TIM_Channel) {
   case LL_TIM_CHANNEL_CH1:
   case LL_TIM_CHANNEL_CH1N:
-    cmp_reg = &tim->TIMx->CCR1;
-    break;
+    return &tim->TIMx->CCR1;
   case LL_TIM_CHANNEL_CH2:
-    cmp_reg = &tim->TIMx->CCR2;
-    break;
+    return &tim->TIMx->CCR2;
   case LL_TIM_CHANNEL_CH3:
-    cmp_reg = &tim->TIMx->CCR3;
-    break;
+    return &tim->TIMx->CCR3;
   case LL_TIM_CHANNEL_CH4:
-    cmp_reg = &tim->TIMx->CCR4;
-    break;
+    return &tim->TIMx->CCR4;
   }
-
-  LL_DMA_SetPeriphAddress(tim->DMAx, tim->DMA_Stream, (uint32_t)cmp_reg);
+  return nullptr;
 }
+
+#if defined(STM32H5) || defined(STM32H7RS)
+
+// GPDMA has no circular mode: emulate it with a single self-linking linked-list
+// node (mem -> CCRx, triggered by the timer UPDATE request). HT/TC events fire
+// per block iteration, driving the half/half buffer refill in the DMA ISR.
+// Mirrors the linked-list setup used by stm32_usart_init_rx_dma.
+static LL_DMA_LinkNodeTypeDef _led_dma_node;
+
+#define WS2812_LL_NODE_UPDATE                                              \
+  (LL_DMA_UPDATE_CTR1 | LL_DMA_UPDATE_CTR2 | LL_DMA_UPDATE_CBR1 |          \
+   LL_DMA_UPDATE_CSAR | LL_DMA_UPDATE_CDAR | LL_DMA_UPDATE_CTR3 |          \
+   LL_DMA_UPDATE_CBR2 | LL_DMA_UPDATE_CLLR)
+
+static void _led_setup_circular_dma(const stm32_pulse_timer_t* tim)
+{
+  LL_DMA_DisableChannel(tim->DMAx, tim->DMA_Stream);
+
+  const bool word = (pulse_inc == 2); // 32-bit timer -> 32-bit CCR writes
+  const uint32_t blkBytes =
+      WS2821_DMA_BUFFER_LEN * sizeof(led_timer_value_t) * pulse_inc;
+
+  LL_DMA_InitNodeTypeDef nodeInit;
+  LL_DMA_NodeStructInit(&nodeInit);
+
+  nodeInit.DestAllocatedPort = LL_DMA_DEST_ALLOCATED_PORT0;
+  nodeInit.DestHWordExchange = LL_DMA_DEST_HALFWORD_PRESERVE;
+  nodeInit.DestByteExchange  = LL_DMA_DEST_BYTE_PRESERVE;
+  nodeInit.DestBurstLength   = 1;
+  nodeInit.DestIncMode       = LL_DMA_DEST_FIXED;            // CCRx fixed
+  nodeInit.DestDataWidth     = word ? LL_DMA_DEST_DATAWIDTH_WORD
+                                    : LL_DMA_DEST_DATAWIDTH_HALFWORD;
+  nodeInit.SrcAllocatedPort  = LL_DMA_SRC_ALLOCATED_PORT1;
+  nodeInit.SrcByteExchange   = LL_DMA_SRC_BYTE_PRESERVE;
+  nodeInit.DataAlignment     = LL_DMA_DATA_ALIGN_ZEROPADD;
+  nodeInit.SrcBurstLength    = 1;
+  nodeInit.SrcIncMode        = LL_DMA_SRC_INCREMENT;         // walk the buffer
+  nodeInit.SrcDataWidth      = word ? LL_DMA_SRC_DATAWIDTH_WORD
+                                    : LL_DMA_SRC_DATAWIDTH_HALFWORD;
+  nodeInit.TransferEventMode = LL_DMA_TCEM_BLK_TRANSFER;     // HT/TC per block
+  nodeInit.TriggerPolarity   = LL_DMA_TRIG_POLARITY_MASKED;
+  nodeInit.BlkHWRequest      = LL_DMA_HWREQUEST_SINGLEBURST;
+  nodeInit.Direction         = LL_DMA_DIRECTION_MEMORY_TO_PERIPH;
+  nodeInit.Request           = tim->DMA_Channel;             // TIMx_UP
+  nodeInit.UpdateRegisters   = WS2812_LL_NODE_UPDATE;
+  nodeInit.NodeType          = LL_DMA_GPDMA_LINEAR_NODE;
+  nodeInit.SrcAddress        = (intptr_t)_led_dma_buffer;
+  nodeInit.DestAddress       = (intptr_t)_led_cmp_reg(tim);
+  nodeInit.BlkDataLength     = blkBytes;
+  LL_DMA_CreateLinkNode(&nodeInit, &_led_dma_node);
+
+  // Connect the node to itself to loop forever (circular emulation)
+  LL_DMA_ConnectLinkNode(&_led_dma_node, LL_DMA_CLLR_OFFSET5,
+                         &_led_dma_node, LL_DMA_CLLR_OFFSET5);
+
+  LL_DMA_SetLinkedListBaseAddr(tim->DMAx, tim->DMA_Stream,
+                               (intptr_t)&_led_dma_node);
+  LL_DMA_ConfigLinkUpdate(tim->DMAx, tim->DMA_Stream, WS2812_LL_NODE_UPDATE,
+                          (intptr_t)&_led_dma_node);
+
+  LL_DMA_InitLinkedListTypeDef ll = {0};
+  ll.Priority          = LL_DMA_HIGH_PRIORITY;
+  ll.LinkStepMode      = LL_DMA_LSM_FULL_EXECUTION;
+  ll.LinkAllocatedPort = LL_DMA_LINK_ALLOCATED_PORT0;
+  ll.TransferEventMode = LL_DMA_TCEM_BLK_TRANSFER;
+  LL_DMA_List_Init(tim->DMAx, tim->DMA_Stream, &ll);
+}
+
+#else
+
+static void _led_set_dma_periph_addr(const stm32_pulse_timer_t* tim)
+{
+  LL_DMA_SetPeriphAddress(tim->DMAx, tim->DMA_Stream,
+                          (uint32_t)_led_cmp_reg(tim));
+}
+
+#endif
 
 static void _init_timer(const stm32_pulse_timer_t* tim)
 {
@@ -192,12 +265,17 @@ static void _init_timer(const stm32_pulse_timer_t* tim)
   stm32_pulse_config_output(tim, true, LL_TIM_OCMODE_PWM1, 0);
   LL_TIM_SetAutoReload(tim->TIMx, WS2812_TIMER_PERIOD - 1);
 
+#if defined(STM32H5) || defined(STM32H7RS)
+  // GPDMA: circular transfer is (re)armed via the linked-list node in
+  // ws2812_update(); nothing to pre-configure here.
+#else
   // pulse driver uses DMA to ARR, but we need CCRx
   _led_set_dma_periph_addr(tim);
 
   LL_DMA_SetMode(tim->DMAx, tim->DMA_Stream, LL_DMA_MODE_CIRCULAR);
   LL_DMA_SetDataLength(tim->DMAx, tim->DMA_Stream, WS2821_DMA_BUFFER_LEN);
   LL_DMA_SetMemoryAddress(tim->DMAx, tim->DMA_Stream, (uint32_t)_led_dma_buffer);
+#endif
 
   // we need to use a higher prio to avoid having
   // issues with some other things used during boot
@@ -251,7 +329,11 @@ bool ws2812_get_state_in_buf(const uint8_t* buf, uint8_t led)
 
 bool ws2812_is_busy(const stm32_pulse_timer_t* tim)
 {
+#if defined(STM32H7RS) || defined(STM32H5)
+  return LL_DMA_IsEnabledChannel(tim->DMAx, tim->DMA_Stream);
+#else
   return LL_DMA_IsEnabledStream(tim->DMAx, tim->DMA_Stream);
+#endif
 }
 
 void ws2812_update(const stm32_pulse_timer_t* tim)
@@ -262,9 +344,17 @@ void ws2812_update(const stm32_pulse_timer_t* tim)
   _led_seq_cnt = 0;
   memset(_led_dma_buffer, 0, sizeof(_led_dma_buffer));
 
+#if defined(STM32H5) || defined(STM32H7RS)
+  // (re)arm the self-linking circular linked-list and start the channel
+  _led_setup_circular_dma(tim);
+  LL_DMA_EnableIT_HT(tim->DMAx, tim->DMA_Stream);
+  LL_DMA_EnableIT_TC(tim->DMAx, tim->DMA_Stream);
+  LL_DMA_EnableChannel(tim->DMAx, tim->DMA_Stream);
+#else
   LL_DMA_EnableIT_HT(tim->DMAx, tim->DMA_Stream);
   LL_DMA_EnableIT_TC(tim->DMAx, tim->DMA_Stream);
   LL_DMA_EnableStream(tim->DMAx, tim->DMA_Stream);
+#endif
 
   LL_TIM_EnableDMAReq_UPDATE(tim->TIMx);
   LL_TIM_CC_EnableChannel(tim->TIMx, tim->TIM_Channel);
