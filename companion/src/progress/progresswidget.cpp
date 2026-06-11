0d16491daaed6e589b62cb55759d2cfcf5c24dc4
elecpower <elecpower@users.noreply.github.com>
2026-06-11 11:02:54 +1000

    fix syntax

--- companion/src/progress/progresswidget.cpp
+++ companion/src/progress/progresswidget.cpp
@@ -53,7 +53,7 @@ ProgressWidget::ProgressWidget(QWidget *parent) :
   newFont.setPointSize(9);
 #endif
   ui->textEdit->setFont(newFont);
-  QTimer::singleShot(0, [this]() { this->refresh(); })
+  QTimer::singleShot(0, [this]() { this->refresh(); });
 }
 
 ProgressWidget::~ProgressWidget()
