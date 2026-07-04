35f270de45ae251b3da30232176c774e5df1be37
philmoz <philmoz@users.noreply.github.com>
2026-07-04 18:40:13 +1000

    fix(color): missing LXL font size in font picker list (#7510)

--- radio/src/translations/i18n/se.h
+++ radio/src/translations/i18n/se.h
@@ -436,6 +436,7 @@
 #define TR_FONT_SIZES_5                 "L"
 #define TR_FONT_SIZES_6                 "XL"
 #define TR_FONT_SIZES_7                 "XXL"
+#define TR_FONT_SIZES_8                "LXL"
 
 #define TR_ENTER                        "[MENY]"
 #define TR_OK                           TR_BW_COL(TR("\010\010\010[OK]", "\010\010\010\010\010[OK]"), "Ok")
