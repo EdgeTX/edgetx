76341a011ed85cedcfd16b807f95697c5de8f650
Peter Feerick <5500713+pfeerick@users.noreply.github.com>
2026-07-20 03:06:07 +0000

    chore(sdram): remove dead code

--- radio/src/boards/helloradio-h750/sdram_driver.cpp
+++ radio/src/boards/helloradio-h750/sdram_driver.cpp
@@ -30,8 +30,6 @@
 
 #define SDCLOCK_PERIOD    FMC_SDRAM_CLOCK_PERIOD_2
 
-#define SDRAM_TIMEOUT     ((uint32_t)0xFFFF)
-
 #define SDRAM_READBURST    FMC_SDRAM_RBURST_DISABLE
 
 #define SDRAM_MODEREG_BURST_LENGTH_1             ((uint16_t)0x0000)
@@ -110,7 +108,6 @@ extern "C" void SDRAM_InitSequence(void)
 {
   FMC_SDRAM_CommandTypeDef FMC_SDRAMCommandStructure;
   uint32_t tmpr = 0;
-  uint32_t timeout = SDRAM_TIMEOUT;
 
   /* Step 3 --------------------------------------------------------------------*/
   /* Configure a clock configuration enable command */
@@ -118,12 +115,6 @@ extern "C" void SDRAM_InitSequence(void)
   FMC_SDRAMCommandStructure.CommandTarget = FMC_SDRAM_CMD_TARGET_BANK1;
   FMC_SDRAMCommandStructure.AutoRefreshNumber = 1;
   FMC_SDRAMCommandStructure.ModeRegisterDefinition = 0;
-  /* Wait until the SDRAM controller is ready */
-  //  while((__FMC_SDRAM_GET_FLAG(FMC_Bank5_6_R, FMC_SDRAM_FLAG_BUSY) != 0) &&
-  //  (timeout > 0))
-  // {
-  //   timeout--;
-  // }
   /* Send the command */
   FMC_SDRAM_SendCommand(FMC_Bank5_6_R, &FMC_SDRAMCommandStructure, 10);
 
@@ -137,15 +128,8 @@ extern "C" void SDRAM_InitSequence(void)
   FMC_SDRAMCommandStructure.CommandTarget = FMC_SDRAM_CMD_TARGET_BANK1;
   FMC_SDRAMCommandStructure.AutoRefreshNumber = 1;
   FMC_SDRAMCommandStructure.ModeRegisterDefinition = 0;
-  /* Wait until the SDRAM controller is ready */
-  // timeout = SDRAM_TIMEOUT;
-  // while((__FMC_SDRAM_GET_FLAG(FMC_Bank5_6_R, FMC_SDRAM_FLAG_BUSY) != 0) && (timeout > 0))
-  // {
-  //   timeout--;
-  // }
   /* Send the command */
   FMC_SDRAM_SendCommand(FMC_Bank5_6_R, &FMC_SDRAMCommandStructure, 10);
-  // delay_ms(1);
 
   /* Step 6 --------------------------------------------------------------------*/
   /* Configure a Auto-Refresh command */
@@ -153,17 +137,8 @@ extern "C" void SDRAM_InitSequence(void)
   FMC_SDRAMCommandStructure.CommandTarget = FMC_SDRAM_CMD_TARGET_BANK1;
   FMC_SDRAMCommandStructure.AutoRefreshNumber = 8;
   FMC_SDRAMCommandStructure.ModeRegisterDefinition = 0;
-
-  /* Wait until the SDRAM controller is ready */
-  // timeout = SDRAM_TIMEOUT;
-  // while((__FMC_SDRAM_GET_FLAG(FMC_Bank5_6_R, FMC_SDRAM_FLAG_BUSY) != 0) &&
-  // (timeout > 0))
-  // {
-  //   timeout--;
-  // }
   /* Send the command */
   FMC_SDRAM_SendCommand(FMC_Bank5_6_R, &FMC_SDRAMCommandStructure, 10);
-  // delay_ms(1);
 
   /* Step 7 --------------------------------------------------------------------*/
   /* Program the external memory mode register */
@@ -178,16 +153,8 @@ extern "C" void SDRAM_InitSequence(void)
   FMC_SDRAMCommandStructure.CommandTarget = FMC_SDRAM_CMD_TARGET_BANK1;
   FMC_SDRAMCommandStructure.AutoRefreshNumber = 1;
   FMC_SDRAMCommandStructure.ModeRegisterDefinition = tmpr;
-
-  /* Wait until the SDRAM controller is ready */
-  // timeout = SDRAM_TIMEOUT;
-  // while((__FMC_SDRAM_GET_FLAG(FMC_Bank5_6_R, FMC_SDRAM_FLAG_BUSY) != 0) && (timeout > 0))
-  // {
-  //   timeout--;
-  // }
   /* Send the command */
   FMC_SDRAM_SendCommand(FMC_Bank5_6_R, &FMC_SDRAMCommandStructure, 10);
-  // delay_ms(1);
 
   /* Step 8 --------------------------------------------------------------------*/
   /* Set the refresh rate counter */
@@ -195,13 +162,6 @@ extern "C" void SDRAM_InitSequence(void)
   /* Set the device refresh counter */
   FMC_SDRAM_SetAutoRefreshNumber(FMC_Bank5_6_R, 15);
   FMC_SDRAM_ProgramRefreshRate(FMC_Bank5_6_R, 1855);
-  /* Wait until the SDRAM controller is ready */
-  // timeout = SDRAM_TIMEOUT;
-  // while((__FMC_SDRAM_GET_FLAG(FMC_Bank5_6_R, FMC_SDRAM_FLAG_BUSY) != 0) && (timeout > 0))
-  // {
-  //   timeout--;
-  // }
-  // delay_ms(1);
 }
 
 extern "C" void SDRAM_Init(void)
