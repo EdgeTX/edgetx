eeacfb9e27f7d16c395d2b48458c07d27ad6bd4f
Michael <101.37584@germanynet.de>
2026-07-30 18:52:19 +0200

    add dependency to hal_settings

--- cmake/Macros.cmake
+++ cmake/Macros.cmake
@@ -80,7 +80,7 @@ function(GenerateDatacopy source output)
   add_custom_command(
     OUTPUT ${output}
     COMMAND ${GEN_DATACOPY_CMD} > ${output}
-    DEPENDS ${GEN_DATACOPY_DEPEND} ${CMAKE_BINARY_DIR}/CMakeCache.txt
+    DEPENDS ${GEN_DATACOPY_DEPEND} ${CMAKE_BINARY_DIR}/CMakeCache.txt hal_settings
     )
 endfunction()
 
