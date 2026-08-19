853adfd88188fef783cdb9eafd003ce6658c9585
Peter Feerick <5500713+pfeerick@users.noreply.github.com>
2026-08-19 04:46:53 +0000

    chore: formatting

--- radio/src/gui/gui_common.cpp
+++ radio/src/gui/gui_common.cpp
@@ -977,8 +977,7 @@ bool isExternalModuleAvailable(int moduleType)
   if (moduleType == MODULE_TYPE_R9M_PXX1 ||
       moduleType == MODULE_TYPE_R9M_PXX2 ||
       moduleType == MODULE_TYPE_XJT_PXX1 ||
-      moduleType == MODULE_TYPE_DSM2
-    )
+      moduleType == MODULE_TYPE_DSM2)
     return false;
 #endif
 
@@ -1016,7 +1015,7 @@ bool isExternalModuleAvailable(int moduleType)
 #endif
 
 #if !defined(DSMP)
-  if (moduleType == MODULE_TYPE_LEMON_DSMP) return false;
+  if (moduleType == MODULE_TYPE_DSMP) return false;
 #endif
 
 #if !defined(SBUS)
