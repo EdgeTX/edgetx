e2dc56735482d947f9c7b61735d6ce36a9965547
philmoz <philmoz@users.noreply.github.com>
2026-08-26 09:16:16 +1000

    fix(color): in manage models, long press on selected model button clears checked state (#7707)
    (cherry picked from commit 94d5a89e338b18a11459fcca9fe612fb0fa309f4)

--- radio/src/gui/colorlcd/model/model_select.cpp
+++ radio/src/gui/colorlcd/model/model_select.cpp
@@ -136,7 +136,6 @@ class ModelButton : public Button
             return true;
           }
         }
-        showNoImgMsg();
       }
 
       return false;
@@ -265,10 +264,12 @@ class ModelsPageBody : public Window
 
       // Long Press Handler for Models
       button->setLongPressHandler([=]() -> uint8_t {
-        button->setFocused();
-        focusedModel = model;
+        if (model != focusedModel) {
+          button->setFocused();
+          focusedModel = model;
+        }
         openMenu();
-        return 0;
+        return model == modelslist.getCurrentModel();
       });
     }
 
